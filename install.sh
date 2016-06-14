9344adc5f113c704dd0c1d1f05a326affb91e4c9
Hugh Perkins <hughperkins@gmail.com>
2016-06-14 15:13:41 +0800

    make all commmands automatically cd into this directory, so can run from anywhere

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # designed/tested on ubuntu 16.04
 
+scriptdir=$(dirname $0)
+cd ${scriptdir}
+
 sudo apt-get install -y python3 python3-dev python-virtualenv libffi-dev libssl-dev
 virtualenv -p python3 env3
 source env3/bin/activate
