ffb2dd9e68265d8f079d145397f9798f121ea7fc
Pytorch Buildbot <soumith+bot@pytorch.org>
2016-09-03 03:56:38 +0000

    fixing build for non PR

--- jenkins/pytorch/build.sh
+++ jenkins/pytorch/build.sh
@@ -8,9 +8,19 @@
 
 env | grep GIT
 env | grep ghprb
-echo "h=$ghprbActualCommit&p=pytorch&b=$GIT_BRANCH&"
+
+COMMIT_TO_TEST=""
+if [ -z "$ghprbActualCommit" ]; then
+    # not building in the ghprb regime, probably building master
+    COMMIT_TO_TEST=$GIT_COMMIT
+else 
+    # building a pull request
+    COMMIT_TO_TEST=$ghprbActualCommit
+fi
+
+echo "h=$COMMIT_TO_TEST&p=pytorch&b=$GIT_BRANCH&"
 stdout_fname=$(mktemp)
-curl -vs -d "h=$ghprbActualCommit&p=pytorch&b=$GIT_BRANCH&s=$shared_secret&g=$github_token" "http://localhost:3237/run" | tee  $stdout_fname
+curl -vs -d "h=$COMMIT_TO_TEST&p=pytorch&b=$GIT_BRANCH&s=$shared_secret&g=$github_token" "http://localhost:3237/run" | tee  $stdout_fname
 
 cat $stdout_fname | grep "ALL CHECKS PASSED"
 
