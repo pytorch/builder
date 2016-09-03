b121323bac2fe7c4b9fdd539c31650e3d63f3c99
Pytorch Buildbot <soumith+bot@pytorch.org>
2016-09-03 01:22:11 +0000

    cutorch -> pytorch

--- jenkins/pytorch/build.sh
+++ jenkins/pytorch/build.sh
@@ -2,15 +2,15 @@
 #
 # it is responsible for:
 # - bringing up a nimbix instance / running a nimbix job
-# - said nimbix job should then handle building torch, cutorch, running cutorch unit tests
+# - said nimbix job should then handle building pytorch unit tests
 
 # for now, this just says hello and stuff
 
 env | grep GIT
 env | grep ghprb
-echo "h=$ghprbActualCommit&p=cutorch&b=$GIT_BRANCH&"
+echo "h=$ghprbActualCommit&p=pytorch&b=$GIT_BRANCH&"
 stdout_fname=$(mktemp)
-curl -vs -d "h=$ghprbActualCommit&p=cutorch&b=$GIT_BRANCH&s=$shared_secret" "http://$WRAPPER_IP:3237/run" | tee  $stdout_fname
+curl -vs -d "h=$ghprbActualCommit&p=pytorch&b=$GIT_BRANCH&s=$shared_secret" "http://localhost:3237/run" | tee  $stdout_fname
 
 cat $stdout_fname | grep "ALL CHECKS PASSED"
 
