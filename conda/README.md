d81f98b91b61c17f2b0711270f65fdc2932f26b2
Soumith Chintala <soumith@fb.com>
2016-09-07 01:15:00 -0400

    adding working conda build scripts

---
+++ conda/README.md
@@ -0,0 +1,18 @@
+## Building conda binaries
+
+- Change the BUILD_VERSION and BUILD_NUMBER variables in build_all.sh as appropriate
+- If pytorch-$BUILD_VERSION folder doesn't exist, copy over the last version and change the meta.yaml if necessary (if tests change etc.)
+  - `cp -r pytorch-0.1.3 pytorch-$BUILD_VERSION`
+  - `git add pytorch-$BUILD_VERSION`
+- Run `./build_all.ash` on an OSX machine and a Linux machine
+
+
+### TODO
+- [ ] Build and test on Linux + CUDA
+- [ ] Build and test on OSX + CUDA
+- [ ] Check what happens when you build on Linux + CUDA on one machine and run the binary on another machine
+  - without cuda
+  - with a different GPU driver than the original
+  - with an insufficient driver version corresponding to the CUDA version
+
+
---
+++ conda/build_all.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -e
+
+if [ -z "$ANACONDA_TOKEN" ]; then 
+    echo "ANACONDA_TOKEN is unset. Please set it in your environment before running this script"; 
+    exit 1
+fi
+
+BUILD_VERSION="0.1.3"
+BUILD_NUMBER=0
+
+export PYTORCH_BUILD_VERSION=$BUILD_VERSION
+export PYTORCH_BUILD_NUMBER=$BUILD_NUMBER
+
+conda config --set anaconda_upload no
+
+conda build --no-anaconda-upload --python 2.7 pytorch-$BUILD_VERSION
+# there's a bug in conda-build (fixed in master but waiting for versioning) that does not let this build now.
+# conda build --no-anaconda-upload --python 3.3 pytorch-$BUILD_VERSION 
+conda build --no-anaconda-upload --python 3.4 pytorch-$BUILD_VERSION
+conda build --no-anaconda-upload --python 3.5 pytorch-$BUILD_VERSION
+
+echo "All builds succeeded, uploading binaries"
+
+anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build --python 2.7 pytorch-$BUILD_VERSION --output)
+anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build --python 3.4 pytorch-$BUILD_VERSION --output)
+anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build --python 3.5 pytorch-$BUILD_VERSION --output)
+
+unset PYTORCH_BUILD_VERSION
