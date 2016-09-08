f9102290be85de39198245ad9d68a0ad041df15e
soumith <soumith@gmail.com>
2016-09-07 23:39:53 -0400

    working CUDA build

--- conda/pytorch-0.1.3/build.sh
+++ conda/pytorch-0.1.3/build.sh
@@ -5,4 +5,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     MACOSX_DEPLOYMENT_TARGET=10.9 python setup.py install
 else
     python setup.py install
+    cp -P /usr/local/cuda/lib64/libcusparse.so* $SP_DIR/torch/lib
+    cp -P /usr/local/cuda/lib64/libcublas.so* $SP_DIR/torch/lib
+    cp -P /usr/local/cuda/lib64/libcudart.so* $SP_DIR/torch/lib
+    
 fi
