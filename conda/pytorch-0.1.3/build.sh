16d83d865691c8cf8f457473f4d437ab3e9c81a3
Soumith Chintala <soumith@fb.com>
2016-09-07 22:04:23 -0400

    fixing osx build

--- conda/pytorch-0.1.3/build.sh
+++ conda/pytorch-0.1.3/build.sh
@@ -3,6 +3,10 @@ export CMAKE_PREFIX_PATH=$PREFIX
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     MACOSX_DEPLOYMENT_TARGET=10.9 python setup.py install
+    install_name_tool -change libmkl_intel_lp64.dylib "@rpath/../../../../libmkl_intel_lp64.dylib" $SP_DIR/torch/lib/libTH.dylib
+    install_name_tool -change libmkl_intel_thread.dylib "@rpath/../../../../libmkl_intel_thread.dylib" $SP_DIR/torch/lib/libTH.dylib
+    install_name_tool -change libmkl_core.dylib "@rpath/../../../../libmkl_core.dylib" $SP_DIR/torch/lib/libTH.dylib
+    install_name_tool -change libiomp5.dylib "@rpath/../../../../libiomp5.dylib" $SP_DIR/torch/lib/libTH.dylib
 else
     python setup.py install
 fi
