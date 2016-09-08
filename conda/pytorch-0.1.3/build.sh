1a4a15453f2d31889560286ecafe81d6961c1da5
Soumith Chintala <soumith@fb.com>
2016-09-07 21:07:14 -0400

    properly linking against mkl, disabling relocations for now

--- conda/pytorch-0.1.3/build.sh
+++ conda/pytorch-0.1.3/build.sh
@@ -1,2 +1,8 @@
 export CMAKE_LIBRARY_PATH=$PREFIX/lib:$PREFIX/include:$CMAKE_LIBRARY_PATH 
-python setup.py install
+export CMAKE_PREFIX_PATH=$PREFIX
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    MACOSX_DEPLOYMENT_TARGET=10.9 python setup.py install
+else
+    python setup.py install
+fi
