0136006d6f6ff03966df86ca75c6ba906049f7c9
Pytorch Buildbot <soumith+bot@pytorch.org>
2016-10-08 17:56:48 +0000

    build locals

--- jenkins/pytorch/build.sh
+++ jenkins/pytorch/build.sh
@@ -1,10 +1,12 @@
-# this will be running on the jenkins instance (ie not on nimbix instance)
+# this will be running on the jenkins instance 
 #
 # it is responsible for:
-# - bringing up a nimbix instance / running a nimbix job
-# - said nimbix job should then handle building pytorch unit tests
-
-# for now, this just says hello and stuff
+# checking BUILDER_TYPE environment var.
+# if BUILDER_TYPE == NIMBIX
+#   - bringing up a nimbix instance / running a nimbix job
+#   - said nimbix job should then handle building pytorch unit tests
+# else if BUILDER_TYPE == LOCAL (or doesn't exist)
+#   current box should handle building pytorch unit tests
 
 env | grep GIT
 env | grep ghprb
@@ -24,9 +26,28 @@ if [ -z "$jenkins_python_version" ]; then
     exit 1
 fi
 
-echo "h=$COMMIT_TO_TEST&p=pytorch&b=$GIT_BRANCH&"
-stdout_fname=$(mktemp)
-curl -vs -d "h=$COMMIT_TO_TEST&p=pytorch&b=$GIT_BRANCH&s=$shared_secret&g=$github_token&py=$jenkins_python_version" "http://localhost:3237/run" | tee  $stdout_fname
+BUILDER=${BUILDER_TYPE:-LOCAL}
 
-cat $stdout_fname | grep "ALL CHECKS PASSED"
+if [ "$BUILDER" == "NIMBIX"]; then
+    echo "h=$COMMIT_TO_TEST&p=pytorch&b=$GIT_BRANCH&"
+    stdout_fname=$(mktemp)
+    curl -vs -d "h=$COMMIT_TO_TEST&p=pytorch&b=$GIT_BRANCH&s=$shared_secret&g=$github_token&py=$jenkins_python_version" "http://localhost:3237/run" | tee  $stdout_fname
 
+    cat $stdout_fname | grep "ALL CHECKS PASSED"
+elif [ "$BUILDER" == "LOCAL"]; then
+    cd /tmp
+    rm -rf /tmp/builder
+    if ! which git
+    then
+	sudo apt-get install -y git
+    fi
+    git clone https://pytorchbot:$github_token@github.com/pytorch/builder --quiet
+    cd builder
+    stdout_fname=$(mktemp)
+    bash nimbix-bootstrap.sh pytorch $COMMIT_TO_TEST $GIT_BRANCH \
+	$github_token $jenkins_python_version | tee  $stdout_fname
+    cat $stdout_fname | grep "ALL CHECKS PASSED"
+else
+    echo "unknown BUILDER = $BUILDER , exiting"
+    exit 1
+fi
