b80f8852fe5bbeddad56fa6ad83207e5210fc51f
soumith <soumith@fb.com>
2016-12-20 08:51:16 -0800

    new build

--- conda/pytorch-0.1.6/build.sh
+++ conda/pytorch-0.1.6/build.sh
@@ -6,20 +6,9 @@ export TORCH_CUDA_ARCH_LIST="3.0;3.5;3.7;5.0;5.2+PTX"
 export PYTORCH_BINARY_BUILD=1
 export TH_BINARY_BUILD=1
 
-gccf() {
-       export PATH="$HOME/bin:$PATH";
-       export LD_LIBRARY_PATH="$HOME/bin/gcc-4.9.3/lib64:$HOME/lib:$LD_LIBRARY_PATH";
-       export CMAKE_PREFIX_PATH="$HOME/bin:$HOME/lib:$HOME/include:$CMAKE_PREFIX_PATH";
-       export CC="$HOME/bin/gcc";
-       export CXX="$HOME/bin/g++"
-}
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
     MACOSX_DEPLOYMENT_TARGET=10.9 python setup.py install
 else
-    # my alias to enable source-compiled gcc 4.9.3 on the centos6 machine
-    gccf
-
     # install
     python setup.py install
 
@@ -30,6 +19,4 @@ else
     cp -P /usr/local/cuda/lib64/libcurand.so* $SP_DIR/torch/lib
     # cudnn
     cp -P /usr/local/cuda/lib64/libcudnn.so* $SP_DIR/torch/lib
-    # gomp
-    cp -P /home/soumith/bin/gcc-4.9.3/lib64/libgomp.so* $SP_DIR/torch/lib
 fi
