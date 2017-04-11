e0193f6d6ead262b3d6e91e03118997528df0f66
Pytorch Buildbot <soumith+bot@pytorch.org>
2017-04-11 00:46:13 +0000

    temp fix

--- jenkins/pytorch/build_nimbix.sh
+++ jenkins/pytorch/build_nimbix.sh
@@ -165,8 +165,8 @@ pip install -r requirements.txt || true
 time python setup.py install
 
 echo "Testing pytorch"
-export OMP_NUM_THREADS=4
-export MKL_NUM_THREADS=4
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
 time test/run_test.sh
 
 echo "Installing torchvision at branch master"
