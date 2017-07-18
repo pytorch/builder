5334ed740667d330ea36ead709c8af0a21b20279
Soumith Chintala <soumith@gmail.com>
2017-07-18 00:40:25 -0400

    manywheel changes

--- manywheel/build.sh
+++ manywheel/build.sh
@@ -1,8 +1,12 @@
 export PYTORCH_BINARY_BUILD=1
 export TH_BINARY_BUILD=1
+export CMAKE_LIBRARY_PATH="/opt/intel/lib:/lib:$CMAKE_LIBRARY_PATH"
+
 git clone https://github.com/pytorch/pytorch
 cd pytorch
+export PATH=/opt/python/cp27-cp27m/bin:$PATH
 /opt/python/cp27-cp27m/bin/pip install -r requirements.txt
+/opt/python/cp27-cp27m/bin/pip install numpy
 /opt/python/cp27-cp27m/bin/python setup.py install
 cd test/
 LD_LIBRARY_PATH="$CMAKE_LIBRARY_PATH:$LD_LIBRARY_PATH" PYCMD=/opt/python/cp27-cp27m/bin/python ./run_test.sh
