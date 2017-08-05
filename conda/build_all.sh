df69874f66b0dd458b37bf0fae3f75a90988aab5
Soumith Chintala <soumith@gmail.com>
2017-08-04 19:53:36 -0700

    conda build scripts fixes

--- conda/build_all.sh
+++ conda/build_all.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 set -e
 
+. /remote/anaconda_token || true
+
 if [ -z "$ANACONDA_TOKEN" ]; then
     echo "ANACONDA_TOKEN is unset. Please set it in your environment before running this script";
     exit 1
 fi
 
-BUILD_VERSION="0.1.12"
-BUILD_NUMBER=2
+BUILD_VERSION="0.2.0"
+BUILD_NUMBER=1
 
+ANACONDA_USER=soumith
 rm -rf pytorch-src
 git clone https://github.com/pytorch/pytorch pytorch-src
 pushd pytorch-src
@@ -22,26 +25,20 @@ conda config --set anaconda_upload no
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
 
-    time conda build -c soumith --no-anaconda-upload --python 2.7 pytorch-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.5 pytorch-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.6 pytorch-$BUILD_VERSION    
-    time conda build -c soumith --no-anaconda-upload --python 3.6.0 pytorch-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.6.1 pytorch-$BUILD_VERSION    
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 pytorch-$BUILD_VERSION
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 pytorch-$BUILD_VERSION
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 pytorch-$BUILD_VERSION    
 else
     ./switch_cuda_version.sh 7.5
-    time conda build -c soumith --no-anaconda-upload --python 2.7 pytorch-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.5 pytorch-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.6 pytorch-$BUILD_VERSION    
-    time conda build -c soumith --no-anaconda-upload --python 3.6.0 pytorch-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.6.1 pytorch-$BUILD_VERSION    
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 pytorch-$BUILD_VERSION
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 pytorch-$BUILD_VERSION
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 pytorch-$BUILD_VERSION    
 
     ./switch_cuda_version.sh 8.0
 
-    time conda build -c soumith --no-anaconda-upload --python 2.7 pytorch-cuda80-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.5 pytorch-cuda80-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.6 pytorch-cuda80-$BUILD_VERSION    
-    time conda build -c soumith --no-anaconda-upload --python 3.6.0 pytorch-cuda80-$BUILD_VERSION
-    time conda build -c soumith --no-anaconda-upload --python 3.6.1 pytorch-cuda80-$BUILD_VERSION    
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 pytorch-cuda80-$BUILD_VERSION
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 pytorch-cuda80-$BUILD_VERSION
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 pytorch-cuda80-$BUILD_VERSION    
 
     ./switch_cuda_version.sh 7.5 # restore
 fi
@@ -50,17 +47,13 @@ echo "All builds succeeded, uploading binaries"
 
 set +e
 
-anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 2.7 pytorch-$BUILD_VERSION --output)
-anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.5 pytorch-$BUILD_VERSION --output)
-anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.6 pytorch-$BUILD_VERSION --output)
-anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.6.0 pytorch-$BUILD_VERSION --output)
-anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.6.1 pytorch-$BUILD_VERSION --output)
+anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 2.7 pytorch-$BUILD_VERSION --output)
+anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 3.5 pytorch-$BUILD_VERSION --output)
+anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 3.6 pytorch-$BUILD_VERSION --output)
 if [[ "$OSTYPE" == "linux"* ]]; then
-    anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 2.7 pytorch-cuda80-$BUILD_VERSION --output)
-    anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.5 pytorch-cuda80-$BUILD_VERSION --output)
-    anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.6 pytorch-cuda80-$BUILD_VERSION --output)
-    anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.6.0 pytorch-cuda80-$BUILD_VERSION --output)
-    anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.6.1 pytorch-cuda80-$BUILD_VERSION --output)
+    anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 2.7 pytorch-cuda80-$BUILD_VERSION --output)
+    anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 3.5 pytorch-cuda80-$BUILD_VERSION --output)
+    anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 3.6 pytorch-cuda80-$BUILD_VERSION --output)
 fi
 
 # set -e
@@ -76,14 +69,14 @@ fi
 # export PYTORCH_VISION_BUILD_VERSION=$VISION_BUILD_VERSION
 # export PYTORCH_VISION_BUILD_NUMBER=$VISION_BUILD_NUMBER
 
-# time conda build -c soumith --no-anaconda-upload --python 2.7 torchvision-$VISION_BUILD_VERSION
-# time conda build -c soumith --no-anaconda-upload --python 3.5 torchvision-$VISION_BUILD_VERSION
-# time conda build -c soumith --no-anaconda-upload --python 3.6 torchvision-$VISION_BUILD_VERSION
+# time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 torchvision-$VISION_BUILD_VERSION
+# time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 torchvision-$VISION_BUILD_VERSION
+# time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 torchvision-$VISION_BUILD_VERSION
 
 # set +e
-# anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 2.7 torchvision-$VISION_BUILD_VERSION --output)
-# anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.5 torchvision-$VISION_BUILD_VERSION --output)
-# anaconda -t $ANACONDA_TOKEN upload --user soumith $(conda build -c soumith --python 3.6 torchvision-$VISION_BUILD_VERSION --output)
+# anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 2.7 torchvision-$VISION_BUILD_VERSION --output)
+# anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 3.5 torchvision-$VISION_BUILD_VERSION --output)
+# anaconda -t $ANACONDA_TOKEN upload --user $ANACONDA_USER $(conda build -c $ANACONDA_USER --python 3.6 torchvision-$VISION_BUILD_VERSION --output)
 
 
 
