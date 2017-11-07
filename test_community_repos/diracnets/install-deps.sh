7f2d5a463f75fc77b0aaf133a1dc065f82679d66
Soumith Chintala <soumith@gmail.com>
2017-11-07 14:50:41 -0800

    fixes

--- test_community_repos/diracnets/install-deps.sh
+++ test_community_repos/diracnets/install-deps.sh
@@ -1,4 +1,4 @@
-conda install -c menpo opencv3
+conda install -c menpo -y opencv3
 pip install git+https://github.com/pytorch/tnt.git@master
 pip install git+https://github.com/szagoruyko/vision.git@opencv
 pip install -r requirements.txt
