0c1021b01ca18421ae2475bc3940c6dc4c96b4c3
Richard Zou <zou3519@gmail.com>
2017-11-13 15:57:24 -0700

    increase niter for dcgan

--- test_community_repos/examples/dcgan/run-script.sh
+++ test_community_repos/examples/dcgan/run-script.sh
@@ -1,6 +1,6 @@
 pushd examples/dcgan
 # smoke test
-python main.py --dataset fake --dataroot . --cuda --niter 1
+python main.py --dataset fake --dataroot . --cuda --niter 100
 RETURN_CODE=$?
 popd
 exit $RETURN_CODE
