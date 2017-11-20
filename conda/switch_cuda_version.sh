fde470636e5ab49bd8c8e43a3a21aa07b87851f7
Soumith Chintala <soumith@gmail.com>
2017-11-19 19:53:44 -0800

    [conda] improve cuda switching script

--- conda/switch_cuda_version.sh
+++ conda/switch_cuda_version.sh
@@ -4,8 +4,12 @@ then
 fi
 
 echo "Switching symlink to /usr/local/cuda-$1"
-rm /usr/local/cuda
+rm -f /usr/local/cuda
 ln -s /usr/local/cuda-$1 /usr/local/cuda
+export CUDA_VERSION=$(ls /usr/local/cuda/lib64/libcudart.so.*|sort|tac | head -1 | rev | cut -d"." -f -3 | rev)
+export CUDNN_VERSION=$(ls /usr/local/cuda/lib64/libcudnn.so.*|sort|tac | head -1 | rev | cut -d"." -f -3 | rev)
 
 ls -alh /usr/local/cuda
 
+echo "CUDA_VERSION=$CUDA_VERSION"
+echo "CUDNN_VERSION=$CUDNN_VERSION"
