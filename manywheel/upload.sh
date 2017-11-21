686fe0099f85cdb7262af7ac0f20b3656eb2d89c
Soumith Chintala <soumith@gmail.com>
2017-11-21 14:43:17 -0800

    add nccl2 metapackage, fix upload script for wheels

--- manywheel/upload.sh
+++ manywheel/upload.sh
@@ -1,2 +1,3 @@
 ls wheelhouse75/ | xargs -I {} aws s3 cp wheelhouse75/{} s3://pytorch/whl/cu75/ --acl public-read
-ls wheelhouse/   | xargs -I {} aws s3 cp wheelhouse/{} s3://pytorch/whl/cu80/ --acl public-read
+ls wheelhouse80/   | xargs -I {} aws s3 cp wheelhouse80/{} s3://pytorch/whl/cu80/ --acl public-read
+ls wheelhouse90/   | xargs -I {} aws s3 cp wheelhouse90/{} s3://pytorch/whl/cu90/ --acl public-read
