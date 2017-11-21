4a885c47c838eac26a04811ad88bc639755a9047
Soumith Chintala <soumith@gmail.com>
2017-11-20 18:48:41 -0800

    fix dockerfiles for wheels

--- manywheel/deploy.sh
+++ manywheel/deploy.sh
@@ -1,3 +1,5 @@
 set -e
-nvidia-docker build -t soumith/manylinux-cuda .
+nvidia-docker build -t soumith/manylinux-cuda80 -f Dockerfile .
+nvidia-docker build -t soumith/manylinux-cuda75 -f Dockerfile_75 .
+nvidia-docker build -t soumith/manylinux-cuda90 -f Dockerfile_90 .
 #docker push soumith/manylinux-cuda
