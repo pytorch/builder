b7ae18ff7d9a96f8359c57cf1a1251fc780a42b6
Soumith Chintala <soumith@gmail.com>
2017-11-22 09:55:10 -0800

    add faiss recipes

---
+++ conda/build_faiss.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+. /remote/anaconda_token || true
+
+set -e
+
+if [ -z "$ANACONDA_TOKEN" ]; then
+    echo "ANACONDA_TOKEN is unset. Please set it in your environment before running this script";
+    exit 1
+fi
+
+export FAISS_BUILD_VERSION="0.1"
+export FAISS_BUILD_NUMBER=1
+
+ANACONDA_USER=pytorch
+rm -rf faiss-src
+git clone https://github.com/facebookresearch/faiss faiss-src --recursive
+pushd faiss-src
+git checkout master
+popd
+
+conda config --set anaconda_upload no
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    export CUDA_VERSION="0.0"
+    export CUDNN_VERSION="0.0"
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 faiss-cpu
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 faiss-cpu
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 faiss-cpu
+else
+    . ./switch_cuda_version.sh 8.0
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 faiss-cpu
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 faiss-cpu
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 faiss-cpu
+
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 2.7 faiss-gpu
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.5 faiss-gpu
+    time conda build -c $ANACONDA_USER --no-anaconda-upload --python 3.6 faiss-gpu
+fi
+
+echo "All builds succeeded"
---
+++ conda/faiss-cpu/build.sh
@@ -0,0 +1,16 @@
+cp $RECIPE_DIR/makefile.inc .
+cp $RECIPE_DIR/setup.py .
+
+make tests/test_blas -j $(nproc)
+make -j $(nproc)
+make py
+
+sed -i "s/from swigfaiss/from .swigfaiss/g" faiss.py
+
+find $SP_DIR/torch -name "*.so*" -maxdepth 1 -type f | while read sofile; do
+    echo "Setting rpath of $sofile to " '$ORIGIN:$ORIGIN/../../..'
+    patchelf --set-rpath '$ORIGIN:$ORIGIN/../../..' $sofile
+    patchelf --print-rpath $sofile
+done
+
+python setup.py install
