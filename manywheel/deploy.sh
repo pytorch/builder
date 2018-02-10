2e80fa1628f9d3fb866d6b7c8f173591e001abd7
Soumith Chintala <soumith@gmail.com>
2018-02-10 14:38:58 -0800

    add new patchelf

--- manywheel/deploy.sh
+++ manywheel/deploy.sh
@@ -1,5 +1,5 @@
 set -e
-nvidia-docker build -t soumith/manylinux-cuda80 -f Dockerfile .
-nvidia-docker build -t soumith/manylinux-cuda75 -f Dockerfile_75 .
+nvidia-docker build -t soumith/manylinux-cuda80 -f Dockerfile_80 .
 nvidia-docker build -t soumith/manylinux-cuda90 -f Dockerfile_90 .
+nvidia-docker build -t soumith/manylinux-cuda91 -f Dockerfile_91 .
 #docker push soumith/manylinux-cuda
