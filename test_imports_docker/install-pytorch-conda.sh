9921c151b5082c06609882cc583648db23f3b3e7
Richard Zou <zou3519@gmail.com>
2018-04-23 10:06:11 -0600

    Update import docker scripts

--- test_imports_docker/install-pytorch-conda.sh
+++ test_imports_docker/install-pytorch-conda.sh
@@ -4,5 +4,5 @@ set -e
 export PATH="/opt/conda/bin:$PATH"
 export CMAKE_PREFIX_PATH="/opt/conda"
 
-conda install -y pytorch -c pytorch
+conda install -y pytorch-nightly -c pytorch
 python -c "import torch; print(torch.__version__)"
--- test_imports_docker/run.sh
+++ test_imports_docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
 
-docker stop img || true
+nvidia-docker stop img || true
 
 # If any of these fail, they'll leave behind a running docker container
 # named img.
@@ -9,23 +9,54 @@ docker stop img || true
 # `docker attach img`. The following is needed to debug inside container:
 #     export PATH="/opt/conda/bin:$PATH"
 
-# pytorch installs on cpu-only dockers
-./test-script.sh ubuntu:12.04 install-conda-ubuntu.sh install-pytorch-conda.sh
-./test-script.sh ubuntu:12.04 install-conda-ubuntu.sh install-pytorch-whl.sh
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-pytorch-conda.sh
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-pytorch-whl.sh
-./test-script.sh ubuntu:16.04 install-conda-ubuntu.sh install-pytorch-conda.sh
-./test-script.sh ubuntu:16.04 install-conda-ubuntu.sh install-pytorch-whl.sh
-./test-script.sh centos:6 install-conda-centos.sh install-pytorch-conda.sh
-./test-script.sh centos:6 install-conda-centos.sh install-pytorch-whl.sh
+# UNCOMMENT WHAT YOU NEED:
+
+# pytorch installs 
+#./test-script.sh nvidia/cuda:9.0-base install-conda-ubuntu.sh install-pytorch-conda.sh import-torch.sh
+#./test-script.sh nvidia/cuda:8.0-runtime-ubuntu14.04 install-conda-ubuntu.sh install-pytorch-conda.sh import-torch.sh
+
+# pytorch / tf
+#./test-script.sh nvidia/cuda:9.0-base install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-conda.sh import-pytorch-tf.sh
+
+# The following crashes:
+# ./test-script.sh nvidia/cuda:9.0-base install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-conda.sh import-tf-pytorch.sh
+
+# This is OK
+# ./test-script.sh nvidia/cuda:8.0-runtime-ubuntu14.04 install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-conda.sh import-pytorch-tf.sh
+
+# this also crashes:
+# ./test-script.sh nvidia/cuda:8.0-runtime-ubuntu14.04 install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-conda.sh import-tf-pytorch.sh
+
+# pytorch / scipy 
+# ./test-script.sh nvidia/cuda:9.0-base install-conda-ubuntu.sh install-scipy-conda.sh install-pytorch-conda.sh import-pytorch-scipy.sh
+#./test-script.sh nvidia/cuda:9.0-base install-conda-ubuntu.sh install-scipy-conda.sh install-pytorch-conda.sh import-scipy-pytorch.sh
+#./test-script.sh nvidia/cuda:8.0-runtime-ubuntu14.04 install-conda-ubuntu.sh install-scipy-conda.sh install-pytorch-conda.sh import-pytorch-scipy.sh
+#./test-script.sh nvidia/cuda:8.0-runtime-ubuntu14.04 install-conda-ubuntu.sh install-scipy-conda.sh install-pytorch-conda.sh import-scipy-pytorch.sh
+
+
+# pytorch / cv2 
+# ./test-script.sh nvidia/cuda:9.0-base install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-conda.sh import-pytorch-cv2.sh
+
+# The following crashes:
+# ./test-script.sh nvidia/cuda:9.0-base install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-conda.sh import-cv2-pytorch.sh
+
+# This is OK
+# ./test-script.sh nvidia/cuda:8.0-runtime-ubuntu14.04 install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-conda.sh import-pytorch-cv2.sh
+# ./test-script.sh nvidia/cuda:8.0-runtime-ubuntu14.04 install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-conda.sh import-cv2-pytorch.sh
+
+# Older scripts, that were used in 0.4 testing
+# ./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-pytorch-whl.sh
+# ./test-script.sh ubuntu:16.04 install-conda-ubuntu.sh install-pytorch-whl.sh
+#./test-script.sh centos:6 install-conda-centos.sh install-pytorch-conda.sh
+# ./test-script.sh centos:6 install-conda-centos.sh install-pytorch-whl.sh
 
 # pytorch / tf compatability (cpu-only)
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-conda.sh import-tf-pytorch.sh import-pytorch-tf.sh
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-whl.sh import-tf-pytorch.sh import-pytorch-tf.sh
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-tf-pip.sh install-pytorch-conda.sh import-tf-pytorch.sh import-pytorch-tf.sh
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-tf-pip.sh install-pytorch-whl.sh import-tf-pytorch.sh import-pytorch-tf.sh
+#./test-script.sh ubuntu:16.04 install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-conda.sh import-tf-pytorch.sh import-pytorch-tf.sh
+# ./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-tf-conda.sh install-pytorch-whl.sh import-tf-pytorch.sh import-pytorch-tf.sh
+#./test-script.sh ubuntu:16.04 install-conda-ubuntu.sh install-tf-pip.sh install-pytorch-conda.sh import-tf-pytorch.sh import-pytorch-tf.sh
+# ./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-tf-pip.sh install-pytorch-whl.sh import-tf-pytorch.sh import-pytorch-tf.sh
 
 # pytorch / cv2 (opencv3) compatibility (cpu-only)
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-whl.sh import-cv2-pytorch.sh import-pytorch-cv2.sh
-./test-script.sh ubuntu:14.04 install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-conda.sh import-cv2-pytorch.sh import-pytorch-cv2.sh
+# ./test-script.sh ubuntu:16.04 install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-whl.sh import-cv2-pytorch.sh import-pytorch-cv2.sh
+#./test-script.sh ubuntu:16.04 install-conda-ubuntu.sh install-cv2-conda.sh install-pytorch-conda.sh import-cv2-pytorch.sh import-pytorch-cv2.sh
 
