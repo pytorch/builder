970160f6f6efa63c8e60ed8aa8302639f2f11110
Soumith Chintala <soumith@gmail.com>
2018-05-03 20:10:12 -0700

    update dockerfiles to prune static libs and use nccl from nvidia downloads

--- manywheel/deploy.sh
+++ manywheel/deploy.sh
@@ -2,4 +2,6 @@ set -e
 nvidia-docker build -t soumith/manylinux-cuda80 -f Dockerfile_80 .
 nvidia-docker build -t soumith/manylinux-cuda90 -f Dockerfile_90 .
 nvidia-docker build -t soumith/manylinux-cuda91 -f Dockerfile_91 .
-#docker push soumith/manylinux-cuda
+nvidia-docker push soumith/manylinux-cuda80
+nvidia-docker push soumith/manylinux-cuda90
+nvidia-docker push soumith/manylinux-cuda91
