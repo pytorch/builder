0ff6ba792dee9f8493993a85253a7adc4ca53ecf
Ailing <ailzhang@fb.com>
2018-06-29 22:52:39 -0700

    faiss-gpu update

--- conda/faiss-gpu/build.sh
+++ conda/faiss-gpu/build.sh
@@ -1,16 +1,13 @@
 cp $RECIPE_DIR/makefile.inc .
 cp $RECIPE_DIR/setup.py .
 
-make tests/test_blas -j $(nproc)
-make -j $(nproc)
-make py
+make -j 10
+make -C gpu -j 10
+make -C python gpu
+make -C python
 
-cd gpu
-make -j $(nproc)
-make py
-cd ..
-
-sed -i "s/from swigfaiss/from .swigfaiss/g" faiss.py
+# not necessary
+# sed -i "s/from swigfaiss/from .swigfaiss/g" faiss.py
 
 find $SP_DIR/torch -name "*.so*" -maxdepth 1 -type f | while read sofile; do
     echo "Setting rpath of $sofile to " '$ORIGIN:$ORIGIN/../../..'
