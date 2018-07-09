ca3fa189957a84a1b58379fb64bfcc9095ac87b4
Ailing <ailzhang@fb.com>
2018-07-09 14:42:28 -0700

    fix issue

--- conda/faiss-gpu/build.sh
+++ conda/faiss-gpu/build.sh
@@ -5,7 +5,7 @@ MAJOR_VERSION=`python -c "import sys;print(sys.version_info[0])"`
 make clean
 make -j 10
 make -C gpu -j 10
-make -C python gpu
 make -C python
+make -C python gpu
 
 python setup.py install
