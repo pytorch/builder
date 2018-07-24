5ed237ddb17f78c35a228620913efc53cdcc62ca
Soumith Chintala <soumith@gmail.com>
2018-07-24 16:28:19 -0700

    add debugging helper scripts for docker image

---
+++ conda/debugging_pytorch.sh
@@ -0,0 +1,22 @@
+# builds inside docker image for debugging
+# also see setup_ccache.sh
+
+# copied from pytorch-0.4.1/build.sh
+export TORCH_CUDA_ARCH_LIST="3.5;5.0+PTX;6.0;6.1;7.0"
+export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
+export NCCL_ROOT_DIR=/usr/local/cuda
+export TH_BINARY_BUILD=1
+export USE_STATIC_CUDNN=1
+export USE_STATIC_NCCL=1
+export ATEN_STATIC_CUDA=1
+export USE_CUDA_STATIC_LINK=1
+
+. ./switch_cuda_version.sh 9.0
+
+
+conda install -y cmake numpy=1.11 setuptools pyyaml cffi mkl=2018 mkl-include typing ninja magma-cuda80 -c pytorch
+
+export CMAKE_PREFIX_PATH="$(dirname $(which conda))/../"
+git clone https://github.com/pytorch/pytorch -b nightly2 --recursive
+cd pytorch
+python setup.py install
---
+++ conda/setup_ccache.sh
@@ -0,0 +1,29 @@
+# sets up ccache inside the docker image (when debugging)
+if ! ls ~/ccache/bin/ccache
+then
+    yum install -y automake autoconf
+    yum install -y asciidoc
+    mkdir -p ~/ccache
+    pushd /tmp
+    rm -rf ccache
+    git clone https://github.com/colesbury/ccache -b ccbin
+    pushd ccache
+    ./autogen.sh
+    ./configure
+    make install prefix=~/ccache
+    popd
+    popd
+
+    mkdir -p ~/ccache/lib
+    mkdir -p ~/ccache/cuda
+    ln -s ~/ccache/bin/ccache ~/ccache/lib/cc
+    ln -s ~/ccache/bin/ccache ~/ccache/lib/c++
+    ln -s ~/ccache/bin/ccache ~/ccache/lib/gcc
+    ln -s ~/ccache/bin/ccache ~/ccache/lib/g++
+    ln -s ~/ccache/bin/ccache ~/ccache/cuda/nvcc
+
+    ~/ccache/bin/ccache -M 25Gi
+fi
+
+export PATH=~/ccache/lib:$PATH
+export CUDA_NVCC_EXECUTABLE=~/ccache/cuda/nvcc
