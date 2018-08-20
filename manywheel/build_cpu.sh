12ff293d0fb403a068d50097e33f77e55a37221d
Anders Papitto <anderspapitto@gmail.com>
2018-08-20 11:40:54 -0700

    dummy change to cpu builder

--- manywheel/build_cpu.sh
+++ manywheel/build_cpu.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+yum install -y zip
+
 if [[ -z "$PYTORCH_BUILD_VERSION" ]]; then
     export PYTORCH_BUILD_VERSION=0.4.1
 fi
@@ -17,23 +19,8 @@ WHEELHOUSE_DIR="wheelhousecpu"
 
 rm -rf /usr/local/cuda*
 
-# Build for given Python versions, or for all in /opt/python if none given
-if [[ -z "$DESIRED_PYTHON" ]]; then
-    pushd /opt/python
-    DESIRED_PYTHON=(*/)
-    popd
-fi
-python_installations=()
-for desired_py in "${DESIRED_PYTHON[@]}"; do
-    python_installations+=("/opt/python/$desired_py")
-    if [[ ! -d "/opt/python/$desired_py" ]]; then
-        echo "Error: Given Python $desired_py is not in /opt/python"
-        echo "All array elements of env variable DESIRED_PYTHON must be"
-        echo "valid Python installations under /opt/python"
-        exit 1
-    fi
-done
-echo "Will build for all Pythons versions: ${DESIRED_PYTHON[@]}"
+ANY_OLD_PYTHON="/opt/python/$(ls /opt/python | head -n1)"
+export PATH=$ANY_OLD_PYTHON/bin:$PATH
 
 # ########################################################
 # # Compile wheels
@@ -52,54 +39,27 @@ else
 fi
 git submodule update --init --recursive
 
-OLD_PATH=$PATH
-for PYDIR in "${python_installations[@]}"; do
-    export PATH=$PYDIR/bin:$OLD_PATH
-    python setup.py clean
-    pip install -r requirements.txt
-    if [[ $PYDIR  == "/opt/python/cp37-cp37m" ]]; then
-	      pip install numpy==1.15
-    else
-	      pip install numpy==1.11
-    fi
-    time python setup.py bdist_wheel -d $WHEELHOUSE_DIR
-done
+# compile
+pip install -r requirements.txt
+mkdir -p build
+pushd build
+python ../tools/build_libtorch.py
+find .
+popd
+
+mkdir libtorch
+cp -r build/lib libtorch/lib
+mkdir libtorch/include
+mkdir libtorch/bin
+
+mkdir -p $WHEELHOUSE_DIR
+zip -rq $WHEELHOUSE_DIR/torch-libtorch.whl libtorch
 
 popd
 
 #######################################################################
 # ADD DEPENDENCIES INTO THE WHEEL
-# 
-# auditwheel repair doesn't work correctly and is buggy
-# so manually do the work of copying dependency libs and patchelfing
-# and fixing RECORDS entries correctly
 ######################################################################
-yum install -y zip openssl
-
-fname_with_sha256() {
-    HASH=$(sha256sum $1 | cut -c1-8)
-    DIRNAME=$(dirname $1)
-    BASENAME=$(basename $1)
-    if [[ $BASENAME == "libnvrtc-builtins.so" ]]; then
-	      echo $1
-    else
-	      INITNAME=$(echo $BASENAME | cut -f1 -d".")
-	      ENDNAME=$(echo $BASENAME | cut -f 2- -d".")
-	      echo "$DIRNAME/$INITNAME-$HASH.$ENDNAME"
-    fi
-}
-
-make_wheel_record() {
-    FPATH=$1
-    if echo $FPATH | grep RECORD >/dev/null 2>&1; then
-	# if the RECORD file, then
-	echo "$FPATH,,"
-    else
-	HASH=$(openssl dgst -sha256 -binary $FPATH | openssl base64 | sed -e 's/+/-/g' | sed -e 's/\//_/g' | sed -e 's/=//g')
-	FSIZE=$(ls -nl $FPATH | awk '{print $5}')
-	echo "$FPATH,sha256=$HASH,$FSIZE"
-    fi
-}
 
 DEPS_LIST=(
     "/usr/lib64/libgomp.so.1"
@@ -114,129 +74,45 @@ cp $PYTORCH_DIR/$WHEELHOUSE_DIR/*.whl /$WHEELHOUSE_DIR
 mkdir /tmp_dir
 pushd /tmp_dir
 
-for whl in /$WHEELHOUSE_DIR/torch*linux*.whl; do
+for whl in /$WHEELHOUSE_DIR/*.whl; do
     rm -rf tmp
     mkdir -p tmp
-    cd tmp
+    pushd tmp
     cp $whl .
 
     unzip -q $(basename $whl)
     rm -f $(basename $whl)
-    
-    # copy over needed dependent .so files over and tag them with their hash
-    patched=()
+
+    pushd libtorch
+    # copy over needed dependent .so files
     for filepath in "${DEPS_LIST[@]}"
     do
 	filename=$(basename $filepath)
-	destpath=torch/lib/$filename
+	destpath=lib/$filename
 	if [[ "$filepath" != "$destpath" ]]; then
 	    cp $filepath $destpath
 	fi
-
-	patchedpath=$(fname_with_sha256 $destpath)
-	patchedname=$(basename $patchedpath)
-	if [[ "$destpath" != "$patchedpath" ]]; then
-	    mv $destpath $patchedpath
-	fi
-	patched+=("$patchedname")
-	echo "Copied $filepath to $patchedpath"
     done
 
-    echo "patching to fix the so names to the hashed names"
-    for ((i=0;i<${#DEPS_LIST[@]};++i));
-    do
-	find torch -name '*.so*' | while read sofile; do
-	    origname=${DEPS_SONAME[i]}
-	    patchedname=${patched[i]}
-	    if [[ "$origname" != "$patchedname" ]]; then
-		set +e
-		patchelf --print-needed $sofile | grep $origname 2>&1 >/dev/null
-		ERRCODE=$?
-		set -e
-		if [ "$ERRCODE" -eq "0" ]; then
-		    echo "patching $sofile entry $origname to $patchedname"
-		    patchelf --replace-needed $origname $patchedname $sofile
-		fi
-	    fi
-	done
-    done
-    
-    # set RPATH of _C.so and similar to $ORIGIN, $ORIGIN/lib
-    find torch -maxdepth 1 -type f -name "*.so*" | while read sofile; do
-	echo "Setting rpath of $sofile to " '$ORIGIN:$ORIGIN/lib'
-	patchelf --set-rpath '$ORIGIN:$ORIGIN/lib' $sofile
-	patchelf --print-rpath $sofile
-    done
-    
     # set RPATH of lib/ files to $ORIGIN
-    find torch/lib -maxdepth 1 -type f -name "*.so*" | while read sofile; do
+    find lib -maxdepth 1 -type f -name "*.so*" | while read sofile; do
 	echo "Setting rpath of $sofile to " '$ORIGIN'
 	patchelf --set-rpath '$ORIGIN' $sofile
 	patchelf --print-rpath $sofile
     done
 
+    popd
 
-    # regenerate the RECORD file with new hashes 
-    record_file=`echo $(basename $whl) | sed -e 's/-cp.*$/.dist-info\/RECORD/g'`
-    echo "Generating new record file $record_file"
-    rm -f $record_file
-    # generate records for torch folder
-    find torch -type f | while read fname; do
-	echo $(make_wheel_record $fname) >>$record_file
-    done
-    # generate records for torch-[version]-dist-info folder
-    find torch*dist-info -type f | while read fname; do
-	echo $(make_wheel_record $fname) >>$record_file
-    done
-
-    # zip up the wheel back
-    zip -rq $(basename $whl) torch*
+    # zip the wheel back up
+    zip -rq $(basename $whl) build
 
     # replace original wheel
     rm -f $whl
     mv $(basename $whl) $whl
-    cd ..
+    popd
     rm -rf tmp
 done
 
-# Take the actual package name. Note how this always works because pip converts
-# - to _ in names.
-pushd /$WHEELHOUSE_DIR
-built_wheels=(torch*.whl)
-IFS='-' read -r package_name some_unused_variable <<< "${built_wheels[0]}"
-echo "Expecting the built wheels to all be called '$package_name'"
-popd
-
 # Copy wheels to host machine for persistence after the docker
 mkdir -p /remote/$WHEELHOUSE_DIR
-cp /$WHEELHOUSE_DIR/torch*.whl /remote/$WHEELHOUSE_DIR/
-
-# remove stuff before testing
-rm -rf /opt/rh
-
-
-# Test that all the wheels work
-export OMP_NUM_THREADS=4 # on NUMA machines this takes too long
-pushd $PYTORCH_DIR/test
-for (( i=0; i<"${#DESIRED_PYTHON[@]}"; i++ )); do
-    # This assumes that there is a 1:1 correspondence between python versions
-    # and wheels, and that the python version is in the name of the wheel,
-    # and that the python version matches the regex "cp\d\d-cp\d\dmu?"
-    pydir="${python_installations[i]}"
-    pyver="${DESIRED_PYTHON[i]}"
-    pyver_short="${pyver:2:1}.${pyver:3:1}"
-
-    # Install the wheel for this Python version
-    "${PYDIR}/bin/pip" uninstall -y "$package_name"
-    "${PYDIR}/bin/pip" install "$package_name" --no-index -f /$WHEELHOUSE_DIR --no-dependencies
-
-    # Print info on the libraries installed in this wheel
-    installed_libraries=($(find "$pydir/lib/python$pyver_short/site-packages/torch/" -name '*.so*'))
-    echo "The wheel installed all of the libraries: ${installed_libraries[@]}"
-    for installed_lib in "${installed_libraries[@]}"; do
-        ldd "$installed_lib"
-    done
-
-    # Test that the wheel works
-    LD_LIBRARY_PATH="/usr/local/nvidia/lib64" PYCMD=$PYDIR/bin/python $PYDIR/bin/python run_test.py
-done
+cp /$WHEELHOUSE_DIR/*.whl /remote/$WHEELHOUSE_DIR/
