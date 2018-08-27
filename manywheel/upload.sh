a804cd698891c19c49bc9944cac87c20f1152631
pjh5 <hellemn@fb.com>
2018-08-27 10:55:08 -0700

    Fixing bug in upload.sh

--- manywheel/upload.sh
+++ manywheel/upload.sh
@@ -6,22 +6,24 @@ if [[ -z "$CUDA_VERSIONS" ]]; then
 fi
 
 for cuda_ver in "${CUDA_VERSIONS[@]}"; do
-    wheel_dir="wheelhouse${cuda_ver}/"
     s3_dir="s3://pytorch/whl/${PIP_UPLOAD_FOLDER}${cuda_ver}/"
-
-    if [[ -d "$wheel_dir" ]]; then
-        # Upload the wheels to s3
-        echo "Uploading all of: $(ls $wheel_dir) to $s3_dir"
-        ls "$wheel_dir" | xargs -I {} aws s3 cp "$wheel_dir"/{} "$s3_dir" --acl public-read
-
-        # Update the html links file in the s3 bucket
-        # Pip uses this html file to look through all the wheels and pick the
-        # most recently uploaded one (by the version, not the actual date of
-        # upload). There is one html file per cuda/cpu version
-        aws s3 ls "$s3_dir" | grep --only-matching '\S*\.whl' | sed 's#.*#<a href="&">&</a>#g' > ./torch_nightly.html
-        echo 'Setting torch_nightly.html to:'
-        cat ./torch_nightly.html
-        # Note the lack of a / b/c duplicate / do cause problems in s3
-        aws s3 cp './torch_nightly.html' "${s3_dir}torch_nightly.html"  --acl public-read
+    if [[ "$cuda_ver" == cpu ]]; then
+        wheel_dir="wheelhousecpu/"
+    else
+        wheel_dir="wheelhouse${cuda_ver:2:2}/"
     fi
+
+    # Upload the wheels to s3
+    echo "Uploading all of: $(ls $wheel_dir) to $s3_dir"
+    ls "$wheel_dir" | xargs -I {} aws s3 cp "$wheel_dir"/{} "$s3_dir" --acl public-read
+
+    # Update the html links file in the s3 bucket
+    # Pip uses this html file to look through all the wheels and pick the
+    # most recently uploaded one (by the version, not the actual date of
+    # upload). There is one html file per cuda/cpu version
+    aws s3 ls "$s3_dir" | grep --only-matching '\S*\.whl' | sed 's#.*#<a href="&">&</a>#g' > ./torch_nightly.html
+    echo 'Setting torch_nightly.html to:'
+    cat ./torch_nightly.html
+    # Note the lack of a / b/c duplicate / do cause problems in s3
+    aws s3 cp './torch_nightly.html' "${s3_dir}torch_nightly.html"  --acl public-read
 done
