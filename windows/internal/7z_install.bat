64880fdcefb4233e0946c7ffb27a123fe39980f3
peter <peter_jiachen@163.com>
2018-11-08 19:45:11 +0800

    .

--- windows/internal/7z_install.bat
+++ windows/internal/7z_install.bat
@@ -1,5 +1,9 @@
 @echo off
 
-set "PATH=%CONDA%\Scripts;%PATH%"
-conda install -y --no-deps -c peterjc123 7z
+curl -k https://www.7-zip.org/a/7z1805-x64.exe -O
 if errorlevel 1 exit /b 1
+
+start /wait 7z1805-x64.exe /S
+if errorlevel 1 exit /b 1
+
+set "PATH=%ProgramFiles%\7-Zip;%PATH%"
