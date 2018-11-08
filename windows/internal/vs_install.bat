45c35395556e6406baaf51e42cc72cc677ea3c48
peter <peter_jiachen@163.com>
2018-11-08 17:36:59 +0800

    .

--- windows/internal/vs_install.bat
+++ windows/internal/vs_install.bat
@@ -1,6 +1,12 @@
 @echo off
 
 curl -k https://aka.ms/vs/15/release/vs_buildtools.exe --output vs_BuildTools.exe
-start /wait .\vs_buildtools.exe --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools
+echo https://aka.ms/vs/15/release/channel > VisualStudio.chman
+mkdir vs2017
+start /wait .\vs_buildtools.exe --nocache --norestart --quiet --wait
+        --add Microsoft.VisualStudio.Workload.VCTools `
+        --installPath "%CD%\vs2017" `
+        --channelUri "%CD%\VisualStudio.chman" `
+        --installChannelUri "%CD%\VisualStudio.chman"
 :: --add Microsoft.VisualStudio.Component.VC.Tools.14.11
 if errorlevel 1 exit /b 1
