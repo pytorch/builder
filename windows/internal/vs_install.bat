416d8a90df209650dbfad9d6224b7f484ae93672
peter <peter_jiachen@163.com>
2018-11-08 15:54:27 +0800

    .

--- windows/internal/vs_install.bat
+++ windows/internal/vs_install.bat
@@ -1,5 +1,6 @@
 @echo off
 
-curl -k https://www.dropbox.com/s/spq7vtfb6uxgo0m/vs_BuildTools.exe?dl=1 --output vs_BuildTools.exe
-start /wait .\vs_buildtools.exe --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools --add Microsoft.VisualStudio.Component.VC.Tools.14.11
+curl -k https://aka.ms/vs/15/release/vs_buildtools.exe --output vs_BuildTools.exe
+start /wait .\vs_buildtools.exe --quiet --wait --add Microsoft.VisualStudio.Workload.VCTools
+:: --add Microsoft.VisualStudio.Component.VC.Tools.14.11
 if errorlevel 1 exit /b 1
