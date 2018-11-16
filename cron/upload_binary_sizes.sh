5fefe58139c204c6ffdb20befb11799be161813b
pjh5 <hellemn@fb.com>
2018-11-16 15:41:07 -0800

    Backfill of binary sizes

--- cron/upload_binary_sizes.sh
+++ cron/upload_binary_sizes.sh
@@ -3,15 +3,26 @@
 set -ex
 echo "collect_binary_sizes.sh at $(pwd) starting at $(date) on $(uname -a) with pid $$"
 SOURCE_DIR=$(cd $(dirname $0) && pwd)
-source "${SOURCE_DIR}/nightly_defaults.sh"
+# N.B. we do not source nightly_defaults.sh to avoid cloning repos every date
+# of a backfill
 
 # Usage:
-#   collect_binary_sizes.sh
+#   collect_binary_sizes.sh [date]
 # Queries s3 and conda to get the binary sizes (as they're stored in the cloud)
 # for a day
 
-binary_sizes_log="$today/binary_sizes.log"
-binary_sizes_json="$today/$NIGHTLIES_DATE.json"
+# Optionally accept a date to upload for
+if [[ "$#" > 0 ]]; then
+    target_date=$1
+    target_version="1.0.0.dev$(echo $target_date | tr -d _)"
+else
+    source "${SOURCE_DIR}/nightly_defaults.sh"
+    target_date="$NIGHTLIES_DATE"
+    target_version="$PYTORCH_BUILD_VERSION"
+fi
+
+binary_sizes_log="$SOURCE_DIR/binary_sizes.log"
+binary_sizes_json="$SOURCE_DIR/$target_date.json"
 rm -f "$binary_sizes_log"
 rm -f "$binary_sizes_json"
 touch "$binary_sizes_log"
@@ -22,14 +33,15 @@ touch "$binary_sizes_log"
 # This is read from `conda search`. 
 conda_platforms=('linux-64' 'osx-64')
 conda_pkg_names=('pytorch-nightly' 'pytorch-nightly-cpu')
-tmp_json="$today/conda_search.json"
+tmp_json="$_conda_search.json"
 for pkg_name in "${conda_pkg_names[@]}"; do
     for platform in "${conda_platforms[@]}"; do
 
         # Read the info from conda-search
+        touch "$tmp_json"
         set +e
         conda search -c pytorch --json --platform "$platform" \
-                "$pkg_name==$PYTORCH_BUILD_VERSION" > "$tmp_json"
+                "$pkg_name==$target_version" > "$tmp_json"
         if [[ "$?" != 0 ]]; then
             set -e
             echo "ERROR: Could not query conda for $platform"
@@ -38,7 +50,7 @@ for pkg_name in "${conda_pkg_names[@]}"; do
         set -e
 
         # Call Python to parse the json into 'log_name_form size_in_bytes'
-        python "$NIGHTLIES_BUILDER_ROOT/cron/parse_conda_json.py" "$tmp_json" "$binary_sizes_log"
+        python "$SOURCE_DIR/parse_conda_json.py" "$tmp_json" "$binary_sizes_log"
     done
 done
 rm -f "$tmp_json"
@@ -51,7 +63,14 @@ for cu_ver in "${cuda_versions[@]}"; do
 
     # Read the info from s3
     s3_dir="s3://pytorch/whl/nightly/${cu_ver}/"
-    outputs=($(aws s3 ls "$s3_dir" | grep --only-matching "\S* \S*$PYTORCH_BUILD_VERSION\S*\.whl"))
+    set +e
+    outputs=($(aws s3 ls "$s3_dir" | grep --only-matching "\S* \S*$target_version\S*\.whl"))
+    if [[ "$?" != 0 ]]; then
+        set -e
+        echo "ERROR: Could find no [many]wheels for $cu_ver"
+        continue
+    fi
+    set -e
 
     # outputs is now a list of [size whl size whl...] as different elements
     for i in $(seq 0 2 $(( ${#outputs[@]} - 1 )) ); do
@@ -82,7 +101,7 @@ done
 
 # Convert the file of '<platform> <log_name> <size>' into a json for easy
 # ingestion in the react HUD
-python "$NIGHTLIES_BUILDER_ROOT/cron/write_json.py" "$binary_sizes_log" "$binary_sizes_json"
+python "$SOURCE_DIR/write_json.py" "$binary_sizes_log" "$binary_sizes_json"
 
 # Upload the log to s3
-aws s3 cp "$binary_sizes_json" "s3://pytorch/$BINARY_SIZES_S3_DIR/" --acl public-read --quiet
+aws s3 cp "$binary_sizes_json" "s3://pytorch/nightly_logs/binary_sizes/" --acl public-read --quiet
