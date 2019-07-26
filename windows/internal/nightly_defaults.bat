49918d582a3772fbd5807d77e9dad8a069d86e3c
peter <peter_jiachen@163.com>
2019-07-26 22:08:20 +0800

    Fix build version for Windows

--- windows/internal/nightly_defaults.bat
+++ windows/internal/nightly_defaults.bat
@@ -122,12 +122,18 @@ popd
 
 :clone_end
 
+if "%CUDA_VERSION%" == "cpu" (
+    set _DESIRED_CUDA=cpu
+) else (
+    set _DESIRED_CUDA=cu%CUDA_VERSION%
+)
+
 :: PYTORCH_BUILD_VERSION
 ::   The actual version string. Used in conda like
 ::       pytorch-nightly==1.0.0.dev20180908
 ::   or in manylinux like
 ::       torch_nightly-1.0.0.dev20180908-cp27-cp27m-linux_x86_64.whl
-if "%PYTORCH_BUILD_VERSION%" == "" set PYTORCH_BUILD_VERSION=1.2.0.dev%NIGHTLIES_DATE_COMPACT%
+if "%PYTORCH_BUILD_VERSION%" == "" set PYTORCH_BUILD_VERSION=1.2.0.dev%NIGHTLIES_DATE_COMPACT%+%_DESIRED_CUDA%
 
 :: PYTORCH_BUILD_NUMBER
 ::   This is usually the number 1. If more than one build is uploaded for the
