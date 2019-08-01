6f8b92ba3dd1ce0abf2b06e635de5c5bb7e49561
Edward Z. Yang <ezyang@fb.com>
2019-08-01 12:21:13 -0400

    Create top-level and subdirectory S3 htmls.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- cron/update_s3_htmls.sh
+++ cron/update_s3_htmls.sh
@@ -16,23 +16,44 @@ if [[ -z "$HTML_NAME" ]]; then
     export HTML_NAME='torch_nightly.html'
 fi
 
-for cuda_ver in "${CUDA_VERSIONS[@]}"; do
-    s3_dir="s3://pytorch/whl/${PIP_UPLOAD_FOLDER}${cuda_ver}/"
+# NB: includes trailing slash (from PIP_UPLOAD_FOLDER)
+s3_base="s3://pytorch/whl/${PIP_UPLOAD_FOLDER}"
 
-    # Pull all existing whls in this directory and turn them into html links
-    # N.B. we use the .dev as a hacky way to exclude all wheels with old
-    # 'yyyy.mm.dd' versions
-    #
-    # NB: replacing + with %2B is to fix old versions of pip which don't
-    # this transform automatically.  This makes the display a little
-    # ugly but whatever
-    aws s3 ls "$s3_dir" | grep --only-matching '\S*\.whl' | sed 's#+#%2B#g' | sed 's#.*#<a href="&">&</a><br>#g' > ./$HTML_NAME
+# Pull all existing whls in this directory and turn them into html links
+# N.B. we use the .dev as a hacky way to exclude all wheels with old
+# 'yyyy.mm.dd' versions
+#
+# NB: replacing + with %2B is to fix old versions of pip which don't
+# this transform automatically.  This makes the display a little
+# ugly but whatever
+function generate_html() {
+  # Trailing slash required in both cases
+  dir="$1"
+  url_prefix="$2"
+  aws s3 ls "${s3_base}${dir}" | grep --only-matching '\S*\.whl' | sed 's#+#%2B#g' | sed 's#.*#<a href="'"${url_prefix}"'&">'"${url_prefix}"'&</a><br>#g'
+}
+
+# This will be included in all the sub-indices
+generate_html '' '../' > "root-$HTML_NAME"
+generate_html '' '' > "$HTML_NAME"
+
+for cuda_ver in "${CUDA_VERSIONS[@]}"; do
+    generate_html "${cuda_ver}/" "" > "${cuda_ver}-$HTML_NAME"
+    cat "root-$HTML_NAME" >> "${cuda_ver}-$HTML_NAME"
+    generate_html "${cuda_ver}/" "${cuda_ver}/" >> "$HTML_NAME"
 
     # Check your work every once in a while
-    echo "Setting $HTML_NAME to:"
-    cat ./$HTML_NAME
+    echo "Setting ${cuda_ver}/$HTML_NAME to:"
+    cat "${cuda_ver}-$HTML_NAME"
 
-    # Upload the html file back up
-    # Note the lack of a / b/c duplicate / do cause problems in s3
-    aws s3 cp ./$HTML_NAME "${s3_dir}$HTML_NAME"  --acl public-read --cache-control 'no-cache,no-store,must-revalidate'
+    aws s3 cp "${cuda_ver}-$HTML_NAME" "s3://pytorch/whl/${PIP_UPLOAD_FOLDER}${cuda_ver}/$HTML_NAME"  --acl public-read --cache-control 'no-cache,no-store,must-revalidate'
+
 done
+
+# Check your work every once in a while
+echo "Setting $HTML_NAME to:"
+cat "$HTML_NAME"
+
+# Upload the html file back up
+# Note the lack of a / b/c duplicate / do cause problems in s3
+aws s3 cp "$HTML_NAME" "$s3_base$HTML_NAME"  --acl public-read --cache-control 'no-cache,no-store,must-revalidate'
