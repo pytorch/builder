064279252adf86ec0397a799289b00044e3a15f2
Edward Z. Yang <ezyang@fb.com>
2019-08-01 10:14:07 -0400

    Revert "Generate index html for top-level nightly folder only."
    This reverts commit 5287298daa4143634c05db1ad97d5cfd1107ef5c.

--- cron/update_s3_htmls.sh
+++ cron/update_s3_htmls.sh
@@ -7,25 +7,32 @@ set -ex
 # version, not the actual date of upload). There is one html file per cuda/cpu
 # version
 
+# Upload for all CUDA/cpu versions if not given one to use
+if [[ -z "$CUDA_VERSIONS" ]]; then
+    export CUDA_VERSIONS=('cpu' 'cu92' 'cu100')
+fi
+
 if [[ -z "$HTML_NAME" ]]; then
     export HTML_NAME='torch_nightly.html'
 fi
 
-s3_dir="s3://pytorch/whl/${PIP_UPLOAD_FOLDER}"
+for cuda_ver in "${CUDA_VERSIONS[@]}"; do
+    s3_dir="s3://pytorch/whl/${PIP_UPLOAD_FOLDER}${cuda_ver}/"
 
-# Pull all existing whls in this directory and turn them into html links
-# N.B. we use the .dev as a hacky way to exclude all wheels with old
-# 'yyyy.mm.dd' versions
-#
-# NB: replacing + with %2B is to fix old versions of pip which don't
-# this transform automatically.  This makes the display a little
-# ugly but whatever
-aws s3 ls "$s3_dir" | grep --only-matching '\S*\.whl' | sed 's#+#%2B#g' | sed 's#.*#<a href="&">&</a><br>#g' > ./$HTML_NAME
+    # Pull all existing whls in this directory and turn them into html links
+    # N.B. we use the .dev as a hacky way to exclude all wheels with old
+    # 'yyyy.mm.dd' versions
+    #
+    # NB: replacing + with %2B is to fix old versions of pip which don't
+    # this transform automatically.  This makes the display a little
+    # ugly but whatever
+    aws s3 ls "$s3_dir" | grep --only-matching '\S*\.whl' | sed 's#+#%2B#g' | sed 's#.*#<a href="&">&</a><br>#g' > ./$HTML_NAME
 
-# Check your work every once in a while
-echo "Setting $HTML_NAME to:"
-cat ./$HTML_NAME
+    # Check your work every once in a while
+    echo "Setting $HTML_NAME to:"
+    cat ./$HTML_NAME
 
-# Upload the html file back up
-# Note the lack of a / b/c duplicate / do cause problems in s3
-aws s3 cp ./$HTML_NAME "${s3_dir}$HTML_NAME"  --acl public-read --cache-control 'no-cache,no-store,must-revalidate'
+    # Upload the html file back up
+    # Note the lack of a / b/c duplicate / do cause problems in s3
+    aws s3 cp ./$HTML_NAME "${s3_dir}$HTML_NAME"  --acl public-read --cache-control 'no-cache,no-store,must-revalidate'
+done
