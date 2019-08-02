7e502f7e86cf8a15a1e29bab6182f8d0f9251f22
Soumith Chintala <soumith@fb.com>
2019-08-02 16:28:42 -0700

    upgrade cudnn, add cuda 10.1 docker

--- manywheel/deploy.sh
+++ manywheel/deploy.sh
@@ -1,5 +1,7 @@
 set -e
 nvidia-docker build -t soumith/manylinux-cuda92 -f Dockerfile_92 .
 nvidia-docker build -t soumith/manylinux-cuda100 -f Dockerfile_100 .
+nvidia-docker build -t soumith/manylinux-cuda101 -f Dockerfile_101 .
 nvidia-docker push soumith/manylinux-cuda92
 nvidia-docker push soumith/manylinux-cuda100
+nvidia-docker push soumith/manylinux-cuda101
