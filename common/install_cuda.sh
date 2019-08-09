dfd3de8a4e29688a903f8cb3acb81aeef7ffaba5
Will Feng <willfeng@fb.com>
2019-08-09 14:03:03 -0400

    break into multiple lines

--- common/install_cuda.sh
+++ common/install_cuda.sh
@@ -3,87 +3,87 @@
 set -ex
 
 # install CUDA 9.2 in the same container
-wget -q https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux -O setup && \
-chmod +x setup && \
-./setup --silent --no-opengl-libs --toolkit && \
+wget -q https://developer.nvidia.com/compute/cuda/9.2/Prod2/local_installers/cuda_9.2.148_396.37_linux -O setup
+chmod +x setup
+./setup --silent --no-opengl-libs --toolkit
 rm -f setup
 
 # patch 1
-wget -q https://developer.nvidia.com/compute/cuda/9.2/Prod2/patches/1/cuda_9.2.148.1_linux -O setup && \
-chmod +x setup && \
-./setup -s --accept-eula && \
+wget -q https://developer.nvidia.com/compute/cuda/9.2/Prod2/patches/1/cuda_9.2.148.1_linux -O setup
+chmod +x setup
+./setup -s --accept-eula
 rm -f setup
 
 # install CUDA 9.2 CuDNN
 # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
-mkdir tmp_cudnn && cd tmp_cudnn && \
-wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.2.24-1+cuda9.2_amd64.deb -O cudnn-dev.deb&& \
-wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.2.24-1+cuda9.2_amd64.deb -O cudnn.deb && \
-ar -x cudnn-dev.deb && tar -xvf data.tar.xz && \
-ar -x cudnn.deb && tar -xvf data.tar.xz && \
-mkdir -p cuda/include && mkdir -p cuda/lib64 && \
-cp -a usr/include/x86_64-linux-gnu/cudnn_v7.h cuda/include/cudnn.h && \
-cp -a usr/lib/x86_64-linux-gnu/libcudnn* cuda/lib64 && \
-mv cuda/lib64/libcudnn_static_v7.a cuda/lib64/libcudnn_static.a && \
-ln -s libcudnn.so.7 cuda/lib64/libcudnn.so && \
-chmod +x cuda/lib64/*.so && \
-cp -a cuda/include/* /usr/local/cuda/include/ && \
-cp -a cuda/lib64/* /usr/local/cuda/lib64/ && \
-cd .. && \
-rm -rf tmp_cudnn && \
+mkdir tmp_cudnn && cd tmp_cudnn
+wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.2.24-1+cuda9.2_amd64.deb -O cudnn-dev.deb
+wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.2.24-1+cuda9.2_amd64.deb -O cudnn.deb
+ar -x cudnn-dev.deb && tar -xvf data.tar.xz
+ar -x cudnn.deb && tar -xvf data.tar.xz
+mkdir -p cuda/include && mkdir -p cuda/lib64
+cp -a usr/include/x86_64-linux-gnu/cudnn_v7.h cuda/include/cudnn.h
+cp -a usr/lib/x86_64-linux-gnu/libcudnn* cuda/lib64
+mv cuda/lib64/libcudnn_static_v7.a cuda/lib64/libcudnn_static.a
+ln -s libcudnn.so.7 cuda/lib64/libcudnn.so
+chmod +x cuda/lib64/*.so
+cp -a cuda/include/* /usr/local/cuda/include/
+cp -a cuda/lib64/* /usr/local/cuda/lib64/
+cd ..
+rm -rf tmp_cudnn
 ldconfig
 
 # install CUDA 10.0 in the same container
-wget -q https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux && \
-chmod +x cuda_10.0.130_410.48_linux && \
-./cuda_10.0.130_410.48_linux --silent --no-opengl-libs --toolkit && \
+wget -q https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux
+chmod +x cuda_10.0.130_410.48_linux
+./cuda_10.0.130_410.48_linux --silent --no-opengl-libs --toolkit
 rm -f cuda_10.0.130_410.48_linux
 
 # install CUDA 10.0 CuDNN
 # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
-mkdir tmp_cudnn && cd tmp_cudnn && \
-wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.2.24-1+cuda10.0_amd64.deb -O cudnn-dev.deb&& \
-wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.2.24-1+cuda10.0_amd64.deb -O cudnn.deb && \
-ar -x cudnn-dev.deb && tar -xvf data.tar.xz && \
-ar -x cudnn.deb && tar -xvf data.tar.xz && \
-mkdir -p cuda/include && mkdir -p cuda/lib64 && \
-cp -a usr/include/x86_64-linux-gnu/cudnn_v7.h cuda/include/cudnn.h && \
-cp -a usr/lib/x86_64-linux-gnu/libcudnn* cuda/lib64 && \
-mv cuda/lib64/libcudnn_static_v7.a cuda/lib64/libcudnn_static.a && \
-ln -s libcudnn.so.7 cuda/lib64/libcudnn.so && \
-chmod +x cuda/lib64/*.so && \
-cp -a cuda/include/* /usr/local/cuda/include/ && \
-cp -a cuda/lib64/* /usr/local/cuda/lib64/ && \
-cd .. && \
-rm -rf tmp_cudnn && \
+mkdir tmp_cudnn && cd tmp_cudnn
+wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.2.24-1+cuda10.0_amd64.deb -O cudnn-dev.deb
+wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.2.24-1+cuda10.0_amd64.deb -O cudnn.deb
+ar -x cudnn-dev.deb && tar -xvf data.tar.xz
+ar -x cudnn.deb && tar -xvf data.tar.xz
+mkdir -p cuda/include && mkdir -p cuda/lib64
+cp -a usr/include/x86_64-linux-gnu/cudnn_v7.h cuda/include/cudnn.h
+cp -a usr/lib/x86_64-linux-gnu/libcudnn* cuda/lib64
+mv cuda/lib64/libcudnn_static_v7.a cuda/lib64/libcudnn_static.a
+ln -s libcudnn.so.7 cuda/lib64/libcudnn.so
+chmod +x cuda/lib64/*.so
+cp -a cuda/include/* /usr/local/cuda/include/
+cp -a cuda/lib64/* /usr/local/cuda/lib64/
+cd ..
+rm -rf tmp_cudnn
 ldconfig
 
 # # install CUDA 10.1 in the same container
-wget -q https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.168_418.67_linux.    && \
-chmod +x cuda_10.1.168_418.67_linux.    && \
-./cuda_10.1.168_418.67_linux.    --extract=/tmp/cuda && \
-rm -f cuda_10.1.168_418.67_linux.    && \
-mv /tmp/cuda/cuda-toolkit /usr/local/cuda-10.1 && \
-rm -rf /tmp/cuda && \
+wget -q https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.168_418.67_linux.run
+chmod +x cuda_10.1.168_418.67_linux.run
+./cuda_10.1.168_418.67_linux.run    --extract=/tmp/cuda
+rm -f cuda_10.1.168_418.67_linux.run
+mv /tmp/cuda/cuda-toolkit /usr/local/cuda-10.1
+rm -rf /tmp/cuda
 rm -f /usr/local/cuda && ln -s /usr/local/cuda-10.1 /usr/local/cuda
 
 # install CUDA 10.1 CuDNN
 # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
-mkdir tmp_cudnn && cd tmp_cudnn && \
-wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.2.24-1+cuda10.1_amd64.deb -O cudnn-dev.deb&& \
-wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.2.24-1+cuda10.1_amd64.deb -O cudnn.deb && \
-ar -x cudnn-dev.deb && tar -xvf data.tar.xz && \
-ar -x cudnn.deb && tar -xvf data.tar.xz && \
-mkdir -p cuda/include && mkdir -p cuda/lib64 && \
-cp -a usr/include/x86_64-linux-gnu/cudnn_v7.h cuda/include/cudnn.h && \
-cp -a usr/lib/x86_64-linux-gnu/libcudnn* cuda/lib64 && \
-mv cuda/lib64/libcudnn_static_v7.a cuda/lib64/libcudnn_static.a && \
-ln -s libcudnn.so.7 cuda/lib64/libcudnn.so && \
-chmod +x cuda/lib64/*.so && \
-cp -a cuda/include/* /usr/local/cuda/include/ && \
-cp -a cuda/lib64/* /usr/local/cuda/lib64/ && \
-cd .. && \
-rm -rf tmp_cudnn && \
+mkdir tmp_cudnn && cd tmp_cudnn
+wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7-dev_7.6.2.24-1+cuda10.1_amd64.deb -O cudnn-dev.deb
+wget -q http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1604/x86_64/libcudnn7_7.6.2.24-1+cuda10.1_amd64.deb -O cudnn.deb
+ar -x cudnn-dev.deb && tar -xvf data.tar.xz
+ar -x cudnn.deb && tar -xvf data.tar.xz
+mkdir -p cuda/include && mkdir -p cuda/lib64
+cp -a usr/include/x86_64-linux-gnu/cudnn_v7.h cuda/include/cudnn.h
+cp -a usr/lib/x86_64-linux-gnu/libcudnn* cuda/lib64
+mv cuda/lib64/libcudnn_static_v7.a cuda/lib64/libcudnn_static.a
+ln -s libcudnn.so.7 cuda/lib64/libcudnn.so
+chmod +x cuda/lib64/*.so
+cp -a cuda/include/* /usr/local/cuda/include/
+cp -a cuda/lib64/* /usr/local/cuda/lib64/
+cd ..
+rm -rf tmp_cudnn
 ldconfig
 
 #####################################################################################
--- common/install_patchelf.sh
+++ common/install_patchelf.sh
@@ -2,12 +2,12 @@
 
 set -ex
 
-git clone https://github.com/NixOS/patchelf && \
-cd patchelf && \
-sed -i 's/serial/parallel/g' configure.ac && \
-./bootstrap.sh && \
-./configure && \
-make && \
-make install && \
-cd .. && \
+git clone https://github.com/NixOS/patchelf
+cd patchelf
+sed -i 's/serial/parallel/g' configure.ac
+./bootstrap.sh
+./configure
+make
+make install
+cd ..
 rm -rf patchelf
