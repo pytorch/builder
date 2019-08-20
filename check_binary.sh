f603bdc4c66e817c4b1f6c3f463495d00560e04b
Will Feng <willfeng@fb.com>
2019-08-20 17:10:59 -0400

    fix error message

--- check_binary.sh
+++ check_binary.sh
@@ -225,7 +225,7 @@ if [[ "$PACKAGE_TYPE" == 'libtorch' ]]; then
 #include <torch/torch.h>
 
 int main(int argc, const char* argv[]) {
-  TORCH_CHECK(true, "Simple test passed!");
+  TORCH_CHECK(true, "Simple test failed!");
   return 0;
 }
 EOL
@@ -240,11 +240,12 @@ fi
 # Check for MKL
 ###############################################################################
 if [[ "$PACKAGE_TYPE" == 'libtorch' ]]; then
+  echo "Checking that MKL is available"
   cat >example-app.cpp <<EOL
 #include <torch/torch.h>
 
 int main(int argc, const char* argv[]) {
-  TORCH_CHECK(torch::hasMKL(), "MKL is available");
+  TORCH_CHECK(torch::hasMKL(), "MKL is not available");
   return 0;
 }
 EOL
@@ -277,15 +278,15 @@ if [[ "$DESIRED_CUDA" != 'cpu' ]]; then
 
 int main(int argc, const char* argv[]) {
   std::cout << "Checking that CUDA archs are setup correctly" << std::endl;
-  TORCH_CHECK(torch::rand({3, 5}, torch::Device(torch::kCUDA)).defined(), "CUDA archs are setup correctly");
+  TORCH_CHECK(torch::rand({3, 5}, torch::Device(torch::kCUDA)).defined(), "CUDA archs are not setup correctly");
 
   // These have to run after CUDA is initialized
 
   std::cout << "Checking that magma is available" << std::endl;
-  TORCH_CHECK(torch::hasMAGMA(), "MAGMA is available");
+  TORCH_CHECK(torch::hasMAGMA(), "MAGMA is not available");
 
   std::cout << "Checking that CuDNN is available" << std::endl;
-  TORCH_CHECK(torch::cuda::cudnn_is_available(), "CuDNN is available");
+  TORCH_CHECK(torch::cuda::cudnn_is_available(), "CuDNN is not available");
   return 0;
 }
 EOL
