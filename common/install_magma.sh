a202d9c16b11afde7d665877c4571d026d5c0a12
Will Feng <willfeng@fb.com>
2019-08-22 11:12:43 -0400

    fix magma

--- common/install_magma.sh
+++ common/install_magma.sh
@@ -2,14 +2,16 @@
 pushd /tmp
 wget -q https://anaconda.org/pytorch/magma-cuda92/2.5.1/download/linux-64/magma-cuda92-2.5.1-1.tar.bz2
 tar -xvf magma-cuda92-2.5.1-1.tar.bz2
-cp -r include/* /usr/local/cuda-9.2/include/
-cp -r lib/* /usr/local/cuda-9.2/lib64/
+mkdir -p /usr/local/cuda-9.2/magma
+mv include /usr/local/cuda-9.2/magma/include
+mv lib /usr/local/cuda-9.2/magma/lib
 rm -rf info lib include magma-cuda92-2.5.1-1.tar.bz2
 
 # Install MAGMA for CUDA 10.0
 pushd /tmp
 wget -q https://anaconda.org/pytorch/magma-cuda100/2.5.1/download/linux-64/magma-cuda100-2.5.1-1.tar.bz2
 tar -xvf magma-cuda100-2.5.1-1.tar.bz2
-cp -r include/* /usr/local/cuda-10.0/include/
-cp -r lib/* /usr/local/cuda-10.0/lib64/
+mkdir -p /usr/local/cuda-10.0/magma
+mv include /usr/local/cuda-10.0/magma/include
+mv lib /usr/local/cuda-10.0/magma/lib
 rm -rf info lib include magma-cuda100-2.5.1-1.tar.bz2
