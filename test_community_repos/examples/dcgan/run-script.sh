1b61fee6abd99438259962d304cb042685feee80
Karl Ostmo <kostmo@gmail.com>
2019-09-16 13:33:33 -0700

    use cuda envar

--- test_community_repos/examples/dcgan/run-script.sh
+++ test_community_repos/examples/dcgan/run-script.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -xe
 
+CUDA_ARG=$1
+
 pushd examples/dcgan
 # smoke test
-python main.py --dataset fake --dataroot . --cuda --niter 100
+python main.py --dataset fake --dataroot . $CUDA_ARG --niter 100
 RETURN_CODE=$?
 popd
 exit $RETURN_CODE
