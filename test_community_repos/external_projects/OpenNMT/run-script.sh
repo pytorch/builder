6288883229e504795719cc518cdd3350b325f87b
Karl Ostmo <kostmo@gmail.com>
2019-09-19 15:21:37 -0700

    fix and skip OpenNMT

--- test_community_repos/external_projects/OpenNMT/run-script.sh
+++ test_community_repos/external_projects/OpenNMT/run-script.sh
@@ -11,5 +11,5 @@ python preprocess.py -train_src data/src-train.txt -train_tgt data/tgt-train.txt
 
 python train.py -data data/demo -save_model demo-model -train_steps 1 $GPU_ARGS
 
-python translate.py -model demo-model_*.pt -src data/src-test.txt -output pred.txt -replace_unk -verbose -gpu 0  -max_sent_length 10
+python translate.py -model demo-model_*.pt -src data/src-test.txt -output pred.txt -replace_unk -verbose -gpu 0 -max_length 10
 
