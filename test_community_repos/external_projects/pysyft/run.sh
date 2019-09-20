fa04e6b0ecb645b3556ed96fb472dac39c27ec1a
Karl Ostmo <kostmo@gmail.com>
2019-09-20 13:41:09 -0700

    pysyft only on Python 3

--- test_community_repos/external_projects/pysyft/run.sh
+++ test_community_repos/external_projects/pysyft/run.sh
@@ -1,21 +1,16 @@
 #!/bin/bash -xe
 
-pip install syft
 
 BASEDIR=$(dirname $0)
 pushd $BASEDIR
 
 
-
 git clone git@github.com:OpenMined/PySyft.git
 pushd PySyft
 
-make notebook
+pytest test/torch
 
 
-#../download_data.sh
-#../install-deps.sh
-#../run-script.sh
 popd
 rm -rf PySyft
 popd
