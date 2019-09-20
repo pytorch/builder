3ed7ffa1f844e9591cdacecc567ce478f6f4efbe
Karl Ostmo <kostmo@gmail.com>
2019-09-20 11:22:47 -0700

    fake unzip

--- test_community_repos/examples/fast_neural_style/unzip.py
+++ test_community_repos/examples/fast_neural_style/unzip.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import zipfile
 import re
 import sys
--- test_community_repos/external_projects/cyclegan/run.sh
+++ test_community_repos/external_projects/cyclegan/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash -xe
 
+# Hack to obtain "unzip" command
+UNZIP_BINARY_PATH=$HOME/bin
+
+mkdir $UNZIP_BINARY_PATH
+ln -s ~/project/test_community_repos/examples/fast_neural_style/unzip.py $UNZIP_BINARY_PATH/unzip
+chmod +x $UNZIP_BINARY_PATH/unzip
+export PATH=$PATH:$UNZIP_BINARY_PATH
+
 BASEDIR=$(dirname $0)
 pushd $BASEDIR
 git clone https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix.git
@@ -10,3 +18,6 @@ pushd pytorch-CycleGAN-and-pix2pix
 popd
 rm -rf pytorch-CycleGAN-and-pix2pix
 popd
+
+# Clean up hack
+rm -r $UNZIP_BINARY_PATH
