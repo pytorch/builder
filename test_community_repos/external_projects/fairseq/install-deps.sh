05d185b8bc3a327558a5cf59a805701d9d56ac7e
Karl Ostmo <kostmo@gmail.com>
2019-09-22 14:19:28 -0700

    macos workaround

--- test_community_repos/external_projects/fairseq/install-deps.sh
+++ test_community_repos/external_projects/fairseq/install-deps.sh
@@ -4,9 +4,14 @@
 python setup.py build
 
 
-# Potential workaround for probem on macos:
-#    fastBPE/fastBPE.hpp:17:10: fatal error: 'thread' file not found
-# See: https://github.com/facebookresearch/XLM/issues/105
-CFLAGS='-stdlib=libc++' python setup.py develop
-#python setup.py develop
+if [[ "$(uname)" != Darwin  ]]; then
+  python setup.py develop
+    
+else
+  # Potential workaround for probem on macos:
+  #    fastBPE/fastBPE.hpp:17:10: fatal error: 'thread' file not found
+  # See: https://github.com/facebookresearch/XLM/issues/105
+
+  CFLAGS='-stdlib=libc++' python setup.py develop
+fi
 
