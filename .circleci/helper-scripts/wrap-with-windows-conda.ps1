fdbcd538b50ee2c35e9cf5e0d96b5ddcbb989dc3
Karl Ostmo <kostmo@gmail.com>
2019-09-23 11:55:08 -0700

    add windows conda activation

--- .circleci/helper-scripts/wrap-with-windows-conda.ps1
+++ .circleci/helper-scripts/wrap-with-windows-conda.ps1
@@ -5,4 +5,8 @@ param(
 choco install miniconda3
 $env:PATH = "C:\tools\miniconda3;C:\tools\miniconda3\Library\usr\bin;C:\tools\miniconda3\Scripts;C:\tools\miniconda3\bin" + $env:PATH
 conda install -yq conda-build
+
+conda env remove -n "env$PYTHON_VERSION"
+conda create -yn "env$PYTHON_VERSION" python="$PYTHON_VERSION"
+
 bash packaging/build_conda.sh $a
