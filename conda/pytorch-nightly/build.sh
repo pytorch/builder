baf68175de6de7f52d59fdec5281f2db2650bf65
Soumith Chintala <soumith@gmail.com>
2019-10-12 11:09:32 -0700

    add proper torch cuda arch list to 10.1 conda

--- conda/pytorch-nightly/build.sh
+++ conda/pytorch-nightly/build.sh
@@ -39,6 +39,8 @@ if [[ -n "$build_with_cuda" ]]; then
         export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0"
     elif [[ $CUDA_VERSION == 10.0* ]]; then
         export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5"
+    elif [[ $CUDA_VERSION == 10.1* ]]; then
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5"
     fi
     export TORCH_NVCC_FLAGS="-Xfatbin -compress-all"
     export NCCL_ROOT_DIR=/usr/local/cuda
