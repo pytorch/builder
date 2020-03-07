6a9759882ae20c52903dbc5172df9bc87a2f9ef2
Eli Uriegas <eliuriegas@fb.com>
2020-03-06 18:27:08 -0800

    magma: Bump to 2.5.2, fix 2.5.2 devtoolset3 builds
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- common/install_magma.sh
+++ common/install_magma.sh
@@ -3,45 +3,45 @@
 function install_92 {
     # Install MAGMA for CUDA 9.2
     pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda92/2.5.1/download/linux-64/magma-cuda92-2.5.1-1.tar.bz2
-    tar -xvf magma-cuda92-2.5.1-1.tar.bz2
+    wget -q https://anaconda.org/pytorch/magma-cuda92/2.5.2/download/linux-64/magma-cuda92-2.5.2-1.tar.bz2
+    tar -xvf magma-cuda92-2.5.2-1.tar.bz2
     mkdir -p /usr/local/cuda-9.2/magma
     mv include /usr/local/cuda-9.2/magma/include
     mv lib /usr/local/cuda-9.2/magma/lib
-    rm -rf info lib include magma-cuda92-2.5.1-1.tar.bz2
+    rm -rf info lib include magma-cuda92-2.5.2-1.tar.bz2
 }
 
 function install_100 {
     # Install MAGMA for CUDA 10.0
     pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda100/2.5.1/download/linux-64/magma-cuda100-2.5.1-1.tar.bz2
-    tar -xvf magma-cuda100-2.5.1-1.tar.bz2
+    wget -q https://anaconda.org/pytorch/magma-cuda100/2.5.2/download/linux-64/magma-cuda100-2.5.2-1.tar.bz2
+    tar -xvf magma-cuda100-2.5.2-1.tar.bz2
     mkdir -p /usr/local/cuda-10.0/magma
     mv include /usr/local/cuda-10.0/magma/include
     mv lib /usr/local/cuda-10.0/magma/lib
-    rm -rf info lib include magma-cuda100-2.5.1-1.tar.bz2
+    rm -rf info lib include magma-cuda100-2.5.2-1.tar.bz2
 }
 
 function install_101 {
     # Install MAGMA for CUDA 10.1
     pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda101/2.5.1/download/linux-64/magma-cuda101-2.5.1-1.tar.bz2
-    tar -xvf magma-cuda101-2.5.1-1.tar.bz2
+    wget -q https://anaconda.org/pytorch/magma-cuda101/2.5.2/download/linux-64/magma-cuda101-2.5.2-1.tar.bz2
+    tar -xvf magma-cuda101-2.5.2-1.tar.bz2
     mkdir -p /usr/local/cuda-10.1/magma
     mv include /usr/local/cuda-10.1/magma/include
     mv lib /usr/local/cuda-10.1/magma/lib
-    rm -rf info lib include magma-cuda101-2.5.1-1.tar.bz2
+    rm -rf info lib include magma-cuda101-2.5.2-1.tar.bz2
 }
 
 function install_102 {
     # Install MAGMA for CUDA 10.2
     pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda102/2.5.1/download/linux-64/magma-cuda102-2.5.1-1.tar.bz2
-    tar -xvf magma-cuda102-2.5.1-1.tar.bz2
+    wget -q https://anaconda.org/pytorch/magma-cuda102/2.5.2/download/linux-64/magma-cuda102-2.5.2-1.tar.bz2
+    tar -xvf magma-cuda102-2.5.2-1.tar.bz2
     mkdir -p /usr/local/cuda-10.2/magma
     mv include /usr/local/cuda-10.2/magma/include
     mv lib /usr/local/cuda-10.2/magma/lib
-    rm -rf info lib include magma-cuda102-2.5.1-1.tar.bz2
+    rm -rf info lib include magma-cuda102-2.5.2-1.tar.bz2
 }
 
 # idiomatic parameter and option handling in sh
--- magma/build.sh
+++ magma/build.sh
@@ -2,8 +2,13 @@
 
 set -eou pipefail
 
-BUILDER_IMAGE="pytorch/conda-cuda"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+BUILDER_IMAGE="pytorch/magma-builder"
 DESIRED_CUDA=${DESIRED_CUDA:-10.2}
+
+# Do this so we don't have to send a docker context
+cat "${DIR}/Dockerfile" | docker build -t "${BUILDER_IMAGE}" -
 
 docker run --rm -i \
     -v $(git rev-parse --show-toplevel):/builder \
