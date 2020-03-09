3d168057b7000640ceaf788b794a5a85ce3eb8c6
Eli Uriegas <eliuriegas@fb.com>
2020-03-09 15:29:11 -0700

    manywheel: Consolidate manywheel images
    It was annoying me how many Dockerfiles we had so I reduced it to 1
    while also saving a load of time on overhead with caching.
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- manywheel/deploy.sh
+++ manywheel/deploy.sh
@@ -1,12 +1,19 @@
-set -e
-docker build -t pytorch/manylinux-cuda100 -f manywheel/Dockerfile_100 .
-docker push pytorch/manylinux-cuda100
+#!/usr/bin/env bash
 
-docker build -t pytorch/manylinux-cuda92 -f manywheel/Dockerfile_92 .
-docker push pytorch/manylinux-cuda92
+set -eou pipefail
 
-docker build -t pytorch/manylinux-cuda101 -f manywheel/Dockerfile_101 .
-docker push pytorch/manylinux-cuda101
+build_image() {
+    BASE_CUDA_VERSION=$1
+}
 
-docker build -t pytorch/manylinux-cuda102 -f manywheel/Dockerfile_102 .
-docker push pytorch/manylinux-cuda102
+for cuda_version in 9.2 10.0 10.1 10.2; do
+    (
+        set -x
+        docker build \
+            -t "pytorch/manylinux-cuda${cuda_version//./}" \
+            --build-arg "BASE_CUDA_VERSION=${cuda_version}" \
+            -f manywheel/Dockerfile \
+            .
+        docker push "pytorch/manylinux-cuda${cuda_version//./}"
+    )
+done
