5920ea9e3c744ff34dd8588ea23b47955ee0818a
peter <peter_jiachen@163.com>
2020-04-08 15:12:00 +0800

    Revert "Try fixing wheel builds"
    This reverts commit d004a9c5da7cbe225f7c9c4b2395b6a115000a5b.

--- windows/build_pytorch.bat
+++ windows/build_pytorch.bat
@@ -107,8 +107,6 @@ if "%USE_SCCACHE%" == "1" (
 set PYTORCH_BINARY_BUILD=1
 set TH_BINARY_BUILD=1
 set INSTALL_TEST=0
-:: Fix wheels build
-set CMAKE_NINJA_CMCLDEPS_RC=0
 
 for %%v in (%DESIRED_PYTHON_PREFIX%) do (
     :: Activate Python Environment
