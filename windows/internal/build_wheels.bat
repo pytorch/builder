521bbd81ed15ca44329b791358945acf33064409
peter <peter_jiachen@163.com>
2020-05-23 16:06:04 +0800

    Fix VS 2019 and CUDA installation scripts

--- windows/internal/build_wheels.bat
+++ windows/internal/build_wheels.bat
@@ -1,5 +1,5 @@
 if "%VC_YEAR%" == "2017" if "%CUDA_VERSION%" == "92" set VSDEVCMD_ARGS=-vcvars_ver=14.11
-if "%VC_YEAR%" == "2017" powershell windows/internal/vs_install.ps1
+powershell windows/internal/vs%VC_YEAR%_install.ps1
 if errorlevel 1 exit /b 1
 
 call windows/internal/cuda_install.bat
