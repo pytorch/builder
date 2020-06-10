1de21e39744f5b319cf7cd535045eea99d8706cc
Nikita Shulga <nshulga@fb.com>
2020-06-09 18:01:01 -0700

    Extract MKL version and build number into env vars

--- common/install_mkl.sh
+++ common/install_mkl.sh
@@ -3,9 +3,11 @@
 set -ex
 
 # MKL
+MKL_VERSION=2019.5
+MKL_BUILD=281
 mkdir -p /opt/intel/lib
 pushd /tmp
-curl -fsSL https://anaconda.org/intel/mkl-static/2019.5/download/linux-64/mkl-static-2019.5-intel_281.tar.bz2 | tar xjv
+curl -fsSL https://anaconda.org/intel/mkl-static/${MKL_VERSION}/download/linux-64/mkl-static-${MKL_VERSION}-intel_${MKL_BUILD}.tar.bz2 | tar xjv
 mv lib/* /opt/intel/lib/
-curl -fsSL https://anaconda.org/intel/mkl-include/2019.5/download/linux-64/mkl-include-2019.5-intel_281.tar.bz2 | tar xjv
+curl -fsSL https://anaconda.org/intel/mkl-include/${MKL_VERSION}/download/linux-64/mkl-include-${MKL_VERSION}-intel_${MKL_BUILD}.tar.bz2 | tar xjv
 mv include /opt/intel/
