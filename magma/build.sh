677ac128795dd260091bd9988141486115b4b264
Xiang Gao <qasdfgtyuiop@gmail.com>
2020-06-17 12:15:38 -0700

    Separate docker not needed

--- magma/build.sh
+++ magma/build.sh
@@ -3,16 +3,11 @@
 set -eou pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-BUILDER_IMAGE="pytorch/magma-builder"
 DESIRED_CUDA=${DESIRED_CUDA:-10.2}
-
-# Do this so we don't have to send a docker context
-cat "${DIR}/Dockerfile" | docker build -t "${BUILDER_IMAGE}" -
 
 docker run --rm -i \
     -v $(git rev-parse --show-toplevel):/builder \
     -w /builder \
     -e "DESIRED_CUDA=${DESIRED_CUDA}" \
-    "${BUILDER_IMAGE}" \
+    "pytorch/conda-cuda:latest" \
     magma/build_magma.sh
