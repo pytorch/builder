e3c5244cd236dc2339c8c5bbc6f9996d9a98f44f
Gao, Xiang <qasdfgtyuiop@gmail.com>
2020-06-18 15:42:57 -0700

    revert-some

--- magma/build.sh
+++ magma/build.sh
@@ -3,11 +3,16 @@
 set -eou pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+BUILDER_IMAGE="pytorch/magma-builder"
 DESIRED_CUDA=${DESIRED_CUDA:-10.2}
+
+# Do this so we don't have to send a docker context
+cat "${DIR}/Dockerfile" | docker build -t "${BUILDER_IMAGE}" -
 
 docker run --rm -i \
     -v $(git rev-parse --show-toplevel):/builder \
     -w /builder \
     -e "DESIRED_CUDA=${DESIRED_CUDA}" \
-    "pytorch/conda-cuda:latest" \
+    "${BUILDER_IMAGE}" \
     magma/build_magma.sh
