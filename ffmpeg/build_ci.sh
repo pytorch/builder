10f37591fb856b11faaf28203fded674d30960c2
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-08-04 15:50:25 -0500

    Add separate ffmpeg workflow

--- ffmpeg/build_ci.sh
+++ ffmpeg/build_ci.sh
@@ -10,9 +10,6 @@ FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff $DIR)
 if [ $FOLDER_COMMIT = $LATEST_COMMIT ]; then
     echo "FFMpeg has changed"
     ./$DIR/build_ffmpeg.sh
-
-    echo "Uploading FFmpeg to PyTorch Anaconda channel"
-    anaconda upload -u pytorch --force $DIR/output/*/ffmpeg*.bz2
 else
     echo "No changes in FFmpeg"
     exit 0;
