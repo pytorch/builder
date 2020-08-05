7c0bcc5d2b5c436c55ba51fac18bb0233972930e
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-08-05 12:02:44 -0500

    Do not trap errors when calling merge-base

--- ffmpeg/build_ci.sh
+++ ffmpeg/build_ci.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-set -eou pipefail
+set -xou pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 BASE_COMMIT=$(git merge-base --fork-point origin/master)
 FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff $DIR)
 
-git merge-base --is-ancestor FOLDER_COMMIT BASE_COMMIT
+git merge-base --is-ancestor $FOLDER_COMMIT $BASE_COMMIT
+COMMIT_SAME=$?
 
-if [ $? -eq 0 ]; then
+set -exou pipefail
+
+if [ $COMMIT_SAME -eq 1 ]; then
     echo "FFMpeg has changed"
-    ./$DIR/build_ffmpeg.sh
+    $DIR/build_ffmpeg.sh
 else
     echo "No changes in FFmpeg"
     exit 0;
