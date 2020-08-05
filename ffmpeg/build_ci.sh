d39d516dbb5be2a5fb24ddd0922e1b890b4e76ec
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-08-05 12:21:19 -0500

    Use refs/remotes/origin/HEAD

--- ffmpeg/build_ci.sh
+++ ffmpeg/build_ci.sh
@@ -9,7 +9,7 @@ git branch
 git fetch origin
 
 FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff $DIR)
-BASE_COMMIT=$(git merge-base --fork-point master)
+BASE_COMMIT=$(git merge-base --fork-point refs/remotes/origin/HEAD)
 
 git merge-base --is-ancestor $FOLDER_COMMIT $BASE_COMMIT
 COMMIT_SAME=$?
