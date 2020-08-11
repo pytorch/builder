4b9753e38c907330c4ee4d34fa354454b1ecceac
Eli Uriegas <eliuriegas@fb.com>
2020-08-11 10:23:35 -0700

    .circleci: Cleanup ffmpeg jobs, fix upload
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- ffmpeg/build_ci.sh
+++ ffmpeg/build_ci.sh
@@ -19,7 +19,6 @@ git fetch origin
 
 FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff $DIR)
 BASE_COMMIT=$(git show-ref refs/remotes/origin/master | awk '{print $1}')
-# BASE_COMMIT=$(git merge-base --fork-point refs/remotes/origin/master)
 
 git merge-base --is-ancestor $FOLDER_COMMIT $BASE_COMMIT
 COMMIT_SAME=$?
@@ -31,5 +30,6 @@ if [ $COMMIT_SAME -eq 1 ]; then
     $DIR/build_ffmpeg.sh
 else
     echo "No changes in FFmpeg"
+    mkdir -p output/
     exit 0;
 fi
