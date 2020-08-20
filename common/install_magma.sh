d15107ea01d621f93935931a995c313b271a65e4
Eli Uriegas <eliuriegas@fb.com>
2020-08-20 15:23:36 -0700

    common: Simplify magma install
    There was a lot of copy / paste within this file and this should make it
    so that we don't need to modify this file in order to add new CUDA
    versions
    
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- common/install_magma.sh
+++ common/install_magma.sh
@@ -1,64 +1,23 @@
 #!/usr/bin/env bash
 
-function install_92 {
-    # Install MAGMA for CUDA 9.2
-    pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda92/2.5.2/download/linux-64/magma-cuda92-2.5.2-1.tar.bz2
-    tar -xvf magma-cuda92-2.5.2-1.tar.bz2
-    mkdir -p /usr/local/cuda-9.2/magma
-    mv include /usr/local/cuda-9.2/magma/include
-    mv lib /usr/local/cuda-9.2/magma/lib
-    rm -rf info lib include magma-cuda92-2.5.2-1.tar.bz2
+set -eou pipefail
+
+MAGMA_VERSION="2.5.2"
+
+function do_install() {
+    cuda_version=$1
+    cuda_version_nodot=${1/./}
+    magma_archive="magma-cuda${cuda_version_nodot}-${MAGMA_VERSION}-1.tar.bz2"
+    cuda_dir="/usr/local/cuda-${cuda_version}"
+    (
+        set -x
+        wget -q https://anaconda.org/pytorch/magma-cuda${cuda_version_nodot}/${MAGMA_VERSION}/download/linux-64/${magma_archive}
+        tar -xvf "${magma_archive}"
+        mkdir -p "${cuda_dir}/magma"
+        mv include "${cuda_dir}/magma/include"
+        mv lib "${cuda_dir}/magma/lib"
+        rm -rf info lib include "${magma_archive}"
+    )
 }
 
-function install_100 {
-    # Install MAGMA for CUDA 10.0
-    pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda100/2.5.2/download/linux-64/magma-cuda100-2.5.2-1.tar.bz2
-    tar -xvf magma-cuda100-2.5.2-1.tar.bz2
-    mkdir -p /usr/local/cuda-10.0/magma
-    mv include /usr/local/cuda-10.0/magma/include
-    mv lib /usr/local/cuda-10.0/magma/lib
-    rm -rf info lib include magma-cuda100-2.5.2-1.tar.bz2
-}
-
-function install_101 {
-    # Install MAGMA for CUDA 10.1
-    pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda101/2.5.2/download/linux-64/magma-cuda101-2.5.2-1.tar.bz2
-    tar -xvf magma-cuda101-2.5.2-1.tar.bz2
-    mkdir -p /usr/local/cuda-10.1/magma
-    mv include /usr/local/cuda-10.1/magma/include
-    mv lib /usr/local/cuda-10.1/magma/lib
-    rm -rf info lib include magma-cuda101-2.5.2-1.tar.bz2
-}
-
-function install_102 {
-    # Install MAGMA for CUDA 10.2
-    pushd /tmp
-    wget -q https://anaconda.org/pytorch/magma-cuda102/2.5.2/download/linux-64/magma-cuda102-2.5.2-1.tar.bz2
-    tar -xvf magma-cuda102-2.5.2-1.tar.bz2
-    mkdir -p /usr/local/cuda-10.2/magma
-    mv include /usr/local/cuda-10.2/magma/include
-    mv lib /usr/local/cuda-10.2/magma/lib
-    rm -rf info lib include magma-cuda102-2.5.2-1.tar.bz2
-}
-
-# idiomatic parameter and option handling in sh
-while test $# -gt 0
-do
-    case "$1" in
-    9.2) install_92
-        ;;
-    10.0) install_100
-        ;;
-    10.1) install_101
-        ;;
-    10.2) install_102
-        ;;
-    *) echo "bad argument $1"; exit 1
-       ;;
-    esac
-    shift
-done
-
+do_install $1
