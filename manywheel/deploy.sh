0a2554bdf0a2ad30c29b0b5a5d39b9ce2d7a0c80
Jeff Daily <jeff.daily@amd.com>
2020-08-26 13:56:59 -0700

    install rocm packages as part of rocm_final target

--- manywheel/deploy.sh
+++ manywheel/deploy.sh
@@ -17,23 +17,22 @@ for rocm_version in 3.7.0; do
             --build-arg "BASE_ROCM_VERSION=${rocm_version}" \
             --build-arg "GPU_IMAGE=rocm/dev-centos-7:${rocm_version_clean}" \
             --target rocm_final \
-            --progress=plain \
             -f manywheel/Dockerfile \
             .
-        #docker push "pytorch/manylinux-rocm${rocm_version//./}"
+        docker push "pytorch/manylinux-rocm${rocm_version//./}"
     )
 done
 
-#for cuda_version in 9.2 10.1 10.2 11.0; do
-#    (
-#        set -x
-#        DOCKER_BUILDKIT=1 docker build \
-#            -t "pytorch/manylinux-cuda${cuda_version//./}" \
-#            --build-arg "BASE_CUDA_VERSION=${cuda_version}" \
-#            --build-arg "GPU_IMAGE=nvidia/cuda:${cuda_version}-devel-centos7" \
-#            --target cuda_final \
-#            -f manywheel/Dockerfile \
-#            .
-#        docker push "pytorch/manylinux-cuda${cuda_version//./}"
-#    )
-#done
+for cuda_version in 9.2 10.1 10.2 11.0; do
+    (
+        set -x
+        DOCKER_BUILDKIT=1 docker build \
+            -t "pytorch/manylinux-cuda${cuda_version//./}" \
+            --build-arg "BASE_CUDA_VERSION=${cuda_version}" \
+            --build-arg "GPU_IMAGE=nvidia/cuda:${cuda_version}-devel-centos7" \
+            --target cuda_final \
+            -f manywheel/Dockerfile \
+            .
+        docker push "pytorch/manylinux-cuda${cuda_version//./}"
+    )
+done
