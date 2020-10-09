fc8cd2a6fe9c9186dc96cab94ee385d035573802
Edgar Andrés Margffoy Tuay <andfoy@gmail.com>
2020-10-08 21:23:21 -0500

    Find tarball

--- ffmpeg/build_ffmpeg.sh
+++ ffmpeg/build_ffmpeg.sh
@@ -2,6 +2,11 @@
 
 set -eou pipefail
 
+if [[ "$OSTYPE" == "msys" ]]; then
+    FFMPEG_TARBALL=$(find tmp -type f -name *.tar.gz)
+    echo $FFMPEG_TARBALL
+fi
+
 conda install -yq conda-build conda-verify
 (
     set -x
