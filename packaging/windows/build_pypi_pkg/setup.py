5494154fad474b1e7d705f4dc31532fabe41b987
Joe Zhu <jozh@microsoft.com>
2020-10-24 12:51:29 +0800

    Resolve review comments

--- packaging/windows/build_pypi_pkg/setup.py
+++ packaging/windows/build_pypi_pkg/setup.py
@@ -5,28 +5,43 @@ import setuptools.command.install
 from setuptools import find_packages, setup
 import wheel.bdist_wheel
 
-pkg_name="torch"
-pkg_ver="{{GENERATE_TORCH_PKG_VER}}"
-torch_win_url="https://download.pytorch.org/whl/torch_stable.html"
-
+pkg_name = "torch"
+pkg_ver = "{{GENERATE_TORCH_PKG_VER}}"
+torch_download_url = "https://download.pytorch.org/whl/torch_stable.html"
 
 python_min_version = (3, 6, 1)
 python_min_version_str = '.'.join((str(num) for num in python_min_version))
-python_max_version = (3, 9, 0)
-python_max_version_str = '.'.join((str(num) for num in python_max_version))
+
+install_requires = [
+    'wheel',
+    'numpy',
+    'future',
+    'typing_extensions',
+    'dataclasses; python_version < "3.7"'
+]
 
 class install_torch(setuptools.command.install.install):
     def run(self):
-        setuptools.command.install.install.run(self)
-        raise Exception(
+        if sys.maxsize.bit_length() == 31:
+            raise UserWarning("We don't support Python x86." \
+                "Please install Python x64 instead.")
+
+        if sys.platform == 'win32':
+            raise UserWarning("Please install using `python "\
+                "-m pip install xxxx -f https://download.pytorch.org/whl/torch_stable.html`.")
+
+        raise UserWarning(
             f"Can not install torch due to local wheel built failed."
-            f"Plesse visit {torch_win_url} for more details."
+            f"Please visit {torch_download_url} for more details."
         )
 
 class bdist_wheel(wheel.bdist_wheel.bdist_wheel):
     def run(self):
         subprocess.check_call(
-            [sys.executable,'-m', 'pip', 'download', f'{pkg_name}==={pkg_ver}', '-f', torch_win_url, '--no-deps', '-d', self.dist_dir])
+            [sys.executable, '-m', 'pip', 'download',
+            f'{pkg_name}==={pkg_ver}', '-f', torch_download_url,
+            '--platform', 'win_amd64', '--only-binary=:all:',
+            '--no-deps', '-d', self.dist_dir])
 
 setup(
     name=pkg_name,
@@ -42,7 +57,8 @@ setup(
     download_url='https://github.com/pytorch/pytorch/tags',
     author='PyTorch Team',
     author_email='packages@pytorch.org',
-    python_requires='>={},<{}'.format(python_min_version_str, python_max_version_str),
+    python_requires='>={}'.format(python_min_version_str),
+    install_requires=install_requires,
     # PyPI package information.
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -58,7 +74,7 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: C++',
         'Programming Language :: Python :: 3',
-    ] + ['Programming Language :: Python :: 3.{}' for i in range(python_min_version[1], python_max_version[1])],
+    ],
     license='BSD-3',
     keywords='pytorch machine learning',
 )
