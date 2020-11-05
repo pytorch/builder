532bd2ee24f7773e79b94396f26d4f8b9fc939e5
Nikita Shulga <nshulga@fb.com>
2020-11-04 18:13:00 -0800

    Add condaforge support

--- build_aarch64_wheel.py
+++ build_aarch64_wheel.py
@@ -61,22 +61,42 @@ def update_apt_repo(addr):
     run_ssh(addr, "sudo apt-get update")
 
 
-def start_build(ami = ubuntu18_04_ami, branch="master"):
+def install_condaforge(addr):
+    print('Install conda-forge')
+    run_ssh(addr, "curl -OL https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh")
+    run_ssh(addr, "sh -f Miniforge3-Linux-aarch64.sh -b")
+    run_ssh(addr, ['sed','-i', '\'/^# If not running interactively.*/i PATH=$HOME/miniforge3/bin:$PATH\'', '.bashrc'])
+
+
+def start_build(ami = ubuntu18_04_ami, branch="master", use_conda=True):
     inst = start_instance(ami)
     addr = inst.public_dns_name
     wait_for_connection(addr, 22)
-    print(f'Configuring the system')
+    if use_conda:
+        install_condaforge(addr)
+        run_ssh(addr, "conda install -y numpy pyyaml")
+    print('Configuring the system')
     update_apt_repo(addr)
+
     run_ssh(addr, "sudo apt-get install -y ninja-build g++ git cmake python3-dev gfortran")
-    run_ssh(addr, "sudo apt-get install -y python3-yaml python3-setuptools python3-wheel python3-pip")
-    run_ssh(addr, "sudo pip3 install dataclasses")
-    # Installing Cython and numpy
-    run_ssh(addr, "sudo pip3 install Cython")
-    run_ssh(addr, "sudo pip3 install numpy")
+    if not use_conda:
+      run_ssh(addr, "sudo apt-get install -y python3-yaml python3-setuptools python3-wheel python3-pip")
+    run_ssh(addr, "pip3 install dataclasses")
+    # Install and switch to gcc-8 on Ubuntu-18.04
+    if ami == ubuntu18_04_ami:
+      run_ssh(addr, "sudo apt-get install -y g++-8 gfortran-8")
+      run_ssh(addr, "sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 100")
+      run_ssh(addr, "sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 100")
+      run_ssh(addr, "sudo update-alternatives --install /usr/bin/gfortran gfortran /usr/bin/gfortran-8 100")
+    if not use_conda:
+      print("Installing Cython + numpy from PyPy")
+      run_ssh(addr, "sudo pip3 install Cython")
+      run_ssh(addr, "sudo pip3 install numpy")
     # Build OpenBLAS
     print('Building OpenBLAS')
     run_ssh(addr, "git clone https://github.com/xianyi/OpenBLAS -b v0.3.10")
     run_ssh(addr, "pushd OpenBLAS; make NO_SHARED=1 -j8; sudo make NO_SHARED=1 install;popd")
+    #run_ssh(addr, "pushd OpenBLAS; make NO_SHARED=1 USE_OPENMP=1 -j8; sudo make NO_SHARED=1 USE_OPENMP=1 install;popd")
 
     print('Checking out the repo')
     run_ssh(addr, f"git clone --recurse-submodules -b {branch} https://github.com/pytorch/pytorch")
