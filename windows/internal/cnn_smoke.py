4b049e0811de2335086b969b69a3179824ee1e50
Yi Zhang <zhanyi@microsoft.com>
2020-11-05 01:52:53 -0800

    some refactor

--- windows/internal/cnn_smoke.py
+++ windows/internal/cnn_smoke.py
@@ -1,34 +1,35 @@
+r"""
+It's used to check basic rnn features in cuda.
+For example, it would throw exception if missing some components
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-class test(nn.Module):
+class SimpleCNN(nn.Module):
     def __init__(self):
-        super(test, self).__init__()
-        self.conv1 = nn.Conv2d(1,1,3)
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 1, 3)
         self.pool = nn.MaxPool2d(2, 2)
-        
-    def forward(self, input):
-        x = self.pool(F.relu(self.conv1(input)))
-        x = x.view(1)
-        return x
+
+    def forward(self, inputs):
+        output = self.pool(F.relu(self.conv1(inputs)))
+        output = output.view(1)
+        return output
 
 device = torch.device("cuda:0")
-net = test().to(device)
-inputs = torch.rand((1, 1, 5, 5), device=device)
-# check cudnn_ops_infer64_8.dll and cudnn_cnn_infer64_8.dll
-outputs = net(inputs)
+net = SimpleCNN().to(device)
+net_inputs = torch.rand((1, 1, 5, 5), device=device)
+outputs = net(net_inputs)
 print(outputs)
 
-# Mock one step training, check cudnn_ops_train64_8.dll and cudnn_cnn_train64_8.dll
 criterion = nn.MSELoss()
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.1)
-label = torch.full((1,), 1.0, dtype=torch.float, device=device)
 
+# Mock one step training
+label = torch.full((1,), 1.0, dtype=torch.float, device=device)
 loss = criterion(outputs, label)
 loss.backward()
 optimizer.step()
-
-
-
