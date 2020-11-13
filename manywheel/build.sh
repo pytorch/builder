e4938ae977605b63bd7c15404832ebdeb07ab66c
Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>
2020-11-13 12:53:05 -0500

    Add CUDA11.1 to manywheel/deploy.sh and manywheel/build.sh (#579)

--- manywheel/build.sh
+++ manywheel/build.sh
@@ -48,7 +48,11 @@ fi
 
 export TORCH_CUDA_ARCH_LIST="3.7;5.0;6.0;7.0"
 case ${CUDA_VERSION} in
-    11.*)
+    11.1)
+        export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;7.5;8.0;8.6"
+        EXTRA_CAFFE2_CMAKE_FLAGS+=("-DATEN_NO_TEST=ON")
+        ;;
+    11.0)
         export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;7.5;8.0"
         EXTRA_CAFFE2_CMAKE_FLAGS+=("-DATEN_NO_TEST=ON")
         ;;
@@ -180,6 +184,22 @@ DEPS_SONAME=(
     "libcudart.so.11.0"
     "libnvToolsExt.so.1"
     "libnvrtc.so.11.0"
+    "libnvrtc-builtins.so"
+    "libgomp.so.1"
+)
+elif [[ $CUDA_VERSION == "11.1" ]]; then
+DEPS_LIST=(
+    "/usr/local/cuda/lib64/libcudart.so.11.1"
+    "/usr/local/cuda/lib64/libnvToolsExt.so.1"
+    "/usr/local/cuda/lib64/libnvrtc.so.11.1"
+    "/usr/local/cuda/lib64/libnvrtc-builtins.so"
+    "$LIBGOMP_PATH"
+)
+
+DEPS_SONAME=(
+    "libcudart.so.11.1"
+    "libnvToolsExt.so.1"
+    "libnvrtc.so.11.1"
     "libnvrtc-builtins.so"
     "libgomp.so.1"
 )
