4bad49364bc0711fe9bd6682db1e2e643007fd57
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2021-02-05 11:01:51 -0800

    windows: Remove python check from libtorch smoke_test (#648)
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- windows/internal/smoke_test.bat
+++ windows/internal/smoke_test.bat
@@ -237,20 +237,6 @@ if "%BUILD_SPLIT_CUDA%" == "ON" (
 .\check-torch-cuda.exe
 if ERRORLEVEL 1 exit /b 1
 
-if NOT "%CUDA_PREFIX%" == "cpu" if "%NVIDIA_GPU_EXISTS%" == "1" (
-    echo Checking that CUDA archs are setup correctly
-    python -c "import torch; torch.randn([3,5]).cuda()"
-    if ERRORLEVEL 1 exit /b 1
-
-    echo Checking that magma is available
-    python -c "import torch; torch.rand(1).cuda(); exit(0 if torch.cuda.has_magma else 1)"
-    if ERRORLEVEL 1 exit /b 1
-
-    echo Checking that CuDNN is available
-    python -c "import torch; exit(0 if torch.backends.cudnn.is_available() else 1)"
-    if ERRORLEVEL 1 exit /b 1
-)
-
 popd
 
 echo Cleaning temp files
