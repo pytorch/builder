3a5db410c47e2d9b42b5823160adf25e8ef1205b
peterjc123 <peterghost86@gmail.com>
2021-02-09 11:03:52 +0800

    Some dependency fixes on Windows (#651)

--- windows/internal/copy.bat
+++ windows/internal/copy.bat
@@ -10,3 +10,4 @@ copy "%CUDA_PATH%\bin\nvrtc*64_*.dll*" pytorch\torch\lib
 
 copy "C:\Program Files\NVIDIA Corporation\NvToolsExt\bin\x64\nvToolsExt64_1.dll*" pytorch\torch\lib
 copy "%CONDA_LIB_PATH%\libiomp*5md.dll" pytorch\torch\lib
+copy "%CONDA_LIB_PATH%\uv.dll" pytorch\torch\lib
--- windows/internal/copy_cpu.bat
+++ windows/internal/copy_cpu.bat
@@ -1,1 +1,2 @@
 copy "%CONDA_LIB_PATH%\libiomp*5md.dll" pytorch\torch\lib
+copy "%CONDA_LIB_PATH%\uv.dll" pytorch\torch\lib
