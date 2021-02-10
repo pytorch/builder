170bc96af863bb614c9a4103d86818744c27cdcf
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2021-02-09 22:15:40 -0800

    libtorch: add option to build singular images (#643)

---
+++ libtorch/build_docker.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+export DOCKER_BUILDKIT=1
+TOPDIR=$(git rev-parse --show-toplevel)
+
+CUDA_VERSION=${CUDA_VERSION:-10.2}
+
+case ${CUDA_VERSION} in
+  cpu)
+    BASE_TARGET=base
+    DOCKER_TAG=cpu
+    ;;
+  *)
+    BASE_TARGET=cuda${CUDA_VERSION}
+    DOCKER_TAG=cuda${CUDA_VERSION}
+    ;;
+esac
+
+(
+  set -x
+  docker build \
+    --target final \
+    --build-arg "BASE_TARGET=${BASE_TARGET}" \
+    --build-arg "CUDA_VERSION=${CUDA_VERSION}" \
+    -t "pytorch/libtorch-cxx11-builder:${DOCKER_TAG}" \
+    -f "${TOPDIR}/libtorch/ubuntu16.04/Dockerfile" \
+    ${TOPDIR}
+)
