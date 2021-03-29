6e0d7251323878199d6852190e6e3a2b0c7bd700
peterjc123 <peterghost86@gmail.com>
2021-03-30 01:18:33 +0800

    Update MSVC toolchain selection for Windows builds (#684)

--- conda/vs2019/activate.bat
+++ conda/vs2019/activate.bat
@@ -13,6 +13,11 @@ set "MSYS2_ENV_CONV_EXCL=CL"
 :: http://stevedower.id.au/blog/building-for-python-3-5-part-two/ for more info
 set "PY_VCRUNTIME_REDIST=%PREFIX%\\bin\\vcruntime140.dll"
 
+if NOT "%VS15INSTALLDIR%" == "" if exist "%VS15INSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" (
+    set "VSINSTALLDIR=%VS15INSTALLDIR%\"
+    goto :vswhere
+)
+
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -legacy -products * -version [16^,17^) -property installationPath`) do (
     if exist "%%i" if exist "%%i\VC\Auxiliary\Build\vcvarsall.bat" (
         set "VSINSTALLDIR=%%i\"
--- windows/internal/check_deps.bat
+++ windows/internal/check_deps.bat
@@ -30,6 +30,11 @@ IF "%VC_YEAR%" == "2017" (
     set VC_VERSION_UPPER=16
 )
 
+if NOT "%VS15INSTALLDIR%" == "" if exist "%VS15INSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat" (
+    set "VS15VCVARSALL=%VS15INSTALLDIR%\VC\Auxiliary\Build\vcvarsall.bat"
+    goto vswhere
+)
+
 for /f "usebackq tokens=*" %%i in (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -legacy -products * -version [%VC_VERSION_LOWER%^,%VC_VERSION_UPPER%^) -property installationPath`) do (
     if exist "%%i" if exist "%%i\VC\Auxiliary\Build\vcvarsall.bat" (
         set "VS15INSTALLDIR=%%i"
