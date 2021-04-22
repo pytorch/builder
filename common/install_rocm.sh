fa157063dfda98f5d1c15078f96ed2d2ce1ec507
Aswin John Mathews <aswin.mathews@amd.com>
2021-04-22 16:06:28 +0000

    Adding to LIB instead of LDFLAGS

--- common/install_rocm.sh
+++ common/install_rocm.sh
@@ -35,13 +35,11 @@ git checkout 878b1ce02e9cfe4a829be22c8f911e9c0b6bd88f
 cp make.inc-examples/make.inc.hip-gcc-mkl make.inc
 echo 'LIBDIR += -L$(MKLROOT)/lib' >> make.inc
 # overwrite original LIB, because it's wrong; it's missing start/end-group
-echo 'LIB = -Wl,--start-group -lmkl_gf_lp64 -lmkl_gnu_thread -lmkl_core -Wl,--end-group -lpthread -lstdc++ -lm -lgomp' >> make.inc
+echo 'LIB = -Wl,--start-group -lmkl_gf_lp64 -lmkl_gnu_thread -lmkl_core -Wl,--end-group -lpthread -lstdc++ -lm -lgomp -lhipspare -lhipblas' >> make.inc
 echo 'LIB += -Wl,--enable-new-dtags -Wl,--rpath,/opt/rocm/lib -Wl,--rpath,$(MKLROOT)/lib -Wl,--rpath,/opt/rocm/magma/lib' >> make.inc
 echo 'DEVCCFLAGS += --amdgpu-target=gfx803 --amdgpu-target=gfx900 --amdgpu-target=gfx906 --amdgpu-target=gfx908 --gpu-max-threads-per-block=256' >> make.inc
 # hipcc with openmp flag causes isnan() on __device__ not to be found; depending on context, compiler may attempt to match with host definition
 sed -i 's/^FOPENMP/#FOPENMP/g' make.inc
-# hipblas and hipsparse libraries need to be added to LDFLAGS
-echo 'LDFLAGS += -lhipblas -lhipsparse' >> make.inc
 export PATH="${PATH}:/opt/rocm/bin"
 make -f make.gen.hipMAGMA -j $(nproc)
 LANG=C.UTF-8 make lib/libmagma.so -j $(nproc) MKLROOT=/opt/intel
