a998efa7fdc65f3f732e0da21bd0fc471dd2bcc0
Eli Uriegas <eliuriegas@fb.com>
2021-04-23 15:17:42 -0700

    fx openssl and llvm installs
    Signed-off-by: Eli Uriegas <eliuriegas@fb.com>

--- common/install_openssl.sh
+++ common/install_openssl.sh
@@ -7,7 +7,8 @@ OPENSSL=openssl-1.1.1k
 wget -q -O ${OPENSSL}.tar.gz https://www.openssl.org/source/${OPENSSL}.tar.gz
 tar xf ${OPENSSL}.tar.gz
 cd ${OPENSSL}
-./config -d '-Wl,--enable-new-dtags,-rpath,$(LIBRPATH)'
-make install
+./config --prefix=/opt/openssl -d '-Wl,--enable-new-dtags,-rpath,$(LIBRPATH)'
+# NOTE: opensl errors out when built with the -j option
+make install_sw
 cd ..
 rm -rf ${OPENSSL}
