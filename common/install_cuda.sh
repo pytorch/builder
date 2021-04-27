56aaec4f481edb5fc25a2a77482a7de4d00edab6
Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>
2021-04-27 18:56:14 -0400

    Change conda-cuda and libtorch code to support CUDA 11.3 (#719)
    Co-authored-by: Eli Uriegas <eliuriegas@fb.com>

--- common/install_cuda.sh
+++ common/install_cuda.sh
@@ -156,7 +156,27 @@ function install_112 {
     rm -f cuda_11.2.0_460.27.04_linux.run
     rm -f /usr/local/cuda && ln -s /usr/local/cuda-11.2 /usr/local/cuda
 
-    # TODO: install CUDA 11.2 CuDNN 8.0.5, currently it's the 11.1 version
+    # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
+    mkdir tmp_cudnn && cd tmp_cudnn
+    wget -q https://developer.download.nvidia.com/compute/redist/cudnn/v8.1.0/cudnn-11.2-linux-x64-v8.1.0.77.tgz -O cudnn-8.1.tgz
+    tar xf cudnn-8.1.tgz
+    cp -a cuda/include/* /usr/local/cuda/include/
+    cp -a cuda/lib64/* /usr/local/cuda/lib64/
+    cd ..
+    rm -rf tmp_cudnn
+    ldconfig
+}
+
+function install_113 {
+    echo "Installing CUDA 11.3 and CuDNN 8.1"
+    rm -rf /usr/local/cuda-11.3 /usr/local/cuda
+    # install CUDA 11.3 in the same container
+    wget -q https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda_11.3.0_465.19.01_linux.run
+    chmod +x cuda_11.3.0_465.19.01_linux.run
+    ./cuda_11.3.0_465.19.01_linux.run --toolkit --silent
+    rm -f cuda_11.3.0_465.19.01_linux.run
+    rm -f /usr/local/cuda && ln -s /usr/local/cuda-11.3 /usr/local/cuda
+
     # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
     mkdir tmp_cudnn && cd tmp_cudnn
     wget -q https://developer.download.nvidia.com/compute/redist/cudnn/v8.1.0/cudnn-11.2-linux-x64-v8.1.0.77.tgz -O cudnn-8.1.tgz
@@ -361,6 +381,38 @@ function prune_112 {
     rm -rf $CUDA_BASE/libnvvp $CUDA_BASE/nsightee_plugins $CUDA_BASE/nsight-compute-2020.3.0 $CUDA_BASE/nsight-systems-2020.4.3
 }
 
+function prune_113 {
+    echo "Pruning CUDA 11.3 and CuDNN"
+    #####################################################################################
+    # CUDA 11.3 prune static libs
+    #####################################################################################
+    export NVPRUNE="/usr/local/cuda-11.3/bin/nvprune"
+    export CUDA_LIB_DIR="/usr/local/cuda-11.3/lib64"
+
+    export GENCODE="-gencode arch=compute_35,code=sm_35 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86"
+    export GENCODE_CUDNN="-gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86"
+
+    if [[ -n "$OVERRIDE_GENCODE" ]]; then
+        export GENCODE=$OVERRIDE_GENCODE
+    fi
+
+    # all CUDA libs except CuDNN and CuBLAS (cudnn and cublas need arch 3.7 included)
+    ls $CUDA_LIB_DIR/ | grep "\.a" | grep -v "culibos" | grep -v "cudart" | grep -v "cudnn" | grep -v "cublas" | grep -v "metis"  \
+      | xargs -I {} bash -c \
+		"echo {} && $NVPRUNE $GENCODE $CUDA_LIB_DIR/{} -o $CUDA_LIB_DIR/{}"
+
+    # prune CuDNN and CuBLAS
+    $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcudnn_static.a -o $CUDA_LIB_DIR/libcudnn_static.a
+    $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcublas_static.a -o $CUDA_LIB_DIR/libcublas_static.a
+    $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcublasLt_static.a -o $CUDA_LIB_DIR/libcublasLt_static.a
+
+    #####################################################################################
+    # CUDA 11.3 prune visual tools
+    #####################################################################################
+    export CUDA_BASE="/usr/local/cuda-11.3/"
+    rm -rf $CUDA_BASE/libnvvp $CUDA_BASE/nsightee_plugins $CUDA_BASE/nsight-compute-2021.1.0 $CUDA_BASE/nsight-systems-2021.1.3
+}
+
 # idiomatic parameter and option handling in sh
 while test $# -gt 0
 do
@@ -376,6 +428,8 @@ do
     11.1) install_111; prune_111
 		;;
     11.2) install_112; prune_112
+		;;
+    11.3) install_113; prune_113
 		;;
 	*) echo "bad argument $1"; exit 1
 	   ;;
