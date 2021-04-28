6f4e0ca67e33dbf1cfbf0d4622973c1f807bcf09
Eli Uriegas <1700823+seemethere@users.noreply.github.com>
2021-04-28 09:51:58 -0700

    manywheel: Refactor docker build / upload scripts (#722)
    Co-authored-by: Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>

---
+++ manywheel/build_all_docker.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+TOPDIR=$(git rev-parse --show-toplevel)
+
+GPU_ARCH_TYPE=cpu "${TOPDIR}/manywheel/build_docker.sh"
+
+for cuda_version in 11.3 11.1 10.2; do
+    GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
+done
+
+for rocm_version in 4.0.1 4.1; do
+    GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
+done
