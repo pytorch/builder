411394884654757ccdfc6e32ff90f254b4d8bae1
Jeff Daily <jeff.daily@amd.com>
2021-05-11 13:33:50 -0700

    update manywheel/build_all_docker.sh for rocm 4.2

--- manywheel/build_all_docker.sh
+++ manywheel/build_all_docker.sh
@@ -10,6 +10,6 @@ for cuda_version in 11.3 11.1 10.2; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
 
-for rocm_version in 4.0.1 4.1; do
+for rocm_version in 4.0.1 4.1 4.2; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
