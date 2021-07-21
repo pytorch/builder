pytorch/builder

commit 2be6507c4e656bf87042732b56628e5a2287690e

Fix binary_macos_libtorch builds
Not sure what had happened, but location of libimop5.dylib has changed in the package

wheel/build_wheel.sh
@@ -238,7 +238,11 @@ else
         cp -r "$(pwd)/any_wheel/torch/lib/include" "$(pwd)/libtorch/"
     fi
     cp -r "$(pwd)/any_wheel/torch/share/cmake" "$(pwd)/libtorch/share/"
-    cp -r "$(pwd)/any_wheel/torch/.dylibs/libiomp5.dylib" "$(pwd)/libtorch/lib/"
+    if [[ -d $(pwd)/any_wheel/torch/.dylibs/libiomp5.dylib ]]; then
+        cp -r "$(pwd)/any_wheel/torch/.dylibs/libiomp5.dylib" "$(pwd)/libtorch/lib/"
+    else
+        cp -r "$(pwd)/any_wheel/torch/lib/libiomp5.dylib" "$(pwd)/libtorch/lib/"
+    fi
     rm -rf "$(pwd)/any_wheel"
 
     echo $PYTORCH_BUILD_VERSION > libtorch/build-version

commit b38c548e3a73f14a941859d20dcd8c5e2ab463ae

Update build_wheel.sh
Fix typo introduced by 2be6507c4e656bf87042732b56628e5a2287690e

wheel/build_wheel.sh
@@ -238,7 +238,7 @@ else
         cp -r "$(pwd)/any_wheel/torch/lib/include" "$(pwd)/libtorch/"
     fi
     cp -r "$(pwd)/any_wheel/torch/share/cmake" "$(pwd)/libtorch/share/"
-    if [[ -d $(pwd)/any_wheel/torch/.dylibs/libiomp5.dylib ]]; then
+    if [[ -x "$(pwd)/any_wheel/torch/.dylibs/libiomp5.dylib" ]]; then
         cp -r "$(pwd)/any_wheel/torch/.dylibs/libiomp5.dylib" "$(pwd)/libtorch/lib/"
     else
         cp -r "$(pwd)/any_wheel/torch/lib/libiomp5.dylib" "$(pwd)/libtorch/lib/"