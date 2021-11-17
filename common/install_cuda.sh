ae55f0d53f89398306dda8bdd192385733432b72
atalman <atalman@fb.com>
2021-11-17 08:35:49 -0800

    Change conda-cuda and libtorch code to support CUDA 11.5

--- common/install_cuda.sh
+++ common/install_cuda.sh
@@ -188,6 +188,27 @@ function install_113 {
     ldconfig
 }
 
+function install_115 {
+    echo "Installing CUDA 11.5 and CuDNN 8.3"
+    rm -rf /usr/local/cuda-11.5 /usr/local/cuda
+    # install CUDA 11.5.0 in the same container
+    wget -q https://developer.download.nvidia.com/compute/cuda/11.5.0/local_installers/cuda_11.5.0_495.29.05_linux.run
+    chmod +x cuda_11.5.0_495.29.05_linux.run
+    ./cuda_11.5.0_495.29.05_linux.run --toolkit --silent
+    rm -f cuda_11.5.0_495.29.05_linux.run
+    rm -f /usr/local/cuda && ln -s /usr/local/cuda-11.5 /usr/local/cuda
+
+    # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
+    mkdir tmp_cudnn && cd tmp_cudnn
+    wget -q https://developer.download.nvidia.com/compute/redist/cudnn/v8.3.0/cudnn-11.5-linux-x64-v8.3.0.98.tgz -O cudnn-8.3.tgz
+    tar xf cudnn-8.3.tgz
+    cp -a cuda/include/* /usr/local/cuda/include/
+    cp -a cuda/lib64/* /usr/local/cuda/lib64/
+    cd ..
+    rm -rf tmp_cudnn
+    ldconfig
+}
+
 function prune_92 {
     echo "Pruning CUDA 9.2 and CuDNN"
     #####################################################################################
@@ -413,6 +434,38 @@ function prune_113 {
     rm -rf $CUDA_BASE/libnvvp $CUDA_BASE/nsightee_plugins $CUDA_BASE/nsight-compute-2021.1.0 $CUDA_BASE/nsight-systems-2021.1.3
 }
 
+function prune_115 {
+    echo "Pruning CUDA 11.5 and CuDNN"
+    #####################################################################################
+    # CUDA 11.3 prune static libs
+    #####################################################################################
+    export NVPRUNE="/usr/local/cuda-11.5/bin/nvprune"
+    export CUDA_LIB_DIR="/usr/local/cuda-11.5/lib64"
+
+    export GENCODE="-gencode arch=compute_35,code=sm_35 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86"
+    export GENCODE_CUDNN="-gencode arch=compute_35,code=sm_35 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_75,code=sm_75 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86"
+
+    if [[ -n "$OVERRIDE_GENCODE" ]]; then
+        export GENCODE=$OVERRIDE_GENCODE
+    fi
+
+    # all CUDA libs except CuDNN and CuBLAS (cudnn and cublas need arch 3.7 included)
+    ls $CUDA_LIB_DIR/ | grep "\.a" | grep -v "culibos" | grep -v "cudart" | grep -v "cudnn" | grep -v "cublas" | grep -v "metis"  \
+      | xargs -I {} bash -c \
+		"echo {} && $NVPRUNE $GENCODE $CUDA_LIB_DIR/{} -o $CUDA_LIB_DIR/{}"
+
+    # prune CuDNN and CuBLAS
+    # $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcudnn_static.a -o $CUDA_LIB_DIR/libcudnn_static.a
+    $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcublas_static.a -o $CUDA_LIB_DIR/libcublas_static.a
+    $NVPRUNE $GENCODE_CUDNN $CUDA_LIB_DIR/libcublasLt_static.a -o $CUDA_LIB_DIR/libcublasLt_static.a
+
+    #####################################################################################
+    # CUDA 11.5 prune visual tools
+    #####################################################################################
+    export CUDA_BASE="/usr/local/cuda-11.5/"
+    rm -rf $CUDA_BASE/libnvvp $CUDA_BASE/nsightee_plugins $CUDA_BASE/nsight-compute-2021.3.0 $CUDA_BASE/nsight-systems-2021.3.3
+}
+
 # idiomatic parameter and option handling in sh
 while test $# -gt 0
 do
@@ -430,6 +483,8 @@ do
     11.2) install_112; prune_112
 		;;
     11.3) install_113; prune_113
+		;;
+    11.5) install_115; prune_115
 		;;
 	*) echo "bad argument $1"; exit 1
 	   ;;
