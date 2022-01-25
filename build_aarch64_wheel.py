0f9cf6d225e90b73115d5b7bb3c671fc1d6070ce
Nikita Shulga <nshulga@fb.com>
2022-01-25 12:06:04 -0800

    Add 3.10 option to aarch64 builds

--- build_aarch64_wheel.py
+++ build_aarch64_wheel.py
@@ -573,7 +573,7 @@ def parse_arguments():
     parser.add_argument("--build-only", action="store_true")
     parser.add_argument("--test-only", type=str)
     parser.add_argument("--os", type=str, choices=list(os_amis.keys()), default='ubuntu18_04')
-    parser.add_argument("--python-version", type=str, choices=['3.6', '3.7', '3.8', '3.9'], default=None)
+    parser.add_argument("--python-version", type=str, choices=['3.6', '3.7', '3.8', '3.9', '3.10'], default=None)
     parser.add_argument("--alloc-instance", action="store_true")
     parser.add_argument("--list-instances", action="store_true")
     parser.add_argument("--keep-running", action="store_true")
