7c2cfcea0b58e8d3f1145557636321b21cdaaec5
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2022-02-23 15:16:04 +0200

    Update conda magma to 2.6.1 (#962)

--- magma/build_magma.sh
+++ magma/build_magma.sh
@@ -11,11 +11,6 @@ cp ${PACKAGE_FILES}/meta.yaml ${PACKAGE_DIR}/meta.yaml
 cp ${PACKAGE_FILES}/thread_queue.patch ${PACKAGE_DIR}/thread_queue.patch
 cp ${PACKAGE_FILES}/cmakelists.patch ${PACKAGE_DIR}/cmakelists.patch
 
-# The cudaPointerAttributes patch for an API change on CUDA 11+
-if [[ $DESIRED_CUDA == 11* ]]; then
-    cp ${PACKAGE_FILES}/cudaPointerAttributes.patch ${PACKAGE_DIR}/cudaPointerAttributes.patch
-fi
-
 conda install -yq conda-build conda-verify
 . ./conda/switch_cuda_version.sh "${DESIRED_CUDA}"
 (
