e8861ff35716901cd886087a169058ad71ca2e5a
atalman <atalman@fb.com>
2022-02-24 05:43:10 -0800

    Increase VolumeSize for ec2 instance to avoid running out of space

--- build_aarch64_wheel.py
+++ build_aarch64_wheel.py
@@ -50,7 +50,16 @@ def start_instance(key_name, ami=ubuntu18_04_ami, instance_type='t4g.2xlarge'):
                                 SecurityGroups=['ssh-allworld'],
                                 KeyName=key_name,
                                 MinCount=1,
-                                MaxCount=1)[0]
+                                MaxCount=1,
+                                BlockDeviceMappings=[
+                                    {
+                                        'DeviceName': '/dev/sda1',
+                                        'Ebs': {
+                                            'VolumeSize': 50,
+                                            'VolumeType': 'standard'
+                                        }
+                                    }
+                                ])[0]
     print(f'Create instance {inst.id}')
     inst.wait_until_running()
     running_inst = ec2_instances_by_id(inst.id)
