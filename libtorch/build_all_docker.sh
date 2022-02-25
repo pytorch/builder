1cbc31f9204d64dfbd3b892e7334087c2264414b
Jeff Daily <jeff.daily@amd.com>
2022-02-25 13:45:39 -0800

    add rocm 5.0 to libtorch/build_all_docker.sh

--- libtorch/build_all_docker.sh
+++ libtorch/build_all_docker.sh
@@ -10,6 +10,6 @@ for cuda_version in 11.5 11.3 11.1 10.2; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
 
-for rocm_version in 4.3.1 4.5.2; do
+for rocm_version in 4.5.2 5.0; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
