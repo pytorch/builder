454619e0b6c75783a8bdb76343d6fec7d0c268e6
Zhu, Hong <hong.zhu@intel.com>
2022-03-11 10:06:31 +0800

    manywheel: add _GLIBCXX_USE_CXX11_ABI=1 support for linux cpu wheel
    The target of this commit is to add the support for a new linux
    cpu pip wheel file built with _GLIBCXX_USE_CXX11_ABI=1.
    
    Currently, linux wheels are built in a system based on CentOS7
    and devtoolset7, and CXX11_ABI is ignored by the compiler. The
    same issue with devtoolset8 and devtoolset9, and so we add a Docker
    file (Dockerfile_cxx11-abi) with Ubuntu 18.04 as base image to
    support CXX11_ABI=1, by referring the Dockerfile for libtorch.
    
    To build the new docker image with CXX11_ABI support, run:
    GPU_ARCH_TYPE=cpu-cxx11-abi manywheel/build_docker.sh
    or
    manywheel/build_all_docker.sh
    
    To build a linux cpu pip wheel with CXX11_ABI within this image, run:
    // the below settings are special for this image
    export DESIRED_CUDA=cpu-cxx11-abi   # change from cpu for wheel name
    export GPU_ARCH_TYPE=cpu-cxx11-abi  # change from cpu for build.sh
    export DOCKER_IMAGE=pytorch/manylinuxcxx11-abi-builder:cpu-cxx11-abi
    export DESIRED_DEVTOOLSET=cxx11-abi
    
    // the below settings are as usual
    export BINARY_ENV_FILE=/tmp/env
    export BUILDER_ROOT=/builder
    export DESIRED_PYTHON=3.7   # or 3.8, 3.9, etc.
    export IS_GHA=1
    export PACKAGE_TYPE=manywheel
    export PYTORCH_FINAL_PACKAGE_DIR=/artifacts
    export PYTORCH_ROOT=/pytorch
    export GITHUB_WORKSPACE=/your_path_to_workspace
    
    // the '-e DESIRED_DEVTOOLSET' below is newly added for this container,
    // others are as usual
    set -x
      mkdir -p artifacts/
      container_name=$(docker run \
        -e BINARY_ENV_FILE \
        -e BUILDER_ROOT \
        -e DESIRED_CUDA \
        -e DESIRED_PYTHON \
        -e GPU_ARCH_TYPE \
        -e IS_GHA \
        -e PACKAGE_TYPE \
        -e PYTORCH_FINAL_PACKAGE_DIR \
        -e PYTORCH_ROOT \
        -e DOCKER_IMAGE \
        -e DESIRED_DEVTOOLSET \
        --tty \
        --detach \
        -v "${GITHUB_WORKSPACE}/pytorch:/pytorch" \
        -v "${GITHUB_WORKSPACE}/builder:/builder" \
        -v "${RUNNER_TEMP}/artifacts:/artifacts" \
        -w / \
        "${DOCKER_IMAGE}"
      )
    
    // build pip wheel as usual,
    // and the built wheel file name looks like: torch-1.12.0.dev20220312+cpu.cxx11.abi-cp37-cp37m-linux_x86_64.whl
    docker exec -t -w "${PYTORCH_ROOT}" "${container_name}" bash -c "bash .circleci/scripts/binary_populate_env.sh"
    docker exec -t "${container_name}" bash -c "source ${BINARY_ENV_FILE} && bash /builder/manywheel/build.sh"
    
    // to verify the built wheel file, we'll see 'True'
    $ pip install torch-1.12.0.dev20220312+cpu.cxx11.abi-cp37-cp37m-linux_x86_64.whl
    $ python -c 'import torch; print(torch._C._GLIBCXX_USE_CXX11_ABI)'
    True
    
    Co-authored-by: Guo Yejun <yejun.guo@intel.com>
    Co-authored-by: Zhu Hong <hong.zhu@intel.com>

--- manywheel/build_all_docker.sh
+++ manywheel/build_all_docker.sh
@@ -7,6 +7,8 @@ TOPDIR=$(git rev-parse --show-toplevel)
 GPU_ARCH_TYPE=cpu "${TOPDIR}/manywheel/build_docker.sh"
 MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=cpu "${TOPDIR}/manywheel/build_docker.sh"
 
+GPU_ARCH_TYPE=cpu-cxx11-abi "${TOPDIR}/manywheel/build_docker.sh"
+
 for cuda_version in 11.5 11.3 10.2; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
     MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
