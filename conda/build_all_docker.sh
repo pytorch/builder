6e1ea62ba7dd5a08dd596e34c147e82045d230e8
ptrblck <ptrblck@users.noreply.github.com>
2022-03-21 12:58:45 -0700

    [WIP] ADD 11.6 workflow for docker image build (#992)
    * ADD 11.6 workflow for docker image build
    
    * use CUDA 11.6.0
    
    * remove libtorch changes
    
    * remove 11.6 from .github/workflows/build-manywheel-images.yml
    
    Co-authored-by: Andrey Talman <atalman@fb.com>
    
    * fix ln for 11.6 in common/install_cuda.sh
    
    Co-authored-by: Andrey Talman <atalman@fb.com>
    
    Co-authored-by: pbialecki <pbialecki@nvidia.com>
    Co-authored-by: Andrey Talman <atalman@fb.com>

--- conda/build_all_docker.sh
+++ conda/build_all_docker.sh
@@ -4,6 +4,6 @@ set -eou pipefail
 
 TOPDIR=$(git rev-parse --show-toplevel)
 
-for CUDA_VERSION in 11.5 11.3 10.2 cpu; do
+for CUDA_VERSION in 11.6 11.5 11.3 10.2 cpu; do
   CUDA_VERSION="${CUDA_VERSION}" conda/build_docker.sh
 done
