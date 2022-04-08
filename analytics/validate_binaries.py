6e54b28ca4926d33217c224f12261c119766334f
Nirav Mehta <niravmehta@fb.com>
2022-04-08 09:14:32 -0700

    Update on "Adding script to validate conda build packages for pytorch"
    [ghstack-poisoned]

--- analytics/validate_binaries.py
+++ analytics/validate_binaries.py
@@ -1,10 +1,15 @@
 from conda.cli.python_api import Commands, run_command
+from tabulate import tabulate
+from datetime import datetime
 import json
 
 PLATFORMS = ["osx-64", "linux-64", "win-64"]
 PYTHON_VERSIONS = ["3.10", "3.9", "3.8", "3.7"]
-CUDA_VERSIONS = ["11.5", "11.3", "11.1", "10.2", "cpu"]
-CUDNN_VERSIONS = ["8.3.2", "8.2.0", "8.0.5", "7.6.5", None]
+CUDA_CUDNN_VERSION = [
+    ("11.5", "8.3.2"), ("11.3", "8.2.0"), ("11.1", "8.0.5"), ("10.2", "7.6.5"), ("cpu", None)
+]
+CHANNEL = "pytorch-test"
+VERSION = "1.11.*"
 
 
 def generate_expected_builds(platform: str) -> set:
@@ -15,7 +20,7 @@ def generate_expected_builds(platform: str) -> set:
             builds.add(f"py{py_version}_0")
             continue
 
-        for cuda_version, cudnn_version in zip(CUDA_VERSIONS, CUDNN_VERSIONS):
+        for cuda_version, cudnn_version in CUDA_CUDNN_VERSION:
             if platform == "win-64":
                 if cuda_version == "10.2":
                     # win does not support cuda 10.2
@@ -29,29 +34,46 @@ def generate_expected_builds(platform: str) -> set:
     return builds
 
 
-def main():
+def size_format(size_num) -> str:
+    for unit in ["", "K", "M", "G"]:
+        if abs(size_num) < 1024.0:
+            return f"{size_num:3.1f}{unit}B"
+
+        size_num /= 1024.0
+    return f"{size_num:3.1f}TB"
+
+
+def main() -> None:
     # Iterate over platform to gather build information of available conda version.
     for platform in PLATFORMS:
         expected_builds = generate_expected_builds(platform)
 
         # Actual builds available in Conda
         stdout, stderr, return_code = run_command(
-            Commands.SEARCH, f"pytorch-test::*[name=pytorch version=1.11.* subdir={platform}]", "--json")
+            Commands.SEARCH, f"{CHANNEL}::*[name=pytorch version={VERSION} subdir={platform}]", "--json")
 
         if return_code != 0:
             raise Exception(stderr)
 
         available_versions = json.loads(stdout)
+        output_data = []
+        headers = ["File Name", "Date", "Size"]
         actual_builds = set()
         for version in available_versions["pytorch"]:
             actual_builds.add(version["build"])
+            output_data.append((
+                version["fn"],
+                datetime.fromtimestamp(version["timestamp"] / 1000),
+                size_format(version["size"])
+            ))
 
         assert len(expected_builds) > 0, "expected builds set should not be empty."
         assert expected_builds == actual_builds, (
             f"Missing following builds in conda: {expected_builds.difference(actual_builds)} for platform {platform}"
         )
 
-        print(f"Successfully verified all binaries are available in Conda for {platform}...")
+        print(f"\nSuccessfully verified following binaries are available in Conda for {platform}...")
+        print(tabulate(output_data, headers=headers, tablefmt="grid"))
 
 
 if __name__ == '__main__':
