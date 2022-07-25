ff781a13ec1ca78dd67d58186487bd8ddb4bb6ac
Jeff Daily <jeff.daily@amd.com>
2022-07-25 07:52:30 -0700

    [ROCm] add dependencies for rocm5.2 (#1033)
    * refactor manywheel/build_rocm.sh
    
    * update manywheel/build_rocm.sh for ROCm 5.2
    
    * update scripts and Dockerfile for ROCm 5.2
    
    * update rocblas aux file list
    
    * update rocm versions used for workflows
    
    * missing amdgpu repo for rocm 5.2

--- libtorch/build_all_docker.sh
+++ libtorch/build_all_docker.sh
@@ -8,6 +8,6 @@ for cuda_version in 11.7 11.6 11.5 11.3 10.2; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
 
-for rocm_version in 5.0 5.1.1; do
+for rocm_version in 5.1.1 5.2; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
