650386005eabf3c184977c871978772d7fe1d3b6
Ivan Yashchuk <IvanYashchuk@users.noreply.github.com>
2022-08-17 19:45:12 +0300

    Update build_magma.sh

--- magma/build_magma.sh
+++ magma/build_magma.sh
@@ -10,6 +10,8 @@ cp ${PACKAGE_FILES}/build.sh ${PACKAGE_DIR}/build.sh
 cp ${PACKAGE_FILES}/meta.yaml ${PACKAGE_DIR}/meta.yaml
 cp ${PACKAGE_FILES}/thread_queue.patch ${PACKAGE_DIR}/thread_queue.patch
 cp ${PACKAGE_FILES}/cmakelists.patch ${PACKAGE_DIR}/cmakelists.patch
+cp ${PACKAGE_FILES}/getrf_shfl.patch ${PACKAGE_DIR}/getrf_shfl.patch
+cp ${PACKAGE_FILES}/getrf_nbparam.patch ${PACKAGE_DIR}/getrf_nbparam.patch
 
 conda install -yq conda-build conda-verify
 . ./conda/switch_cuda_version.sh "${DESIRED_CUDA}"
