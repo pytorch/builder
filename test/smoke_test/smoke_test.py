2a04026c81aa66cb18467fe0ccf846ab99e925e1
atalman <atalman@fb.com>
2022-08-19 11:24:00 -0700

    modified smoke tests

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -14,19 +14,19 @@ import torchaudio.utils  # noqa: F401
 from torchaudio.io import StreamReader
 import torchvision.datasets as dset
 import torchvision.transforms
-is_cuda_system = os.environ['CUDA_VER'] != "cpu"
+cuda_version_expected = os.environ['CUDA_VER']
+is_cuda_system = cuda_version_expected != "cpu"
 #todo add torch, torchvision and torchaudio tests
 print(torch.__version__)
 print(torchvision.__version__)
 print(torchaudio.__version__)
 print('Is torchvision useable?', all(x is not None for x in [torch.ops.image.decode_png, torch.ops.torchvision.roi_align]))
-print(f"CUDA IS AVAILABLE: {torch.cuda.is_available()}")
 if(not torch.cuda.is_available() and is_cuda_system):
-    print("CUDA NOT AVAILABLE ON GPU")
+    print(f"Expected CUDA {cuda_version_expected}. However CUDA is not loaded.")
     sys.exit(1)
 if(torch.cuda.is_available()):
-    if(torch.version.cuda != os.environ['CUDA_VER']):
-        print("WRONG CUDA VERSION LOADED")
+    if(torch.version.cuda != cuda_version_expected):
+        print(f"Wrong CUDA version. Loaded: {torch.version.cuda} Expected: {cuda_version_expected}")
         sys.exit(1)
     y=torch.randn([3,5]).cuda()
     print(torch.version.cuda)
