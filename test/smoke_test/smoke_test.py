16c0759f8df72eb7e6a6132dcbfb56ee731030b4
atalman <atalman@fb.com>
2022-08-26 06:06:41 -0700

    enable audio smoke tests

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -2,24 +2,24 @@ import os
 import sys
 import torch
 import torchvision
-#import torchaudio
+import torchaudio
 import torch.nn as nn
-#import torchaudio.compliance.kaldi  # noqa: F401
-#import torchaudio.datasets  # noqa: F401
-#import torchaudio.functional  # noqa: F401
-#import torchaudio.models  # noqa: F401
-#import torchaudio.pipelines  # noqa: F401
-#import torchaudio.sox_effects  # noqa: F401
-#import torchaudio.transforms  # noqa: F401
-#import torchaudio.utils  # noqa: F401
-#import torchvision.datasets as dset
-#import torchvision.transforms
+import torchaudio.compliance.kaldi  # noqa: F401
+import torchaudio.datasets  # noqa: F401
+import torchaudio.functional  # noqa: F401
+import torchaudio.models  # noqa: F401
+import torchaudio.pipelines  # noqa: F401
+import torchaudio.sox_effects  # noqa: F401
+import torchaudio.transforms  # noqa: F401
+import torchaudio.utils  # noqa: F401
+import torchvision.datasets as dset
+import torchvision.transforms
 cuda_version_expected = os.environ['CUDA_VER']
 is_cuda_system = cuda_version_expected != "cpu" and cuda_version_expected != ""
 #todo add torch, torchvision and torchaudio tests
 print(f"torch: {torch.__version__}")
 print(f"torchvision: {torchvision.__version__}")
-#print(f"torchaudio: {torchaudio.__version__}")
+print(f"torchaudio: {torchaudio.__version__}")
 print('Is torchvision useable?', all(x is not None for x in [torch.ops.image.decode_png, torch.ops.torchvision.roi_align]))
 if(not torch.cuda.is_available() and is_cuda_system):
     print(f"Expected CUDA {cuda_version_expected}. However CUDA is not loaded.")
@@ -29,6 +29,6 @@ if(torch.cuda.is_available()):
         print(f"Wrong CUDA version. Loaded: {torch.version.cuda} Expected: {cuda_version_expected}")
         sys.exit(1)
     y=torch.randn([3,5]).cuda()
-    print(torch.version.cuda)
+    print(f"torch cuda: {torch.version.cuda}")
     #todo add cudnn version validation
-    print(torch.backends.cudnn.version())
+    print(f"torch cudnn: {torch.backends.cudnn.version()}")
