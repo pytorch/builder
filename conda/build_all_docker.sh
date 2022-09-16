31cdcc5aa51d9f3a0bab33955bbd543ebe120f85
T Coxon <97948946+tttc3@users.noreply.github.com>
2022-09-16 11:57:50 +0100

    Use TOPDIR in call to build_all_docker.sh

--- conda/build_all_docker.sh
+++ conda/build_all_docker.sh
@@ -5,5 +5,5 @@ set -eou pipefail
 TOPDIR=$(git rev-parse --show-toplevel)
 
 for CUDA_VERSION in 11.7 11.6 11.5 11.3 10.2 cpu; do
-  CUDA_VERSION="${CUDA_VERSION}" conda/build_docker.sh
+  CUDA_VERSION="${CUDA_VERSION}" ${TOPDIR}/conda/build_docker.sh
 done
