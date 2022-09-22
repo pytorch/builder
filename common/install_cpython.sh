83d07dd6744ebdf2fcf1d3741465e68fd34861f4
Nikita Shulga <nshulga@fb.com>
2022-09-22 16:33:24 -0700

    Move to rc2 of 3.11 python
    Need it to get several convenience functions

--- common/install_cpython.sh
+++ common/install_cpython.sh
@@ -67,9 +67,9 @@ function build_cpython {
     check_var $py_ver
     check_var $PYTHON_DOWNLOAD_URL
     local py_ver_folder=$py_ver
-    # Only beta version of 3.11 is available right now
+    # Only rc2 version of 3.11 is available right now
     if [ "$py_ver" = "3.11.0" ]; then
-        py_ver=$py_ver"b1"
+        py_ver=$py_ver"rc2"
     fi
     wget -q $PYTHON_DOWNLOAD_URL/$py_ver_folder/Python-$py_ver.tgz
     if [ $(lex_pyver $py_ver) -lt $(lex_pyver 3.3) ]; then
