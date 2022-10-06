6bc0bc280a2785d6d0e635935d848b491b4d6c55
Nikita Shulga <nshulga@fb.com>
2022-10-06 11:44:06 -0700

    Pin condaforge version
    Most recent version fails with  invalid cert error when trying to update
    python

--- build_aarch64_wheel.py
+++ build_aarch64_wheel.py
@@ -212,7 +212,7 @@ def install_condaforge_python(host: RemoteHost, python_version="3.8") -> None:
         install_condaforge(host, suffix="download/4.10.3-10/Miniforge3-4.10.3-10-Linux-aarch64.sh")
         host.run_cmd(f"conda install -y python={python_version} numpy pyyaml")
     else:
-        install_condaforge(host)
+        install_condaforge(host, suffix="download/4.11.0-4/Miniforge3-4.11.0-4-Linux-aarch64.sh")
         # Pytorch-1.10 or older are not compatible with setuptools=59.6 or newer
         host.run_cmd(f"conda install -y python={python_version} numpy pyyaml setuptools=59.5.0")
 
