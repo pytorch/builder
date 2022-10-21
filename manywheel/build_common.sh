02fe95c5c4d8b4980b03719e10d53f98d2c0f59d
Huy Do <huydhn@gmail.com>
2022-10-21 14:39:21 -0700

    Add sccache to manywheel binary build

--- manywheel/build_common.sh
+++ manywheel/build_common.sh
@@ -4,6 +4,58 @@
 set -ex
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
+# Courtesy of pytorch/.jenkins/pytorch/common_utils.sh
+#
+# - 1st arg:  code to add
+# - remaining args:  names of traps to modify
+#
+trap_add() {
+    trap_add_cmd=$1; shift || fatal "${FUNCNAME[0]} usage error"
+    for trap_add_name in "$@"; do
+        trap -- "$(
+            # helper fn to get existing trap command from output
+            # of trap -p
+            extract_trap_cmd() { printf '%s\n' "$3"; }
+            # print existing trap command with newline
+            eval "extract_trap_cmd $(trap -p "${trap_add_name}")"
+            # print the new trap command
+            printf '%s\n' "${trap_add_cmd}"
+        )" "${trap_add_name}" \
+            || fatal "unable to add to trap ${trap_add_name}"
+    done
+}
+# set the trace attribute for the above function.  this is
+# required to modify DEBUG or RETURN traps because functions don't
+# inherit them unless the trace attribute is set
+declare -f -t trap_add
+
+# Initialize sccache
+if which sccache > /dev/null; then
+    # Save sccache logs to file
+    sccache --stop-server > /dev/null  2>&1 || true
+    rm -f ~/sccache_error.log || true
+
+    export SCCACHE_IDLE_TIMEOUT=1200
+    export SCCACHE_ERROR_LOG=~/sccache_error.log
+    export RUST_LOG=sccache::server=error
+
+    # Report sccache stats for easier debugging
+    sccache --zero-stats
+
+    # Report sccache stats for easier debugging
+    sccache --zero-stats
+    function sccache_epilogue() {
+        echo "::group::Sccache Compilation Log"
+        echo '=================== sccache compilation log ==================='
+        python "$script_dir/print_sccache_log.py" ~/sccache_error.log 2>/dev/null
+        echo '=========== If your build fails, please take a look at the log above for possible reasons ==========='
+        sccache --show-stats
+        sccache --stop-server || true
+        echo "::endgroup::"
+    }
+
+    trap_add sccache_epilogue EXIT
+fi
 
 # Require only one python installation
 if [[ -z "$DESIRED_PYTHON" ]]; then
