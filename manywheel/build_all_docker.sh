e278838ea21efcf1e53a59731ba50daf2ed428b3
Jithun Nair <jithun.nair@amd.com>
2022-11-14 23:01:12 +0000

    Enable ROCm5.3 docker builds

--- manywheel/build_all_docker.sh
+++ manywheel/build_all_docker.sh
@@ -14,7 +14,7 @@ for cuda_version in 11.5 11.3 10.2; do
     MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
 
-for rocm_version in 5.1.1 5.2; do
+for rocm_version in 5.2 5.3; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/manywheel/build_docker.sh"
     MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
