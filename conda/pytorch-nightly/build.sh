f217c6a5c9bc0b7418ee80e750b01a0eb0f07a87
ptrblck <ptrblck@users.noreply.github.com>
2022-11-24 11:36:25 -0800

    Enable nightly CUDA 11.8 builds (#1206)
    * enable nightly builds for CUDA 11.8
    
    * add CUDA 11.8 version to manywheel, remove 11.3 and 11.5

--- conda/pytorch-nightly/build.sh
+++ conda/pytorch-nightly/build.sh
@@ -69,6 +69,13 @@ if [[ -n "$build_with_cuda" ]]; then
         export USE_STATIC_CUDNN=0
         #for cuda 11.7 include all dynamic loading libraries
         DEPS_LIST=(/usr/local/cuda/lib64/libcudnn*.so.8 /usr/local/cuda-11.7/extras/CUPTI/lib64/libcupti.so.11.7)
+    elif [[ $CUDA_VERSION == 11.8* ]]; then
+	export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6"
+	#for cuda 11.8 we use cudnn 8.5
+	#which does not have single static libcudnn_static.a deliverable to link with
+	export USE_STATIC_CUDNN=0
+	#for cuda 11.8 include all dynamic loading libraries
+	DEPS_LIST=(/usr/local/cuda/lib64/libcudnn*.so.8 /usr/local/cuda-11.8/extras/CUPTI/lib64/libcupti.so.11.8)
     fi
     export NCCL_ROOT_DIR=/usr/local/cuda
     export USE_STATIC_NCCL=1  # links nccl statically (driven by tools/setup_helpers/nccl.py, some of the NCCL cmake files such as FindNCCL.cmake and gloo/FindNCCL.cmake)
