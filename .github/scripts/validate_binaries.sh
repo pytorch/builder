1ca3efee33c26ab77bdb979d2195045f5038bf94
atalman <atalman@fb.com>
2022-12-05 11:56:28 -0800

    Windows fixes

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
+if [[ ${TARGET_OS} == 'windows' ]]; then
+    source /c/Jenkins/Miniconda3/etc/profile.d/conda.sh
+else
+    eval "$(conda shell.bash hook)"
+fi
+
 if [[ ${PACKAGE_TYPE} == "libtorch" ]]; then
     curl ${INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
-    eval "$(conda shell.bash hook)"
     conda create -y -n ${ENV_NAME} python=${DESIRED_PYTHON} numpy pillow
     conda activate ${ENV_NAME}
     export CONDA_LIBRARY_PATH="$(dirname $(which python))/../lib"
@@ -13,7 +18,7 @@ else
     INSTALLATION=${INSTALLATION/"conda install"/"conda install -y"}
     eval $INSTALLATION
     python  ./test/smoke_test/smoke_test.py
-    if [[ ${TARGET_OS} != 'macos' ]]; then
+    if [[ ${TARGET_OS} != 'macos' and ${TARGET_OS} != 'windows' ]]; then
         ${PWD}/check_binary.sh
     fi
 fi
