995744c37e16b10bf1ff635c95d9912aa4a65563
atalman <atalman@fb.com>
2022-12-05 08:17:58 -0800

    adding wondows use case

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env bash
 set -ex
-eval "$(conda shell.bash hook)"
-conda create -y -n ${ENV_NAME} python=${DESIRED_PYTHON} numpy pillow
-conda activate ${ENV_NAME}
-export CONDA_LIBRARY_PATH="$(dirname $(which python))/../lib"
-export LD_LIBRARY_PATH=$CONDA_LIBRARY_PATH:$LD_LIBRARY_PATH
 
 if [[ ${PACKAGE_TYPE} == "libtorch" ]]; then
     curl ${INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
+    eval "$(conda shell.bash hook)"
+    conda create -y -n ${ENV_NAME} python=${DESIRED_PYTHON} numpy pillow
+    conda activate ${ENV_NAME}
+    export CONDA_LIBRARY_PATH="$(dirname $(which python))/../lib"
+    export LD_LIBRARY_PATH=$CONDA_LIBRARY_PATH:$LD_LIBRARY_PATH
     INSTALLATION=${INSTALLATION/"conda install"/"conda install -y"}
     eval $INSTALLATION
     python  ./test/smoke_test/smoke_test.py
-    ${PWD}/check_binary.sh
+    if [[ ${TARGET_OS} != 'windows' ]]; then
+        ${PWD}/check_binary.sh
+    fi
 fi
