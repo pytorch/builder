e35024cfecdec0391ef7bbe78d08279484b07787
atalman <atalman@fb.com>
2022-12-05 07:14:56 -0800

    testing

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -ex
 
-conda create -y -n ${ENV_NAME} python=${{ matrix.python_version }} numpy pillow
+conda create -y -n ${ENV_NAME} python=${DESIRED_PYTHON} numpy pillow
 conda activate ${ENV_NAME}
 export CONDA_LIBRARY_PATH="$(dirname $(which python))/../lib"
 export LD_LIBRARY_PATH=$CONDA_LIBRARY_PATH:$LD_LIBRARY_PATH
 
-if [[ ${{ matrix.package_type }} == "libtorch" ]]; then
-    curl ${{ matrix.installation }} -o libtorch.zip
+if [[ ${PACKAGE_TYPE} == "libtorch" ]]; then
+    curl ${INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
     INSTALLATION=${INSTALLATION/"conda install"/"conda install -y"}
