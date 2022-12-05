0e2def4bd76164e8166de543e4015a3803052208
atalman <atalman@fb.com>
2022-12-05 09:09:10 -0800

    testing

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -5,10 +5,7 @@ if [[ ${PACKAGE_TYPE} == "libtorch" ]]; then
     curl ${INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
-    if [[ ${TARGET_OS} != 'windows' ]]; then
-        eval "$(conda shell.bash hook)"
-    fi
-
+    eval "$(conda shell.bash hook)"
     conda create -y -n ${ENV_NAME} python=${DESIRED_PYTHON} numpy pillow
     conda activate ${ENV_NAME}
     export CONDA_LIBRARY_PATH="$(dirname $(which python))/../lib"
@@ -16,7 +13,7 @@ else
     INSTALLATION=${INSTALLATION/"conda install"/"conda install -y"}
     eval $INSTALLATION
     python  ./test/smoke_test/smoke_test.py
-    if [[ ${TARGET_OS} != 'windows' ]]; then
+    if [[ ${TARGET_OS} != 'macos' ]]; then
         ${PWD}/check_binary.sh
     fi
 fi
