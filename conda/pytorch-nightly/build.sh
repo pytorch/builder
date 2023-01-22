b86675496ad53e072eccb1704640eb12f0044277
johnnynunez <johnnynuca14@gmail.com>
2023-01-22 12:36:12 +0100

    working cuda 12.0

--- conda/pytorch-nightly/build.sh
+++ conda/pytorch-nightly/build.sh
@@ -75,7 +75,7 @@ if [[ -n "$build_with_cuda" ]]; then
         #which does not have single static libcudnn_static.a deliverable to link with
         export USE_STATIC_CUDNN=0
         #for cuda 11.8 include all dynamic loading libraries
-	      DEPS_LIST=(/usr/local/cuda/lib64/libcudnn*.so.8 /usr/local/cuda-11.8/extras/CUPTI/lib64/libcupti.so.11.8)
+        DEPS_LIST=(/usr/local/cuda/lib64/libcudnn*.so.8 /usr/local/cuda-11.8/extras/CUPTI/lib64/libcupti.so.11.8)
     elif [[ $CUDA_VERSION == 12.0* ]]; then
         export TORCH_CUDA_ARCH_LIST="$TORCH_CUDA_ARCH_LIST;6.0;6.1;7.0;7.5;8.0;8.6;9.0"
         #for cuda 12.0 we use cudnn 8.7
