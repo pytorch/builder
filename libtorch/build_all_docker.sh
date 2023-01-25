f71393488bda52497c3b4a2d341cffb747e9cca1
Pruthvi Madugundu <pruthvigithub@gmail.com>
2023-01-25 00:36:57 -0800

    Update ROCm 5.4 nightly wheels

--- libtorch/build_all_docker.sh
+++ libtorch/build_all_docker.sh
@@ -8,6 +8,6 @@ for cuda_version in 11.8 11.7 11.6; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
 
-for rocm_version in 5.2 5.3; do
+for rocm_version in 5.3 5.4.2; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
