fa3269513a28179f5fca0fb1050b77cbe4a4d18c
Mike Schneider <xncqr@amazon.com>
2023-01-28 22:55:20 +0000

    put in glibfortran.a hack

--- aarch64_ci_build.sh
+++ aarch64_ci_build.sh
@@ -11,7 +11,7 @@ PATH=/opt/conda/bin:$PATH
 ###############################################################################
 # Install OS dependent packages
 ###############################################################################
-yum -y install less
+yum -y install less wget zstd
 
 ###############################################################################
 # Install conda
@@ -24,6 +24,20 @@ rm /mambaforge.sh
 /opt/conda/bin/conda install -y -c conda-forge python=${PYTHON_VERSION} numpy pyyaml setuptools patchelf
 python --version
 conda --version
+
+
+###############################################################################
+# Exec libglfortran.a hack
+#
+# libgfortran.a from quay.io/pypa/manylinux2014_aarch64 was not compiled with -fPIC.
+# This causes __stack_chk_guard@@GLIBC_2.17 on pytorch build. To solve, get
+# ubuntu's libgfortran.a which was compiled with -fPIC
+###############################################################################
+cd ~/
+wget wget http://ports.ubuntu.com/ubuntu-ports/pool/universe/g/gcc-10/libgfortran-10-dev_10.4.0-6ubuntu1_arm64.deb
+ar x libgfortran-10-dev_10.4.0-6ubuntu1_arm64.deb
+tar --use-compress-program=unzstd -xvf data.tar.zst
+cp -f ~/usr/lib/gcc/aarch64-linux-gnu/10/libgfortran.a /opt/rh/devtoolset-10/root/usr/lib/gcc/aarch64-redhat-linux/10/
 
 ###############################################################################
 # Run aarch64 builder python
