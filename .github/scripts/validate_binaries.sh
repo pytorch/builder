a2d2429df5d47683c83c893891ba67ae8edf4ad5
atalman <atalman@fb.com>
2023-01-31 06:19:57 -0800

    Add pypi with cudnn package test

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -2,6 +2,7 @@ if [[ ${MATRIX_PACKAGE_TYPE} == "libtorch" ]]; then
     curl ${MATRIX_INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
+    #special case for Python 3.11
     if [ $MATRIX_PYTHON_VERSION == '3.11' ]; then
         export CPYTHON_VERSIONS=3.11.0
         sudo yum -y install openssl-devel libssl-dev bzip2-devel libffi-devel
@@ -15,6 +16,17 @@ else
         eval ${PIP_INSTALLATION}
         eval ${PYTHON_PATH}/python ./test/smoke_test/smoke_test.py --package torchonly
     else
+
+        # Special case Pypi installation package, only applicable to linux nightly CUDA 11.7 builds, wheel package
+        if [ ${TARGET_OS} == 'linux' and ${MATRIX_CHANNEL} == 'nightly' and ${MATRIX_GPU_ARCH_VERSION} == '11.7' and ${MATRIX_PACKAGE_TYPE} == 'manywheel' ];
+            conda create -yp ${ENV_NAME}_pypi python=${MATRIX_PYTHON_VERSION} numpy
+            INSTALLATION_PYPI=${MATRIX_INSTALLATION/"cu117"/"cu117_pypi_cudnn"}
+            conda run -p ${ENV_NAME}_pypi $INSTALLATION_PYPI
+            python ./test/smoke_test/smoke_test.py --package
+            conda activate base
+            conda env remove -p ${ENV_NAME}_pypi
+        fi
+
         conda create -y -n ${ENV_NAME} python=${MATRIX_PYTHON_VERSION} numpy pillow
         conda activate ${ENV_NAME}
         INSTALLATION=${MATRIX_INSTALLATION/"conda install"/"conda install -y"}
