51ca1f5ddb4eeff193a47a5f02a6264cb614c859
Andrey Talman <atalman@fb.com>
2023-02-01 15:00:01 -0500

    Add pypi with cudnn package test (#1289)
    * Add pypi with cudnn package test
    
    * Add pypi with cudnn package test
    
    * test
    
    * test
    
    * More pypi cudnn changes
    
    * test
    
    * Fix pipy smoke test
    
    * Remove debug comments

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -2,6 +2,7 @@ if [[ ${MATRIX_PACKAGE_TYPE} == "libtorch" ]]; then
     curl ${MATRIX_INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
+    #special case for Python 3.11
     if [ $MATRIX_PYTHON_VERSION == '3.11' ]; then
         export CPYTHON_VERSIONS=3.11.0
         sudo yum -y install openssl-devel libssl-dev bzip2-devel libffi-devel
@@ -15,6 +16,19 @@ else
         eval ${PIP_INSTALLATION}
         eval ${PYTHON_PATH}/python ./test/smoke_test/smoke_test.py --package torchonly
     else
+
+        # Special case Pypi installation package, only applicable to linux nightly CUDA 11.7 builds, wheel package
+        if [[ ${TARGET_OS} == 'linux' && ${MATRIX_CHANNEL} == 'nightly' && ${MATRIX_GPU_ARCH_VERSION} == '11.7' && ${MATRIX_PACKAGE_TYPE} == 'manywheel' ]]; then
+            conda create -yp ${ENV_NAME}_pypi python=${MATRIX_PYTHON_VERSION} numpy
+            INSTALLATION_PYPI=${MATRIX_INSTALLATION/"cu117"/"cu117_pypi_cudnn"}
+            INSTALLATION_PYPI=${INSTALLATION_PYPI/"torchvision torchaudio"/""}
+            INSTALLATION_PYPI=${INSTALLATION_PYPI/"index-url"/"extra-index-url"}
+            conda run -p ${ENV_NAME}_pypi ${INSTALLATION_PYPI}
+            conda run -p ${ENV_NAME}_pypi python ./test/smoke_test/smoke_test.py --package torchonly
+            conda activate base
+            conda env remove -p ${ENV_NAME}_pypi
+        fi
+
         conda create -y -n ${ENV_NAME} python=${MATRIX_PYTHON_VERSION} numpy pillow
         conda activate ${ENV_NAME}
         INSTALLATION=${MATRIX_INSTALLATION/"conda install"/"conda install -y"}
