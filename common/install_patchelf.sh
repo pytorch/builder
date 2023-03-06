0cd3b66fe6c2d86336197f5ce6dc95792abf42e8
Huy Do <huydhn@gmail.com>
2023-03-05 20:25:46 -0800

    Fix comment typo

--- common/install_patchelf.sh
+++ common/install_patchelf.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-# Pin the version to latest release 0.17.2, newer version starts failing
-# to build on the current image
+# Pin the version to latest release 0.17.2, building newer commit starts
+# to fail on the current image
 git clone -b 0.17.2 --single-branch https://github.com/NixOS/patchelf
 cd patchelf
 sed -i 's/serial/parallel/g' configure.ac
