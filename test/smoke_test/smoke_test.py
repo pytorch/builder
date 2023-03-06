682704ff6c9b2a8019e6b04ab36b82ab1a5ce4d3
atalman <atalman@fb.com>
2023-03-06 15:06:28 -0800

    fix typo

--- test/smoke_test/smoke_test.py
+++ test/smoke_test/smoke_test.py
@@ -107,7 +107,7 @@ def smoke_test_cuda(package: str) -> None:
         print(f"cuDNN enabled? {torch.backends.cudnn.enabled}")
 
         # torch.compile is available only on Linux and python 3.8-3.10
-        if (sys.platform == "linux" or sys.platform == "linux2") and sys.version_info < (3, 11, 0)::
+        if (sys.platform == "linux" or sys.platform == "linux2") and sys.version_info < (3, 11, 0):
             smoke_test_compile()
 
         # This check has to be run last, since its messing up CUDA runtime
