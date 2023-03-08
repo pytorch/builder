7e3d105a3d444d6170a93053961d6dff6c1a0215
Andrey Talman <atalman@fb.com>
2023-03-08 16:28:19 -0500

    Update aarch64_linux/build_aarch64_wheel.py
    Co-authored-by: Nikita Shulga <nikita.shulga@gmail.com>

--- aarch64_linux/build_aarch64_wheel.py
+++ aarch64_linux/build_aarch64_wheel.py
@@ -519,7 +519,7 @@ def start_build(host: RemoteHost, *,
         build_date = host.check_output("cd pytorch ; git log --pretty=format:%s -1").strip().split()[0].replace("-", "")
         version = host.check_output("cat pytorch/version.txt").strip()[:-2]
         build_vars += f"BUILD_TEST=0 PYTORCH_BUILD_VERSION={version}.dev{build_date} PYTORCH_BUILD_NUMBER=1"
-    if branch.startswith("v"):
+    if branch.startswith("v1.") or branch.startswith("v2."):
         build_vars += f"BUILD_TEST=0 PYTORCH_BUILD_VERSION={branch[1:branch.find('-')]} PYTORCH_BUILD_NUMBER=1"
     if host.using_docker():
         build_vars += " CMAKE_SHARED_LINKER_FLAGS=-Wl,-z,max-page-size=0x10000"
