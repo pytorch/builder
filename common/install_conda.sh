d4082a3230d7088008a0f5698e0818660a61f12d
Huy Do <huydhn@gmail.com>
2023-03-13 10:37:25 -0700

    Revert "Upgrade cmake version to 3.22.1 to build triton (#1331)"
    This reverts commit 18c5017d9eedd2e5e203608e9ad6c05a2324b3ac.

--- common/install_conda.sh
+++ common/install_conda.sh
@@ -9,7 +9,5 @@ chmod +x  Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -p /opt/conda
 rm Miniconda3-latest-Linux-x86_64.sh
 export PATH=/opt/conda/bin:$PATH
-# cmake-3.22.1 from conda, same as the one used by PyTorch CI. The system cmake
-# is too old to build triton
-conda install -y conda-build anaconda-client git ninja cmake=3.22.1
+conda install -y conda-build anaconda-client git ninja
 conda remove -y --force patchelf
--- common/install_patchelf.sh
+++ common/install_patchelf.sh
@@ -2,9 +2,7 @@
 
 set -ex
 
-# Pin the version to latest release 0.17.2, building newer commit starts
-# to fail on the current image
-git clone -b 0.17.2 --single-branch https://github.com/NixOS/patchelf
+git clone https://github.com/NixOS/patchelf
 cd patchelf
 sed -i 's/serial/parallel/g' configure.ac
 ./bootstrap.sh
