740a721253ad000ff461ad240b8268a24f752d04
atalman <atalman@fb.com>
2023-03-14 10:10:42 -0700

    test

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -2,34 +2,29 @@ if [[ ${MATRIX_PACKAGE_TYPE} == "libtorch" ]]; then
     curl ${MATRIX_INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
+    if [[ ${MATRIX_INSTALLATION}=='pip3 install torch torchvision torchaudio' and ${MATRIX_GPU_ARCH_TYPE} == 'cpu']]
+        MATRIX_INSTALLATION='pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu'
+    fi
+    if [[ ${MATRIX_INSTALLATION}=='pip3 install torch torchvision torchaudio' and ${MATRIX_GPU_ARCH_VERSION} == '11.7']]
+        MATRIX_INSTALLATION='pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu117'
+    fi
+
     #special case for Python 3.11
-    if [[ ${MATRIX_PYTHON_VERSION} == '3.11' ]]; then
+    if [[ ${MATRIX_PYTHON_VERSION} == '3.11' && ${MATRIX_PACKAGE_TYPE} == 'manywheel' ]]; then
         conda create -y -n ${ENV_NAME} python=${MATRIX_PYTHON_VERSION}
         conda activate ${ENV_NAME}
 
-        INSTALLATION=${MATRIX_INSTALLATION/"-c pytorch"/"-c malfet -c pytorch"}
-        INSTALLATION=${INSTALLATION/"pytorch-cuda"/"pytorch-${MATRIX_CHANNEL}::pytorch-cuda"}
-        INSTALLATION=${INSTALLATION/"conda install"/"conda install -y"}
+        #INSTALLATION=${MATRIX_INSTALLATION/"-c pytorch"/"-c malfet -c pytorch"}
+        #INSTALLATION=${INSTALLATION/"pytorch-cuda"/"pytorch::pytorch-cuda"}
+        INSTALLATION=${MATRIX_INSTALLATION/"conda install"/"conda install -y"}
 
         eval $INSTALLATION
         python ./test/smoke_test/smoke_test.py
         conda deactivate
         conda env remove -n ${ENV_NAME}
+    elif [[ ${MATRIX_PYTHON_VERSION} == '3.11' && ${MATRIX_PACKAGE_TYPE} != 'manywheel' ]]
+        # do nothing
     else
-
-
-
-        # Special case Pypi installation package, only applicable to linux nightly CUDA 11.7 builds, wheel package
-        if [[ ${TARGET_OS} == 'linux' && ${MATRIX_GPU_ARCH_VERSION} == '11.7' && ${MATRIX_PACKAGE_TYPE} == 'manywheel' && ${MATRIX_CHANNEL} != 'nightly' ]]; then
-            conda create -yp ${ENV_NAME}_pypi python=${MATRIX_PYTHON_VERSION} numpy ffmpeg
-            INSTALLATION_PYPI=${MATRIX_INSTALLATION/"cu117"/"cu117_pypi_cudnn"}
-            INSTALLATION_PYPI=${INSTALLATION_PYPI/"torchvision torchaudio"/""}
-            INSTALLATION_PYPI=${INSTALLATION_PYPI/"index-url"/"extra-index-url"}
-            conda run -p ${ENV_NAME}_pypi ${INSTALLATION_PYPI}
-            conda run -p ${ENV_NAME}_pypi python ./test/smoke_test/smoke_test.py --package torchonly
-            conda deactivate
-            conda env remove -p ${ENV_NAME}_pypi
-        fi
 
         # Please note ffmpeg is required for torchaudio, see https://github.com/pytorch/pytorch/issues/96159
         conda create -y -n ${ENV_NAME} python=${MATRIX_PYTHON_VERSION} numpy ffmpeg
