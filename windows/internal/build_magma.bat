b25f4e925d582770484f2c3a5dd55516d4e03113
pbialecki <piotr.bialecki@hotmail.de>
2023-04-04 03:14:10 -0700

    add CUDA version checks for Windows MAGMA builds

--- windows/internal/build_magma.bat
+++ windows/internal/build_magma.bat
@@ -35,8 +35,15 @@ cd magma
 mkdir build && cd build
 
 set GPU_TARGET=All
-set CUDA_ARCH_LIST= -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70
-set "CUDA_ARCH_LIST=%CUDA_ARCH_LIST% -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86"
+if "%CUVER_NODOT%" == "121" (
+  set CUDA_ARCH_LIST=-gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86 -gencode arch=compute_90,code=sm_90
+)
+if "%CUVER_NODOT%" == "118" (
+  set CUDA_ARCH_LIST= -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86 -gencode arch=compute_90,code=sm_90
+)
+if "%CUVER_NODOT%" == "117" (
+  set CUDA_ARCH_LIST= -gencode arch=compute_37,code=sm_37 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_60,code=sm_60 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_80,code=sm_80 -gencode arch=compute_86,code=sm_86
+)
 
 set CC=cl.exe
 set CXX=cl.exe
