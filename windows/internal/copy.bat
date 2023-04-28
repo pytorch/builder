87abd9a6a2b200158d57a32d9eb9c10e6d0e6410
atalman <atalman@fb.com>
2023-04-27 17:19:58 -0700

    Fix nvjitlink inclusion in 12.1 wheels

--- windows/internal/copy.bat
+++ windows/internal/copy.bat
@@ -7,6 +7,7 @@ copy "%CUDA_PATH%\bin\cusolver*64_*.dll*" pytorch\torch\lib
 
 copy "%CUDA_PATH%\bin\cudnn*64_*.dll*" pytorch\torch\lib
 copy "%CUDA_PATH%\bin\nvrtc*64_*.dll*" pytorch\torch\lib
+copy "%CUDA_PATH%\bin\nvrtc*64_*.dll*" pytorch\torch\lib
 copy "%CUDA_PATH%\extras\CUPTI\lib64\cupti64_*.dll*" pytorch\torch\lib
 
 copy "C:\Program Files\NVIDIA Corporation\NvToolsExt\bin\x64\nvToolsExt64_1.dll*" pytorch\torch\lib
@@ -17,4 +18,9 @@ copy "%libuv_ROOT%\bin\uv.dll" pytorch\torch\lib
 ::copy zlib if it exist in windows/system32
 if exist "C:\Windows\System32\zlibwapi.dll" (
     copy "C:\Windows\System32\zlibwapi.dll"  pytorch\torch\lib
+)
+
+::copy nvJitLink dll is requires for cuda 12+
+if exist "%CUDA_PATH%\bin\nvJitLink_*.dll*" (
+    copy "%CUDA_PATH%\bin\nvJitLink_*.dll*"  pytorch\torch\lib
 )
