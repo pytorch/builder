3237101a0a709aec3111e7d00718e63114fd9f6c
Bo Li <110066325+BLOrange-AMD@users.noreply.github.com>
2023-05-23 15:44:17 -0500

    Upgrade nightly wheels to rocm5.5 (#1407)
    * Add MIOpen db files to wheel
    
    * Update magma commits for various branches to include header path updates
    
    * Add ROCm5.5 support with Navi31-tuned MIOpen branch
    
    * Upgrade nightly wheels to rocm5.5
    
    * Update build_docker.sh for gfx1100
    
    * Update build_docker.sh for gfx1100
    
    ---------
    
    Co-authored-by: Jithun Nair <jithun.nair@amd.com>
    Co-authored-by: Jithun Nair <37884920+jithunnair-amd@users.noreply.github.com>

--- libtorch/build_all_docker.sh
+++ libtorch/build_all_docker.sh
@@ -8,6 +8,6 @@ for cuda_version in 12.1 11.8 11.7; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
 
-for rocm_version in 5.3 5.4.2; do
+for rocm_version in 5.4.2 5.5; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
--- manywheel/build_all_docker.sh
+++ manywheel/build_all_docker.sh
@@ -14,7 +14,7 @@ for cuda_version in 11.8 11.7; do
     MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
 
-for rocm_version in 5.3 5.4.2; do
+for rocm_version in 5.4.2 5.5; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/manywheel/build_docker.sh"
     MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
