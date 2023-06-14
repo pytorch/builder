5b310b426f8abf4777dac6d7b177029263ab7b07
Andrey Talman <atalman@fb.com>
2023-06-14 08:30:28 -0400

    Add poetry and pypi tests (#1419)
    * smoke test poetry
    
    Add a little more tests
    
    test
    
    Test poetry
    
    test
    
    Test poetry on python 3.10
    
    Add more poetry tests
    
    Test en us
    
    test
    
    test
    
    Try verboose
    
    testing
    
    testing
    
    try quiet install
    
    Code refactooring
    
    test
    
    move linux pipy validation to workflow
    
    test
    
    test
    
    Fix path
    
    try test pipy
    
    More torch installations
    
    test
    
    testing
    
    test
    
    test
    
    test new
    
    fix install 2
    
    try poetry nightly
    
    test nightly
    
    test
    
    test
    
    Test poetry validation
    
    test
    
    test_new
    
    test
    
    * Put back executing this on pull

--- .github/scripts/validate_binaries.sh
+++ .github/scripts/validate_binaries.sh
@@ -2,18 +2,6 @@ if [[ ${MATRIX_PACKAGE_TYPE} == "libtorch" ]]; then
     curl ${MATRIX_INSTALLATION} -o libtorch.zip
     unzip libtorch.zip
 else
-    # Special case Pypi installation package, only applicable to linux nightly CUDA 11.7 builds, wheel package
-    if [[ ${TARGET_OS} == 'linux' && ${MATRIX_GPU_ARCH_VERSION} == '11.7' && ${MATRIX_PACKAGE_TYPE} == 'manywheel' && ${MATRIX_CHANNEL} != 'nightly' ]]; then
-        conda create -yp ${ENV_NAME}_pypi python=${MATRIX_PYTHON_VERSION} numpy ffmpeg
-        INSTALLATION_PYPI=${MATRIX_INSTALLATION/"cu117"/"cu117_pypi_cudnn"}
-        INSTALLATION_PYPI=${INSTALLATION_PYPI/"torchvision torchaudio"/""}
-        INSTALLATION_PYPI=${INSTALLATION_PYPI/"index-url"/"extra-index-url"}
-        conda run -p ${ENV_NAME}_pypi ${INSTALLATION_PYPI}
-        conda run -p ${ENV_NAME}_pypi python ./test/smoke_test/smoke_test.py --package torchonly
-        conda deactivate
-        conda env remove -p ${ENV_NAME}_pypi
-    fi
-
     # Please note ffmpeg is required for torchaudio, see https://github.com/pytorch/pytorch/issues/96159
     conda create -y -n ${ENV_NAME} python=${MATRIX_PYTHON_VERSION} numpy ffmpeg
     conda activate ${ENV_NAME}
