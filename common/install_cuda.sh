941be28cb5c686dc41b7ea8681701e64192c3002
ptrblck <ptrblck@users.noreply.github.com>
2023-08-11 21:20:54 -0700

    update to CUDA 12.1U1 (#1476)
    Should fix  pytorch/pytorch#94772 in wheel builds

--- common/install_cuda.sh
+++ common/install_cuda.sh
@@ -37,10 +37,10 @@ function install_121 {
     echo "Installing CUDA 12.1 and cuDNN 8.9 and NCCL 2.18.1"
     rm -rf /usr/local/cuda-12.1 /usr/local/cuda
     # install CUDA 12.1.0 in the same container
-    wget -q https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda_12.1.0_530.30.02_linux.run
-    chmod +x cuda_12.1.0_530.30.02_linux.run
-    ./cuda_12.1.0_530.30.02_linux.run --toolkit --silent
-    rm -f cuda_12.1.0_530.30.02_linux.run
+    wget -q https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda_12.1.1_530.30.02_linux.run
+    chmod +x cuda_12.1.1_530.30.02_linux.run
+    ./cuda_12.1.1_530.30.02_linux.run --toolkit --silent
+    rm -f cuda_12.1.1_530.30.02_linux.run
     rm -f /usr/local/cuda && ln -s /usr/local/cuda-12.1 /usr/local/cuda
 
     # cuDNN license: https://developer.nvidia.com/cudnn/license_agreement
