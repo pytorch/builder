a414219beff5961ee40af86fe1db31c54982c240
Nikita Shulga <nshulga@meta.com>
2023-10-23 20:49:50 -0700

    [aarch64_wheel] Flake8 fix

--- aarch64_linux/aarch64_wheel_ci_build.py
+++ aarch64_linux/aarch64_wheel_ci_build.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
 
     # work around to fix Raspberry pie crash
     print("Applying mkl-dnn patch to fix Raspberry pie crash")
-    os.system(f"cd /pytorch/third_party/ideep/mkl-dnn && patch -p1 < /builder/mkldnn_fix/aarch64-fix-default-build-flags-to-armv8-a.patch")
+    os.system("cd /pytorch/third_party/ideep/mkl-dnn && patch -p1 < /builder/mkldnn_fix/aarch64-fix-default-build-flags-to-armv8-a.patch")
     os.system(f"cd /pytorch; {build_vars} python3 setup.py bdist_wheel")
     pytorch_wheel_name = complete_wheel("pytorch")
     print(f"Build Compelete. Created {pytorch_wheel_name}..")
