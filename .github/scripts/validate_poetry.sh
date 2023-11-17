70ae396c158458cb40cd253afd61a07c8af23f97
atalman <atalman@fb.com>
2023-11-17 13:23:45 -0800

    poetry_fix

--- .github/scripts/validate_poetry.sh
+++ .github/scripts/validate_poetry.sh
@@ -19,16 +19,14 @@ if [[ ! -z ${RELEASE_VERSION} ]]; then
     RELEASE_SUFFIX="@${RELEASE_VERSION}"
 fi
 
-# Installing poetry from our custom repo. We need to configure it before use and disable authentication
-export PYTHON_KEYRING_BACKEND=keyring.backends.null.Keyring
-poetry source add --priority=explicit pytorch "https://download.pytorch.org/whl/${MATRIX_DESIRED_CUDA}"
+
 if [[ ${TORCH_ONLY} == 'true' ]]; then
     poetry --quiet add torch${RELEASE_SUFFIX}
 else
     poetry --quiet add --source pytorch torch${RELEASE_SUFFIX} torchaudio torchvision
 fi
 
-python ../test/smoke_test/smoke_test.py ${TEST_SUFFIX} --runtime-error-check disabled
+python ../test/smoke_test/smoke_test.py --package torchonly --runtime-error-check disabled
 conda deactivate
 conda env remove -p ${ENV_NAME}_poetry
 cd ..
