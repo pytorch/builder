ae85e4cbb9ad276cd9229ef7f8c454b25b36c825
Andrey Talman <atalman@fb.com>
2023-12-11 21:57:35 +0000

    Release 212 testing (#1638)
    * Revert "Validate pypi build only for release (#1624)"
    
    This reverts commit 56556d0aaca4da61c0497608b9136b058573c8d6.
    
    * Revert "Validate pypi build only for release (#1623)"
    
    This reverts commit 4db3d68b5a4c3b87148ba394d31960ff805c3850.

--- .github/scripts/validate_pipy.sh
+++ .github/scripts/validate_pipy.sh
@@ -10,9 +10,14 @@ fi
 
 if [[ ${TORCH_ONLY} == 'true' ]]; then
     TEST_SUFFIX=" --package torchonly"
-    pip3 install torch${RELEASE_SUFFIX}
+    pip3 install --pre torch${RELEASE_SUFFIX} --extra-index-url "https://download.pytorch.org/whl/${MATRIX_CHANNEL}/${MATRIX_DESIRED_CUDA}_pypi_cudnn"
 else
-    pip3 install torch${RELEASE_SUFFIX} torchvision torchaudio
+    if [[ ${MATRIX_CHANNEL} != "release" ]]; then
+        pip3 install --pre torch${RELEASE_SUFFIX}  --extra-index-url "https://download.pytorch.org/whl/${MATRIX_CHANNEL}/${MATRIX_DESIRED_CUDA}_pypi_cudnn"
+        pip3 install --pre torchvision torchaudio --extra-index-url "https://download.pytorch.org/whl/${MATRIX_CHANNEL}/${MATRIX_DESIRED_CUDA}"
+    else
+        pip3 install torch${RELEASE_SUFFIX}  torchvision torchaudio
+    fi
 fi
 
 python ./test/smoke_test/smoke_test.py ${TEST_SUFFIX} --runtime-error-check disabled
