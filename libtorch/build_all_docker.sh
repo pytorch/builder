94d243977055eab4aa61f641376f87f0aae77b70
Jithun Nair <jithun.nair@amd.com>
2023-12-19 06:12:29 +0000

    Update ROCm versions for docker images

--- libtorch/build_all_docker.sh
+++ libtorch/build_all_docker.sh
@@ -8,6 +8,6 @@ for cuda_version in 12.1 11.8; do
     GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
 
-for rocm_version in 5.6 5.7; do
+for rocm_version in 5.7 6.0; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/libtorch/build_docker.sh"
 done
--- manywheel/build_all_docker.sh
+++ manywheel/build_all_docker.sh
@@ -16,7 +16,7 @@ for cuda_version in 12.1 11.8; do
     MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=cuda GPU_ARCH_VERSION="${cuda_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
 
-for rocm_version in 5.6 5.7; do
+for rocm_version in 5.7 6.0; do
     GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/manywheel/build_docker.sh"
     MANYLINUX_VERSION=2014 GPU_ARCH_TYPE=rocm GPU_ARCH_VERSION="${rocm_version}" "${TOPDIR}/manywheel/build_docker.sh"
 done
