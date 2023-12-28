27c47026e4108f8e2f02892ab5062a42f28a3160
Andrey Talman <atalman@fb.com>
2023-12-28 17:56:03 +0000

    Add test ops validation for validation workflows (#1650)
    * Add test ops validation
    
    * include workflows

---
+++ .github/scripts/validate_test_ops.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+retry () {
+    $*  || (sleep 1 && $*) || (sleep 2 && $*) || (sleep 4 && $*) || (sleep 8 && $*)
+}
+
+# Clone the Pytorch branch
+retry git clone --depth 1 https://github.com/pytorch/pytorch.git
+retry git submodule update --init --recursive
+pushd pytorch
+
+# Run test_ops validation
+export CUDA_LAUNCH_BLOCKING=1
+python3 test/test_ops.py
