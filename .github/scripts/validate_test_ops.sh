50cb7c5c54556cb1376b5f706de10ea2d8c1ac71
Andrey Talman <atalman@fb.com>
2023-12-28 21:24:21 +0000

    Add test ops validation for validation workflows (#1653)

--- .github/scripts/validate_test_ops.sh
+++ .github/scripts/validate_test_ops.sh
@@ -6,12 +6,19 @@ retry () {
     $*  || (sleep 1 && $*) || (sleep 2 && $*) || (sleep 4 && $*) || (sleep 8 && $*)
 }
 
+BRANCH = "@main"
+if [[ ${MATRIX_CHANNEL} == "test" ]]
+    SHORT_VERSION=${MATRIX_STABLE_VERSION%.*}
+    BRANCH="@release/${SHORT_VERSION}"
+fi
+
+
 # Clone the Pytorch branch
-retry git clone --depth 1 https://github.com/pytorch/pytorch.git
+retry git clone --depth 1 https://github.com/pytorch/pytorch.git${BRANCH}
 retry git submodule update --init --recursive
 pushd pytorch
 
-pip install expecttest pyyaml jinja2
+pip install expecttest pyyaml jinja2 packaging
 
 # Run test_ops validation
 export CUDA_LAUNCH_BLOCKING=1
