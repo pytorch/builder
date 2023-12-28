bbfccae5c56fa236c3cd91ac6bd0077617e7971e
atalman <atalman@fb.com>
2023-12-28 12:31:28 -0800

    Add test ops validation for validation workflows

--- .github/scripts/validate_test_ops.sh
+++ .github/scripts/validate_test_ops.sh
@@ -11,6 +11,8 @@ retry git clone --depth 1 https://github.com/pytorch/pytorch.git
 retry git submodule update --init --recursive
 pushd pytorch
 
+pip install expecttest pyyaml jinja2
+
 # Run test_ops validation
 export CUDA_LAUNCH_BLOCKING=1
 python3 test/test_ops.py
