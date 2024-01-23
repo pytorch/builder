ed7cef3cf6883ec907c8933649d6ac3243dd1ea4
Andrey Talman <atalman@fb.com>
2024-01-23 17:18:45 -0500

    Revert "Revert "Dynamic MKL windows""

--- windows/internal/copy.bat
+++ windows/internal/copy.bat
@@ -11,6 +11,10 @@ copy "%CUDA_PATH%\extras\CUPTI\lib64\cupti64_*.dll*" pytorch\torch\lib
 
 copy "C:\Program Files\NVIDIA Corporation\NvToolsExt\bin\x64\nvToolsExt64_1.dll*" pytorch\torch\lib
 copy "%CONDA_LIB_PATH%\libiomp*5md.dll" pytorch\torch\lib
+IF "%PACKAGE_TYPE%"=="libtorch" (
+    copy "%CONDA_LIB_PATH%\mkl_intel_thread.1.dll" pytorch\torch\lib
+    copy "%CONDA_LIB_PATH%\mkl_core.1.dll" pytorch\torch\lib
+)
 :: Should be set in build_pytorch.bat
 copy "%libuv_ROOT%\bin\uv.dll" pytorch\torch\lib
 
--- windows/internal/copy_cpu.bat
+++ windows/internal/copy_cpu.bat
@@ -1,3 +1,8 @@
 copy "%CONDA_LIB_PATH%\libiomp*5md.dll" pytorch\torch\lib
 :: Should be set in build_pytorch.bat
 copy "%libuv_ROOT%\bin\uv.dll" pytorch\torch\lib
+
+IF "%PACKAGE_TYPE%"=="libtorch" (
+    copy "%CONDA_LIB_PATH%\mkl_intel_thread.1.dll" pytorch\torch\lib
+    copy "%CONDA_LIB_PATH%\mkl_core.1.dll" pytorch\torch\lib
+)
