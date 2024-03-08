b7d68efee28e1ca346d98d50de1a4db856a17115
pbialecki <piotr.bialecki@hotmail.de>
2024-03-08 08:00:42 -0800

    add 12.4 workflow for docker image build

--- conda/build_all_docker.sh
+++ conda/build_all_docker.sh
@@ -4,6 +4,6 @@ set -eou pipefail
 
 TOPDIR=$(git rev-parse --show-toplevel)
 
-for CUDA_VERSION in 12.1 11.8 cpu; do
+for CUDA_VERSION in 12.4 12.1 11.8 cpu; do
   CUDA_VERSION="${CUDA_VERSION}" conda/build_docker.sh
 done
