c9fb8c7813624bce2af3df50689dc44349cd8864
atalman <atalman@fb.com>
2024-05-13 08:23:50 -0700

    [BE] Remove unused files and dead code

--- .circleci/scripts/binary_populate_env.sh
+++ .circleci/scripts/binary_populate_env.sh
@@ -143,7 +143,6 @@ export BUILD_JNI=$BUILD_JNI
 export PIP_UPLOAD_FOLDER="$PIP_UPLOAD_FOLDER"
 export DOCKER_IMAGE="$DOCKER_IMAGE"
 
-# Remove WORKD_DIR, PYTORCH_ROOT, BUILDER_ROOT defined & persisted in binary_checkout.sh
 export MAC_PACKAGE_WORK_DIR="$WORK_DIR"
 export MINICONDA_ROOT="$WORK_DIR/miniconda"
 export PYTORCH_FINAL_PACKAGE_DIR="$WORK_DIR/final_pkgs"
