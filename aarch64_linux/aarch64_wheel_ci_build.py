d7ffad81c370f2ae484f0b99f2a4454eff547ff8
Ting Lu <tingl@nvidia.com>
2024-05-20 06:37:40 -0700

    Use export USE_PRIORITIZED_TEXT_FOR_LD=1 instead of command line

--- aarch64_linux/aarch64_wheel_ci_build.py
+++ aarch64_linux/aarch64_wheel_ci_build.py
@@ -199,7 +199,8 @@ if __name__ == "__main__":
         branch = "master"
 
     print("Building PyTorch wheel")
-    build_vars = "MAX_JOBS=5 USE_PRIORITIZED_TEXT_FOR_LD=1 CMAKE_SHARED_LINKER_FLAGS=-Wl,-z,max-page-size=0x10000 "
+    os.system("export USE_PRIORITIZED_TEXT_FOR_LD=1")
+    build_vars = "MAX_JOBS=5 CMAKE_SHARED_LINKER_FLAGS=-Wl,-z,max-page-size=0x10000 "
     os.system("cd /pytorch; python setup.py clean")
 
     override_package_version = os.getenv("OVERRIDE_PACKAGE_VERSION")
