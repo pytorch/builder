2828e336eedb1c97484ead54bec63f7962acab93
Ting Lu <tingl@nvidia.com>
2024-05-29 23:45:27 -0700

    pick one way

--- aarch64_linux/aarch64_ci_build.sh
+++ aarch64_linux/aarch64_ci_build.sh
@@ -33,5 +33,6 @@ if [ "$DESIRED_CUDA" = "cpu" ]; then
     python /builder/aarch64_linux/aarch64_wheel_ci_build.py --enable-mkldnn
 else
     echo "BASE_CUDA_VERSION is set to: $DESIRED_CUDA"
-    USE_PRIORITIZED_TEXT_FOR_LD=1 python /builder/aarch64_linux/aarch64_wheel_ci_build.py --enable-mkldnn --enable-cuda
+    #USE_PRIORITIZED_TEXT_FOR_LD for enable linker script optimization https://github.com/pytorch/pytorch/pull/121975/files
+    USE_PRIORITIZED_TEXT_FOR_LD=1 python /builder/aarch64_linux/aarch64_wheel_ci_build.py --enable-mkldnn --enable-cuda 
 fi
