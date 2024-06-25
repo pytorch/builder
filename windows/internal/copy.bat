1f78755a4642dcbf1473c563e59fa83772b36307
Andrey Talman <atalman@fb.com>
2024-06-25 15:28:58 -0400

    Updated MKL on Windows to 2022.2.1 (#1887)
    * Updated MKL on Windows to 2022.2.1
    
    * Fix build
    
    * another fix
    
    ---------
    
    Co-authored-by: mantaionut <ionut@janeasystems.com>

--- windows/internal/copy.bat
+++ windows/internal/copy.bat
@@ -11,7 +11,6 @@ copy "%CUDA_PATH%\extras\CUPTI\lib64\cupti64_*.dll*" pytorch\torch\lib
 
 copy "C:\Program Files\NVIDIA Corporation\NvToolsExt\bin\x64\nvToolsExt64_1.dll*" pytorch\torch\lib
 copy "%CONDA_LIB_PATH%\libiomp*5md.dll" pytorch\torch\lib
-
 :: Should be set in build_pytorch.bat
 copy "%libuv_ROOT%\bin\uv.dll" pytorch\torch\lib
 
