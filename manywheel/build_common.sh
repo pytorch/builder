25b69b380b6655f7e9c3ae6bbfb5e2edb1891645
Sahan Paliskara <sahancpal@gmail.com>
2024-06-25 13:29:59 -0700

    Update build_common.sh

--- manywheel/build_common.sh
+++ manywheel/build_common.sh
@@ -445,6 +445,9 @@ for pkg in /$WHEELHOUSE_DIR/torch_no_python*.whl /$WHEELHOUSE_DIR/torch*linux*.w
         echo "removing extraneous .so and .a files"
         # todo @PaliC: Remove these .so and .a files before hand in the split build
         rm *.so *.a || true
+
+        echo "Packaging the following files"
+        ls -alR
     fi
 
     # zip up the wheel back
