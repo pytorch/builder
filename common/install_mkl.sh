aa3b96c18e833ef57404f20dde27fdbc28d10a8f
Andrey Talman <atalman@fb.com>
2024-06-28 16:21:34 -0400

    Switch mkl-static and mkl-include install to pip

--- common/install_mkl.sh
+++ common/install_mkl.sh
@@ -8,7 +8,7 @@ MKL_VERSION=2022.2.1
 mkdir -p /opt/intel/
 pushd /tmp
 
-pip download -d . mkl-static==${MKL_VERSION}
+python3 -mpip download -d . mkl-static==${MKL_VERSION}
 unzip mkl_static-${MKL_VERSION}-py2.py3-none-manylinux1_x86_64.whl
 unzip mkl_include-${MKL_VERSION}-py2.py3-none-manylinux1_x86_64.whl
 mv mkl_static-${MKL_VERSION}.data/data/lib /opt/intel/
