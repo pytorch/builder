ca0bb29ba3a09281d16c880fe66bfc467fb4016e
atalman <atalman@fb.com>
2024-06-28 14:02:11 -0700

    fix

--- common/install_mkl.sh
+++ common/install_mkl.sh
@@ -7,7 +7,9 @@ MKL_VERSION=2022.2.1
 mkdir -p /opt/intel/
 pushd /tmp
 
+python3 -mpip install wheel
 python3 -mpip download -d . mkl-static==${MKL_VERSION}
 python3 -m wheel unpack mkl_static-${MKL_VERSION}-py2.py3-none-manylinux1_x86_64.whl
 python3 -m wheel unpack mkl_include-${MKL_VERSION}-py2.py3-none-manylinux1_x86_64.whl
-mv mkl_static-${MKL_VERSION}.data/data/lib /opt/intel/
+mv mkl_static-${MKL_VERSION}/mkl_static-${MKL_VERSION}.data/data/lib /opt/intel/
+mv mkl_include-${MKL_VERSION}/mkl_include-${MKL_VERSION}.data/data/include /opt/intel/
