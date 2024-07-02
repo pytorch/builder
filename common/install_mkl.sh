cf271eb7261cdd4ac48ce1ea46f86f14d7e3f5af
Andrey Talman <atalman@fb.com>
2024-07-02 09:14:58 -0400

    MKL install use pipy (#1899)
    * MKL install use pipy
    
    * fix
    
    * test
    
    * test
    
    * remove_debug

--- common/install_mkl.sh
+++ common/install_mkl.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
 set -ex
 
 # MKL
 MKL_VERSION=2022.2.1
-MKL_BUILD=16993
-mkdir -p /opt/intel/lib
+
+mkdir -p /opt/intel/
 pushd /tmp
-curl -fsSL https://anaconda.org/intel/mkl-static/${MKL_VERSION}/download/linux-64/mkl-static-${MKL_VERSION}-intel_${MKL_BUILD}.tar.bz2 | tar xjv
-mv lib/* /opt/intel/lib/
-curl -fsSL https://anaconda.org/intel/mkl-include/${MKL_VERSION}/download/linux-64/mkl-include-${MKL_VERSION}-intel_${MKL_BUILD}.tar.bz2 | tar xjv
-mv include /opt/intel/
+
+python3 -mpip install wheel
+python3 -mpip download -d . mkl-static==${MKL_VERSION}
+python3 -m wheel unpack mkl_static-${MKL_VERSION}-py2.py3-none-manylinux1_x86_64.whl
+python3 -m wheel unpack mkl_include-${MKL_VERSION}-py2.py3-none-manylinux1_x86_64.whl
+mv mkl_static-${MKL_VERSION}/mkl_static-${MKL_VERSION}.data/data/lib /opt/intel/
+mv mkl_include-${MKL_VERSION}/mkl_include-${MKL_VERSION}.data/data/include /opt/intel/
