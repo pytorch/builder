e29cb0d6d39e92f6c41fe9e74496ef3ba0bd5064
Jithun Nair <jithun.nair@amd.com>
2024-08-22 23:28:24 +0000

    Revert temporary build of MIOpen from source, since mainline build 14616 has the patch already

--- common/install_miopen.sh
+++ common/install_miopen.sh
@@ -45,9 +45,8 @@ MIOPEN_CMAKE_COMMON_FLAGS="
 "
 # Pull MIOpen repo and set DMIOPEN_EMBED_DB based on ROCm version
 if [[ $ROCM_INT -ge 60300 ]] && [[ $ROCM_INT -lt 60400 ]]; then
-    # TEMPORARY FOR ROCM6.3 MAINLINE: use std:filesystem patch
-    #echo "ROCm 6.3 MIOpen does not need any patches, do not build from source"
-    MIOPEN_BRANCH=amd-master
+    echo "ROCm 6.3 MIOpen does not need any patches, do not build from source"
+    exit 0
 elif [[ $ROCM_INT -ge 60200 ]] && [[ $ROCM_INT -lt 60300 ]]; then
     echo "ROCm 6.2 MIOpen does not need any patches, do not build from source"
     exit 0
