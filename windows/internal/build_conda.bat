8839aadfe98ba422aa79a5d3d61e263962a20e36
Andrey Talman <atalman@fb.com>
2024-12-04 16:03:15 +0000

    Remove windows conda build script (#2047)

--- windows/internal/build_conda.bat
+++
@@ -1,8 +0,0 @@
-call windows/internal/vc_install_helper.bat
-if errorlevel 1 exit /b 1
-
-call windows/internal/cuda_install.bat
-if errorlevel 1 exit /b 1
-
-bash ./conda/build_pytorch.sh %CUDA_VERSION% %PYTORCH_BUILD_VERSION% %PYTORCH_BUILD_NUMBER%
-if errorlevel 1 exit /b 1
